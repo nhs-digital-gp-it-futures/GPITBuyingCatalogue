bb3fe8a08634e37c80e3c16ce956350956fc0cf4
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2021-04-07 12:03:52 +0100

    Basic layout complete

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Controllers/HomeController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Controllers
 {
