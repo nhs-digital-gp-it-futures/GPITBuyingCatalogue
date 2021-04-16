7ae22a226ac1cf6ca944e867238e719c07aafcef
PhysicsJon <j.r.alsop.18@gmail.com>
2021-04-16 15:35:21 +0100

    Add tests and improve BrowserFactory

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Actions/SolutionsActions.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Actions/SolutionsActions.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using System;
+using System.Collections.Generic;
 
 namespace NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests.Actions
 {
@@ -20,6 +21,32 @@ namespace NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests.Actions
             {
                 return false;
             }
+        }
+
+        internal IEnumerable<IWebElement> GetSolutionsInList()
+        {
+            return Driver.FindElements(Objects.SolutionsObjects.SolutionsInList);
+        }
+
+        internal static bool SolutionListCardDisplaysAllRequiredSections(IWebElement solution)
+        {
+            try
+            {
+                solution.FindElement(Objects.SolutionsObjects.SolutionName);
+                solution.FindElement(Objects.SolutionsObjects.SolutionSummary);
+                solution.FindElement(Objects.SolutionsObjects.SolutionCapabilityList);
+                solution.FindElement(Objects.SolutionsObjects.SolutionSupplierName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        internal static void ClickSolution(IWebElement selectedSolution)
+        {
+            selectedSolution.Click();
         }
     }
 }
--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/SolutionsObjects.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/SolutionsObjects.cs
@@ -5,5 +5,21 @@ namespace NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests.Objects
     internal static class SolutionsObjects
     {
         public static By SolutionList => CustomBy.DataTestId("solution-cards");
+
+        public static By SolutionsInList => CustomBy.DataTestId("solution-card");
+
+        public static By SolutionName => CustomBy.DataTestId("solution-card-name");
+
+        public static By SolutionSupplierName => CustomBy.DataTestId("solution-card-supplier");
+
+        public static By SolutionSummary => CustomBy.DataTestId("solution-card-summary");
+
+        public static By SolutionCapabilityList => CustomBy.DataTestId("capability-list");
+
+        public static By SolutionCapabilityName => CustomBy.DataTestId("capability-list-item");
+
+        public static By FoundationSolutionIndicators => CustomBy.DataTestId("solution-card-foundation");
+
+        public static By CompareSolutions => CustomBy.DataTestId("compare-button", "a");
     }
 }
