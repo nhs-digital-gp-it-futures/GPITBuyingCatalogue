ed6ab7add3de9015aee8f8d7c6636550f6976482
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2021-04-26 09:59:10 +0100

    DevSync

---
+++ tests/NHSD.GPIT.BuyingCatalogue.Framework.UnitTests/Extensions/BooleanExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
+using NUnit.Framework;
+
+namespace NHSD.GPIT.BuyingCatalogue.Framework.UnitTests.Extensions
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    internal static class BooleanExtensionsTests
+    {
+        [Test]
+        [TestCase(null, "No")]
+        [TestCase("true", "Yes")]
+        [TestCase("false", "No")]
+        public static void BooleanExtension_TpYesNo_ReturnsYesOrNoCorrectly(bool? value, string expected)
+        {
+            var result = value.ToYesNo();
+            Assert.AreEqual(result, expected);
+        }      
+    }
+}
