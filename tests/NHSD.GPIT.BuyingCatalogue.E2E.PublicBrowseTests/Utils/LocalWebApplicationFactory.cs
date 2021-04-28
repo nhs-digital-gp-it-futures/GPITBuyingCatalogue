d7195cbbec32dafd2296d1809c8402ebceceae35
PhysicsJon <j.r.alsop.18@gmail.com>
2021-04-28 09:28:34 +0100

    Add env variable for marketing page operation

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Utils/LocalWebApplicationFactory.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Utils/LocalWebApplicationFactory.cs
@@ -26,6 +26,7 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Utils
         // Need to find a better way of doing this
         private const string BC_DB_CONNECTION = "Server=localhost,1450;Database=buyingcatalogue;User=SA;password=8VSKwQ8xgk35qWFm8VSKwQ8xgk35qWFm!;Integrated Security=false";
         private const string ID_DB_CONNECTION = "Server=localhost,1450;Database=CatalogueUsers;User=SA;password=8VSKwQ8xgk35qWFm8VSKwQ8xgk35qWFm!;Integrated Security=false";
+        private const string OPERATING_MODE = "private";
 
         private const string Browser = "chrome";
 
@@ -35,15 +36,8 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Utils
             
             DbName = Guid.NewGuid().ToString();
 
-            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(nameof(BC_DB_CONNECTION))))
-            {
-                Environment.SetEnvironmentVariable(nameof(BC_DB_CONNECTION), BC_DB_CONNECTION);
-            }
+            SetEnvVariables();
 
-            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(nameof(ID_DB_CONNECTION))))
-            {
-                Environment.SetEnvironmentVariable(nameof(ID_DB_CONNECTION), BC_DB_CONNECTION);
-            }
             host = CreateWebHostBuilder().Build();
             host.Start();
 
@@ -101,6 +95,24 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Utils
             
             builder.UseUrls($"{LocalhostBaseAddress}:{new Random().Next(10000, 50000)}");
             return builder;
+        }
+
+        private void SetEnvVariables()
+        {
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(nameof(BC_DB_CONNECTION))))
+            {
+                Environment.SetEnvironmentVariable(nameof(BC_DB_CONNECTION), BC_DB_CONNECTION);
+            }
+
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(nameof(ID_DB_CONNECTION))))
+            {
+                Environment.SetEnvironmentVariable(nameof(ID_DB_CONNECTION), BC_DB_CONNECTION);
+            }
+
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(nameof(OPERATING_MODE))))
+            {
+                Environment.SetEnvironmentVariable(nameof(OPERATING_MODE), OPERATING_MODE);
+            }
         }
 
         [ExcludeFromCodeCoverage]
