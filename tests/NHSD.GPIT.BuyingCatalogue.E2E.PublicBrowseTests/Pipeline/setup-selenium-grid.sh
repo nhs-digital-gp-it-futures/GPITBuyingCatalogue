e176c94b3c8bfdc216c80bb739e5a7ed236bbd51
PhysicsJon <j.r.alsop.18@gmail.com>
2021-04-28 16:29:57 +0100

    update pipeline and remove unneeded files

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Pipeline/setup-selenium-grid.sh
+++
@@ -1,5 +0,0 @@
-﻿#/bin/bash
-
-docker-compose stop &&
-docker-compose rm -f &&
-docker-compose -f "tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Pipeline/docker-compose.yml" up --scale chrome=4 -d
