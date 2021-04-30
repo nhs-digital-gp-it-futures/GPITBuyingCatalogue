bf8e55a0d66d0d430d289a57ae740330af5fde85
PhysicsJon <j.r.alsop.18@gmail.com>
2021-04-30 16:50:05 +0100

    Add initial Client Application type tests

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Marketing/ClientApplication/BrowserBased/ConnectivityAndResolution.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.Marketing.ClientApplication.BrowserBased
+{
+    public sealed class ConnectivityAndResolution : TestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public ConnectivityAndResolution(LocalWebApplicationFactory factory) : base(factory, "marketing/supplier/solution/99999-99/section/browser-based/connectivity-and-resolution")
+        {
+            using var context = GetBCContext();
+            var solution = context.Solutions.Single(s => s.Id == "99999-99");
+            solution.ClientApplication = @"{
+                        ""ClientApplicationTypes"": [
+                            ""browser-based""
+                        ],
+                        ""BrowsersSupported"": [],
+                        ""MobileResponsive"": null,
+                        ""Plugins"": null,
+                        ""HardwareRequirements"": null,
+                        ""NativeMobileHardwareRequirements"": null,
+                        ""NativeDesktopHardwareRequirements"": null,
+                        ""AdditionalInformation"": null,
+                        ""MinimumConnectionSpeed"": null,
+                        ""MinimumDesktopResolution"": null,
+                        ""MobileFirstDesign"": null,
+                        ""NativeMobileFirstDesign"": null,
+                        ""MobileOperatingSystems"": null,
+                        ""MobileConnectionDetails"": null,
+                        ""MobileMemoryAndStorage"": null,
+                        ""MobileThirdParty"": {
+                            ""ThirdPartyComponents"": null,
+                            ""DeviceCapabilities"": null
+                        },
+                        ""NativeMobileAdditionalInformation"": null,
+                        ""NativeDesktopOperatingSystemsDescription"": null,
+                        ""NativeDesktopMinimumConnectionSpeed"": null,
+                        ""NativeDesktopThirdParty"": null,
+                        ""NativeDesktopMemoryAndStorage"": null,
+                        ""NativeDesktopAdditionalInformation"": null
+                    }";
+            context.SaveChanges();
+
+            driver.Navigate().Refresh();
+        }
+
+        [Fact]
+        public async Task ConnectivityAndResolution_SelectBothFields()
+        {
+            MarketingPages.ClientApplicationTypeActions.SelectConnectionSpeedDropdown(1);
+            MarketingPages.ClientApplicationTypeActions.SelectResolutionDropdown(1);
+
+            MarketingPages.CommonActions.ClickSave();
+
+            using var context = GetBCContext();
+            var clientApplication = (await context.Solutions.SingleAsync(s => s.Id == "99999-99")).ClientApplication;
+            clientApplication.Should().ContainEquivalentOf(@$"""MinimumConnectionSpeed"":""0.5Mbps""");
+            clientApplication.Should().ContainEquivalentOf(@$"""MinimumDesktopResolution"":""16:9 - 640 x 360""");
+        }
+
+        [Fact]
+        public void ConnectivityAndResolution_SectionComplete()
+        {
+            MarketingPages.ClientApplicationTypeActions.SelectConnectionSpeedDropdown(1);
+            MarketingPages.ClientApplicationTypeActions.SelectResolutionDropdown(1);
+
+            MarketingPages.CommonActions.ClickSave();
+
+            MarketingPages.DashboardActions.SectionMarkedComplete("Connectivity and resolution").Should().BeTrue();
+        }
+
+        [Fact]
+        public void ConnectivityAndResolution_SectionIncomplete()
+        {
+            MarketingPages.CommonActions.ClickGoBackLink();
+
+            MarketingPages.DashboardActions.SectionMarkedComplete("Connectivity and resolution").Should().BeFalse();
+        }
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Marketing/ClientApplication/BrowserBased/PlugInsOrExtensions.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.Marketing.ClientApplication.BrowserBased
+{
+    public sealed class PlugInsOrExtensions : TestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public PlugInsOrExtensions(LocalWebApplicationFactory factory) : base(factory, "marketing/supplier/solution/99999-99/section/browser-based/plug-ins-or-extensions")
+        {
+            using var context = GetBCContext();
+            var solution = context.Solutions.Single(s => s.Id == "99999-99");
+            solution.ClientApplication = @"{
+                        ""ClientApplicationTypes"": [
+                            ""browser-based""
+                        ],
+                        ""BrowsersSupported"": [],
+                        ""MobileResponsive"": null,
+                        ""Plugins"": null,
+                        ""HardwareRequirements"": null,
+                        ""NativeMobileHardwareRequirements"": null,
+                        ""NativeDesktopHardwareRequirements"": null,
+                        ""AdditionalInformation"": null,
+                        ""MinimumConnectionSpeed"": null,
+                        ""MinimumDesktopResolution"": null,
+                        ""MobileFirstDesign"": null,
+                        ""NativeMobileFirstDesign"": null,
+                        ""MobileOperatingSystems"": null,
+                        ""MobileConnectionDetails"": null,
+                        ""MobileMemoryAndStorage"": null,
+                        ""MobileThirdParty"": {
+                            ""ThirdPartyComponents"": null,
+                            ""DeviceCapabilities"": null
+                        },
+                        ""NativeMobileAdditionalInformation"": null,
+                        ""NativeDesktopOperatingSystemsDescription"": null,
+                        ""NativeDesktopMinimumConnectionSpeed"": null,
+                        ""NativeDesktopThirdParty"": null,
+                        ""NativeDesktopMemoryAndStorage"": null,
+                        ""NativeDesktopAdditionalInformation"": null
+                    }";
+            context.SaveChanges();
+
+            driver.Navigate().Refresh();
+        }
+
+        [Theory]
+        [InlineData("Yes")]
+        [InlineData("No")]
+        public async Task PlugInsOrExtensions_SelectRadioButton(string label)
+        {
+            MarketingPages.ClientApplicationTypeActions.ClickRadioButtonWithText(label);
+            var additional = MarketingPages.ClientApplicationTypeActions.EnterAdditionalInformation(1000);
+
+            MarketingPages.CommonActions.ClickSave();
+
+            string labelConvert = label == "Yes" ? "true" : "false";
+
+            using var context = GetBCContext();
+            var clientApplication = (await context.Solutions.SingleAsync(s => s.Id == "99999-99")).ClientApplication;
+            clientApplication.Should().ContainEquivalentOf(@$"Plugins"":{{""Required"":{labelConvert},""AdditionalInformation"":""{additional}""}}");
+        }
+
+        [Fact]
+        public void PlugInsOrExtensions_SectionComplete()
+        {
+            MarketingPages.ClientApplicationTypeActions.ClickRadioButtonWithText("Yes");
+            MarketingPages.ClientApplicationTypeActions.EnterAdditionalInformation(1000);
+
+            MarketingPages.CommonActions.ClickSave();
+
+            MarketingPages.DashboardActions.SectionMarkedComplete("Plug-ins or extensions required").Should().BeTrue();
+        }
+
+        [Fact]
+        public void PlugInsOrExtensions_SectionIncomplete()
+        {
+            MarketingPages.CommonActions.ClickGoBackLink();
+
+            MarketingPages.DashboardActions.SectionMarkedComplete("Plug-ins or extensions required").Should().BeFalse();
+        }
+    }
+}
