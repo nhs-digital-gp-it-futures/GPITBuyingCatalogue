d3f83bf73db4d84560d0f1e56bc9555437abc1ca
Gavin.Wakeley <gavin.wakeley@bjss.com>
2021-05-13 10:05:01 +0100

    Re-enabled code ready for merge

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,7 @@ output "webapp_URL" {
 output "webapp_default_URL" {
   value = module.webapp.webapp_default_site_hostname
 }
+
+output "webapp_verification_id" {
+  value = module.webapp.webapp_verification_id
+}
--- terraform/webapp_bindings.tf
+++ terraform/webapp_bindings.tf
@@ -5,23 +5,23 @@ resource "azurerm_app_service_certificate" "webapp" {
   key_vault_secret_id = data.azurerm_key_vault_secret.ssl_cert.id
 }
 
-# resource "azurerm_app_service_custom_hostname_binding" "webapp" {
-#   hostname            = local.gw_webappURL
-#   app_service_name    = module.webapp.webapp_name
-#   resource_group_name = azurerm_resource_group.webapp.name
+resource "azurerm_app_service_custom_hostname_binding" "webapp" {
+  hostname            = local.gw_webappURL
+  app_service_name    = module.webapp.webapp_name
+  resource_group_name = azurerm_resource_group.webapp.name
 
-# depends_on = [
-#     module.webapp,
-#    ]
-# }
+depends_on = [
+    module.webapp,
+   ]
+}
 
-# resource "azurerm_app_service_certificate_binding" "webapp" {
-#   hostname_binding_id = azurerm_app_service_custom_hostname_binding.webapp.id
-#   certificate_id      = azurerm_app_service_certificate.webapp.id
-#   ssl_state           = "SniEnabled"
+resource "azurerm_app_service_certificate_binding" "webapp" {
+  hostname_binding_id = azurerm_app_service_custom_hostname_binding.webapp.id
+  certificate_id      = azurerm_app_service_certificate.webapp.id
+  ssl_state           = "SniEnabled"
 
-#   depends_on = [
-#     azurerm_app_service_certificate.webapp,
-#     azurerm_app_service_custom_hostname_binding.webapp
-#    ]
-# }
+  depends_on = [
+    azurerm_app_service_certificate.webapp,
+    azurerm_app_service_custom_hostname_binding.webapp
+   ]
+}
