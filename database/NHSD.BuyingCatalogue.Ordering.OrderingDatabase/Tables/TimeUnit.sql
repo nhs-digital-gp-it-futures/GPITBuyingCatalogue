4a0bed576364b8559d275a2949c332ab4fc66543
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2021-05-17 16:49:24 +0100

    Added the CatalogueOrdering database

---
+++ database/NHSD.BuyingCatalogue.Ordering.OrderingDatabase/Tables/TimeUnit.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE dbo.TimeUnit
+(
+    Id int NOT NULL,
+    [Name] nvarchar(20) NOT NULL,
+    [Description] nvarchar(32) NOT NULL,
+    CONSTRAINT PK_TimeUnit PRIMARY KEY (Id),
+    CONSTRAINT AK_TimeUnit_Name UNIQUE NONCLUSTERED ([Name]),
+    CONSTRAINT AK_TimeUnit_Description UNIQUE NONCLUSTERED ([Description]),
+);
