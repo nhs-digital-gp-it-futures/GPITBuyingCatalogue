a3305d2c9260ab39728bce4587c6413b8ead2b04
EnitanOO <Enitan61513@Mastek.Com>
2021-06-14 11:25:54 +0100

    Feature & part Client App Type details e2e tests added

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/PublicBrowse/Solution/FeatureDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.PublicBrowse.Solution
+{
+    public class FeatureDetails : TestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public FeatureDetails(LocalWebApplicationFactory factory) : base(factory, "solutions/futures/99999-001/features")
+        {
+        }
+
+        [Fact]
+        public async Task FeatureDetails_VerifyFeatureContent()
+        {
+            {
+                using var context = GetBCContext();
+                var featureInfo = (await context.Solutions.SingleAsync(s => s.Id == "99999-001")).Features;
+
+                var featureList = PublicBrowsePages.SolutionAction.GetFeatureContent();
+
+                var dbList = featureInfo.Replace("[", string.Empty).Replace("\"", string.Empty).Replace("]", string.Empty).Split(',').ToList().Select(s => s.Trim());
+                featureList.Should().BeEquivalentTo(dbList);
+            }
+        }
+    }
+}
