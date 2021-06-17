bd7d8ef8048095be22ded5baa2d9c2f40f24d915
Neha Jain <neha.jain@mastek.com>
2021-06-17 11:48:07 +0100

    created header

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using NHSD.GPIT.BuyingCatalogue.Framework.Logging;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ILogWrapper<HomeController> logger;
+
+        public HomeController(ILogWrapper<HomeController> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [Route("admin")]
+        public IActionResult Index()
+        {
+            logger.LogInformation($"Taking user to {nameof(HomeController)}.{nameof(Index)}");
+
+            return View();
+        }
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Admin/Controllers/HomeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NHSD.GPIT.BuyingCatalogue.Framework.Logging;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers;
+using NUnit.Framework;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Admin.Controllers
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    internal static class HomeControllerTests
+    {
+        [Test]
+        public static void ClassIsCorrectlyDecorated()
+        {
+            typeof(HomeController).Should().BeDecoratedWith<AreaAttribute>(x => x.RouteValue == "Admin");
+        }
+
+        [Test]
+        public static void Constructor_NullLogging_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                _ = new HomeController(null));
+        }
+
+        [Test]
+        public static async Task Get_Index_ReturnsDefaultView()
+        {
+            var controller = new HomeController(Mock.Of<ILogWrapper<HomeController>>());
+
+            var result = await controller.Index();
+
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            Assert.IsNull(((ViewResult)result).ViewName);
+        }
+    }
+}
