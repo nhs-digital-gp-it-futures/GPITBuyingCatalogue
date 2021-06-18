b9b0ff4ae9ccfec5dc3da346f0b4db42b75295bc
Keith Lyon <Keith.Lyon@mastek.com>
2021-06-18 16:38:25 +0100

    setting colour from PublicationStatus

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Models/CatalogueSolutionsModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Models/CatalogueSolutionsModel.cs
@@ -8,21 +8,34 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models
 {
     public class CatalogueSolutionsModel : NavBaseModel
     {
+        public List<PublicationStatus> PublicationStatuses { get; set; }
+
         public List<CatalogueItem> CatalogueItems { get; set; }
 
         public string GetStatusClass(int statusId)
         {
-            string colour;
+            string colour = "white";
 
-            switch (statusId)
-            {
-                case 2: colour = "yellow"; break;
-                case 3: colour = "green"; break;
-                case 4: colour = "red"; break;
-                default: colour = "white"; break;
-            }
+            if (PublicationStatus.Published.Id == statusId)
+                colour = "white";
+            if (PublicationStatus.Unpublished.Id == statusId)
+                colour = "yellow";
+            if (PublicationStatus.Published.Id == statusId)
+                colour = "green";
+            if (PublicationStatus.Withdrawn.Id == statusId)
+                colour = "red";
 
             return $"nhsuk-tag--{colour}";
+
+            // this doesn't work... WHY ?
+            // switch (statusId)
+            // {
+            //    case PublicationStatus.Draft.Id: colour = "white"; break;
+            //    case PublicationStatus.Unpublished.Id: colour = "yellow"; break;
+            //    case PublicationStatus.Published.Id: colour = "green"; break;
+            //    case PublicationStatus.Withdrawn.Id: colour = "red"; break;
+            //    default: colour = "white"; break;
+            // }
         }
     }
 }
