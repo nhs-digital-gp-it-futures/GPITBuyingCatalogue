591623346ae355152870d9bb346f79e2ac06c09a
Keith Lyon <Keith.Lyon@mastek.com>
2021-06-18 14:46:09 +0100

    showing solutions with mock data

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/CatalogueSolutionsController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/CatalogueSolutionsController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Models.GPITBuyingCatalogue;
 using NHSD.GPIT.BuyingCatalogue.Framework.Logging;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
 {
@@ -16,11 +21,42 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string id)
         {
             logger.LogInformation($"Taking user to {nameof(CatalogueSolutionsController)}.{nameof(Index)}");
 
-            return View();
+            var solutionModel = new CatalogueSolutionsModel()
+            {
+                CatalogueItems = DummyData(),
+            };
+
+            return View(solutionModel);
+        }
+
+
+        private List<CatalogueItem> DummyData() 
+        {
+            List<CatalogueItem> items = new List<CatalogueItem>
+            {
+                new CatalogueItem()
+                {
+                    CatalogueItemId = "1000",
+                    Name = "abc",
+                    SupplierId = "1",
+                    PublishedStatusId = 1,
+
+                },
+                new CatalogueItem()
+                {
+                    CatalogueItemId = "22222",
+                    Name = "asdfasdfasdf",
+                    SupplierId = "333",
+                    PublishedStatusId = 3,
+
+                },
+            };
+
+            return items;
         }
     }
 }
---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Models/CatalogueSolutionsModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Models.GPITBuyingCatalogue;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models;
+
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models
+{
+    public class CatalogueSolutionsModel : NavBaseModel
+    {
+        public List<CatalogueItem> CatalogueItems { get; set; }
+    }
+}
