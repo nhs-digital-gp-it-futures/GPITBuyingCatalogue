100bc3c40413d3947b44c9d838325295b9a8454b
Keith Lyon <Keith.Lyon@mastek.com>
2021-06-18 12:05:07 +0100

    navigating to catalogue-solutions page

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/CatalogueSolutionsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using NHSD.GPIT.BuyingCatalogue.Framework.Logging;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin/catalogue-solutions")]
+    public class CatalogueSolutionsController : Controller
+    {
+        private readonly ILogWrapper<HomeController> logger;
+
+        public CatalogueSolutionsController(ILogWrapper<HomeController> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public IActionResult Index()
+        {
+            logger.LogInformation($"Taking user to {nameof(CatalogueSolutionsController)}.{nameof(Index)}");
+
+            return View();
+        }
+    }
+}
