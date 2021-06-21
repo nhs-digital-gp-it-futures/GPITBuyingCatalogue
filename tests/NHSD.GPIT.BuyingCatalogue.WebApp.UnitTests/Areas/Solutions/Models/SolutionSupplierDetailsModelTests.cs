2a9c2132592dd8963733b211ad780ebbaa84eb58
Kiran Singh <kiran.singh@mastek.com>
2021-06-21 07:08:04 +0100

    Minor refactors Move title to layout Use CSS to preserve user input

---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Solutions/Models/SolutionSupplierDetailsModelTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models;
+using NUnit.Framework;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Solutions.Models
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    internal static class SolutionSupplierDetailsModelTests
+    {
+        [Test]
+        public static void Class_Inherits_SolutionDisplayBaseModel()
+        {
+            typeof(SolutionSupplierDetailsModel)
+                .Should()
+                .BeAssignableTo<SolutionDisplayBaseModel>();
+        }
+
+        [Test, AutoData]
+        public static void HasContacts_ValidContacts_ReturnsTrue(SolutionSupplierDetailsModel model)
+        {
+            model.Contacts.Any(x => x != null).Should().BeTrue();
+
+            model.HasContacts().Should().BeTrue();
+        }
+
+        [Test]
+        public static void HasContacts_NullContacts_ReturnsFalse()
+        {
+            var model = new SolutionSupplierDetailsModel
+            {
+                Contacts = new List<SupplierContactViewModel> { null, null, },
+            };
+
+            model.HasContacts().Should().BeFalse();
+        }
+
+        [Test]
+        public static void HasContacts_EmptyContacts_ReturnsFalse()
+        {
+            var model = new SolutionSupplierDetailsModel
+            {
+                Contacts = new List<SupplierContactViewModel>(),
+            };
+
+            model.HasContacts().Should().BeFalse();
+        }
+
+        [Test]
+        public static void HasContacts_NullCollection_ReturnsFalse()
+        {
+            var model = new SolutionSupplierDetailsModel
+            {
+                Contacts = null,
+            };
+
+            model.HasContacts().Should().BeFalse();
+        }
+    }
+}
