47c42c35dfce3b910d4eaeddc927641a6f030048
EnitanOO <Enitan61513@Mastek.Com>
2021-06-22 17:29:29 +0100

    Supplier details e2e tests added

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/PublicBrowse/Solution/SupplierDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.PublicBrowse.Solution
+{
+    public class SupplierDetails : TestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public SupplierDetails(LocalWebApplicationFactory factory) : base(factory, "solutions/futures/99999-001/supplier-details")
+        {
+        }
+
+        [Theory]
+        [InlineData("Contact name")]
+        [InlineData("Contact details")]
+
+        public void SupplierDetails_AdminHeaderDisplayed(string rowHeader)
+        {
+            PublicBrowsePages.SolutionAction.GetTableRowContent(rowHeader).Should().NotBeNullOrEmpty();
+        }
+        
+        [Fact]
+        public async Task SupplierDetails_VerifyInformation()
+        {
+            using var context = GetBCContext();
+            var info = (await context.Suppliers.SingleAsync(s => s.Id == "99999")).Summary;
+            var supplierInfo = PublicBrowsePages.SolutionAction.GetSummaryAndDescriptions();
+
+            supplierInfo.Should().Contain(info);
+            
+        }
+    }
+}
