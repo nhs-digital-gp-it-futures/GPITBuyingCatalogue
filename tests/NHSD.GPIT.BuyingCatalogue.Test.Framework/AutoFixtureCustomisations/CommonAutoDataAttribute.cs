d12a11da75ac3cef0b2c0f17cd1017218f8b29f3
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2021-06-29 11:44:32 +0100

    Removed SupplierCustomization

--- tests/NHSD.GPIT.BuyingCatalogue.Test.Framework/AutoFixtureCustomisations/CommonAutoDataAttribute.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.Test.Framework/AutoFixtureCustomisations/CommonAutoDataAttribute.cs
@@ -9,16 +9,14 @@ namespace NHSD.GPIT.BuyingCatalogue.Test.Framework.AutoFixtureCustomisations
         public CommonAutoDataAttribute() :
             base(() => new Fixture().Customize(
                 new CompositeCustomization(
-                    new AutoMoqCustomization(),                    
-                    new IgnoreCircularReferenceCustomisation(),                    
+                    new AutoMoqCustomization(),                                        
                     new CallOffIdCustomization(),
                     new CatalogueItemIdCustomization(),
 
                     // TODO: causes test run initialization slow down
-                    new SolutionCustomization(),
-                    new SupplierCustomization()
-                    
-                    // TODO: This one in particular, yet no tests broken when its removed
+                    new IgnoreCircularReferenceCustomisation(),
+                    new SolutionCustomization()
+                    //new SupplierCustomization()                    
                     //new CataloguePriceCustomization()
                     )))
         {
