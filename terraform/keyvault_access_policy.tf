85c30ae6c51273fa868c1d1af39b8dcd5c226ab8
Steve Scott <steve.scott@bjss.com>
2021-06-29 14:46:59 +0100

    Update access policy for keyvault

--- terraform/keyvault_access_policy.tf
+++ terraform/keyvault_access_policy.tf
@@ -2,9 +2,9 @@ data "azurerm_client_config" "current" {
 }
 
 
-resource "azurerm_key_vault_access_policy" "keyvault_current_access" {
+resource "azurerm_key_vault_access_policy" "keyvault_pipeline_access" {
   key_vault_id = azurerm_key_vault.keyvault.id
-  tenant_id    = var.tenant_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
   key_permissions = [
@@ -57,81 +57,6 @@ resource "azurerm_key_vault_access_policy" "keyvault_current_access" {
     "restore",
     "setissuers",
     "update"
-
-  ]
-
-  depends_on = [
-    azurerm_key_vault.keyvault,
-  ]
-
-  lifecycle {
-    ignore_changes = [
-      object_id
-    ]
-  }
-}
-
-resource "azurerm_key_vault_access_policy" "keyvault_devops_access" {
-  key_vault_id = azurerm_key_vault.keyvault.id
-  tenant_id    = var.tenant_id
-  object_id    = data.azurerm_key_vault_secret.spn_appid.value
-  
-  key_permissions = [
-
-    "backup",
-    "create",
-    "decrypt",
-    "delete",
-    "encrypt",
-    "get",
-    "import",
-    "list",
-    "purge",
-    "recover",
-    "restore",
-    "sign",
-    "unwrapKey",
-    "update",
-    "verify",
-    "wrapKey"
-  ]
-
-  secret_permissions = [
-
-    "backup",
-    "delete",
-    "get",
-    "list",
-    "purge",
-    "recover",
-    "restore",
-    "set"
-  ]
-
-  certificate_permissions = [
-
-    "backup",
-    "create",
-    "delete",
-    "deleteissuers",
-    "get",
-    "getissuers",
-    "import",
-    "list",
-    "listissuers",
-    "managecontacts",
-    "manageissuers",
-    "purge",
-    "recover",
-    "restore",
-    "setissuers",
-    "update"
-
-  ]
-
-  depends_on = [
-    azurerm_key_vault.keyvault,
-    azurerm_key_vault_access_policy.keyvault_current_access
   ]
 }
 
@@ -192,10 +117,5 @@ resource "azurerm_key_vault_access_policy" "keyvault_access" {
     "setissuers",
     "update"
 
-  ]
-
-  depends_on = [
-    azurerm_key_vault.keyvault,
-    azurerm_key_vault_access_policy.keyvault_devops_access
   ]
 }
