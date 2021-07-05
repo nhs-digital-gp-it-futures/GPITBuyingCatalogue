93f04eef63d05b749fb3945194ea654c1d558663
EnitanOO <Enitan61513@Mastek.Com>
2021-07-05 08:45:11 +0100

    Add Solution e2e tests added

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Actions/Admin/AddSolution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Actions.Common;
+using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Actions.Admin
+{
+    internal sealed class AddSolution : ActionBase
+    {
+        public AddSolution(IWebDriver driver) : base(driver)
+        {
+        }
+
+        internal bool SolutionNameFieldDisplayed()
+        {
+            return ElementDisplayed(Objects.Admin.AddSolutionObjects.SolutionName);
+        }
+
+        internal bool SupplierNameFieldDisplayed()
+        {
+            return ElementDisplayed(Objects.Admin.AddSolutionObjects.SupplierName);
+        }
+
+        public bool FrameworkNamesDisplayed()
+        {
+            try
+            {
+                Wait.Until(d => d.FindElements(Objects.Admin.AddSolutionObjects.SolutionFrameworks).Count == 2);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool GetFoundationSolution()
+        {
+            var checkbox = Driver.FindElements(Objects.Admin.AddSolutionObjects.SolutionFrameworks)[0];
+            checkbox.Click();
+            try
+            {
+                Wait.Until(s => ElementDisplayed(Objects.Admin.AddSolutionObjects.FoundationSolution));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        internal bool SaveSolutionButtonDisplayed()
+        {
+            return ElementDisplayed(Objects.Admin.AddSolutionObjects.SaveSolutionButton);
+        }
+
+        private bool ElementDisplayed(By by)
+        {
+            try
+            {
+                Driver.FindElement(by);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Admin/AddSolution.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.Admin
+{
+    public class AddSolution : TestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public AddSolution(LocalWebApplicationFactory factory) : base(factory, "admin/catalogue-solutions/add-solution")
+        {
+            Login();
+        }
+
+        [Fact]
+        public void AddSolution_AddSolutionSectionDisplayed()
+        {
+            AdminPages.AddSolution.SolutionNameFieldDisplayed().Should().BeTrue();
+            AdminPages.AddSolution.SupplierNameFieldDisplayed().Should().BeTrue();
+            AdminPages.AddSolution.SaveSolutionButtonDisplayed().Should().BeTrue();
+        }
+
+        [Fact]
+        public void AddSolution_FrameworksDisplayed()
+        {
+            AdminPages.AddSolution.FrameworkNamesDisplayed().Should().BeTrue();     
+        }
+
+        [Fact]
+        public void AddSolution_FoundationSolutionDisplayed()
+        {
+            AdminPages.AddSolution.GetFoundationSolution().Should().BeTrue();
+        }
+    }
+}
