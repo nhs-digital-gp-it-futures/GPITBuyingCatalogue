21dd05644be65543046c96fb7f107b4f68efff69
EnitanOO <Enitan61513@Mastek.Com>
2021-07-06 13:55:41 +0100

    Manage Catalogue Solutions e2e tests added

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Actions/Admin/AddSolution.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Actions/Admin/AddSolution.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using NHSD.GPIT.BuyingCatalogue.E2ETests.Actions.Common;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Models.GPITBuyingCatalogue;
 using OpenQA.Selenium;
 
 namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Actions.Admin
@@ -55,6 +56,92 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Actions.Admin
             return ElementDisplayed(Objects.Admin.AddSolutionObjects.SaveSolutionButton);
         }
 
+        public void ManageCatalougeSolution()
+        {
+            Driver.FindElement(Objects.Admin.AddSolutionObjects.CatalogueSolutionLink).Click();
+        }
+
+        public void ClickFilterCatalogueSolutionsButton()
+        {
+            Driver.FindElement(Objects.Admin.AddSolutionObjects.CatalogueSolutionFilter).Click();
+        }
+
+        public void ClickApplyFilterButton()
+        {
+            Driver.FindElement(Objects.Admin.AddSolutionObjects.SaveSolutionButton).Click();
+        }
+
+        public int NumberOfFilterRadioButtonsDisplayed()
+        {
+            ManageCatalougeSolution();
+            ClickFilterCatalogueSolutionsButton();
+            return Driver.FindElements(Objects.Admin.AddSolutionObjects.FilterRadioButton).Count;
+        }
+
+        public PublicationStatus SelectFilterRadioButton(int index = 0)
+        {
+            Wait.Until(d => NumberOfFilterRadioButtonsDisplayed() > 0);
+            var element = Driver.FindElements(Objects.Admin.AddSolutionObjects.FilterRadioButton)[index].FindElement(By.TagName("input"));
+            element.Click();
+            var id = element.GetAttribute("id");
+            int value = int.Parse(id);
+            return (PublicationStatus)value;
+        }
+
+        public PublicationStatus FilterCatalogueSolutions(int index = 0)
+        {
+            var publicationStatus = SelectFilterRadioButton(index);
+            ClickApplyFilterButton();
+            return publicationStatus;
+        }
+
+        public bool AddSolutionLinkDisplayed()
+        {
+            ManageCatalougeSolution();
+            try
+            {
+                Wait.Until(s => ElementDisplayed(Objects.Admin.AddSolutionObjects.AddSolutionLink));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        internal bool CatalogueSolutionTableDisplayed()
+        {
+            ManageCatalougeSolution();
+            try
+            {
+                Wait.Until(d => d.FindElements(Objects.Admin.AddSolutionObjects.CatalogueSolutionTable));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public int GetNumberOfItemsInTable()
+        {
+            int numberOfItems = -1;
+            try
+            {
+                numberOfItems = Driver.FindElement(Objects.Admin.AddSolutionObjects.CatalogueSolutionTable)
+                   .FindElements(By.CssSelector("tbody tr")).Count();
+            }
+            catch (NoSuchElementException)
+            {
+                numberOfItems = 0;
+            }
+            catch
+            {
+                throw new WebDriverException("Something massively wrong");
+            }
+            return numberOfItems;
+        }
+
         private bool ElementDisplayed(By by)
         {
             try
@@ -66,6 +153,6 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Actions.Admin
             {
                 return false;
             }
-        }
+        }  
     }
 }
---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Admin/ManageCatalogueSolutions.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.Admin
+{
+    public class ManageCatalogueSolutions : TestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public ManageCatalogueSolutions(LocalWebApplicationFactory factory) : base(factory, "admin")
+        {
+            Login();
+        }
+
+        [Fact]
+        public void ManageCatalogueSolutions_AddSolutionLinkDisplayed()
+        {
+            AdminPages.AddSolution.AddSolutionLinkDisplayed().Should().BeTrue();
+        }
+
+        [Fact]
+        public void ManageCatalogueSolutions_SolutionTableDisplayed()
+        {
+            AdminPages.AddSolution.CatalogueSolutionTableDisplayed().Should().BeTrue();
+
+        }
+
+        [Fact]
+        public void ManageCatalogueSolutions_FilterRadioButtonsDisplayed()
+        {
+            AdminPages.AddSolution.NumberOfFilterRadioButtonsDisplayed().Should().Be(5);
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public async Task ManageCatalogueSolutions_FilteredCatalogueSolutionsDisplayedAsync(int index)
+        {
+            var publicationStatus = AdminPages.AddSolution.FilterCatalogueSolutions(index);
+
+            await using var context = GetEndToEndDbContext();
+            var dbSolutions = await context.CatalogueItems
+                                            .Where(c => c.SupplierId == "99999")
+                                            .Where(c => c.PublishedStatus == publicationStatus)
+                                            // .Where(c => c.CatalogueItemType == CatalogueItemType.Solution) ***awaiting bug fix***
+                                            .ToListAsync();
+
+            AdminPages.AddSolution.GetNumberOfItemsInTable().Should().Be(dbSolutions.Count);
+        }
+    }
+}
