fef2ec1782c17db5750d880c80230ab0163f2ff4
Lee Rogers <lee.rogers@bjss.com>
2021-07-09 15:05:11 +0100

    Add Manage Catalogue Solutions Page, add Unit test for Controller and Fix Unit tests for other CatalogueSolutionController Endpoints

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/CatalogueSolutionsController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/CatalogueSolutionsController.cs
@@ -2,7 +2,9 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Solutions;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Users;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models;
 using PublicationStatus = NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models.PublicationStatus;
 
@@ -14,11 +16,14 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
     public sealed class CatalogueSolutionsController : Controller
     {
         private readonly ISolutionsService solutionsService;
+        private readonly IUsersService usersService;
 
         public CatalogueSolutionsController(
-            ISolutionsService solutionsService)
+            ISolutionsService solutionsService,
+            IUsersService usersService)
         {
             this.solutionsService = solutionsService ?? throw new ArgumentNullException(nameof(solutionsService));
+            this.usersService = usersService ?? throw new ArgumentNullException(nameof(usersService));
         }
 
         [HttpGet]
@@ -40,6 +45,24 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
             solutionModel.SetSelected(publicationStatus);
 
             return View(solutionModel);
+        }
+
+        [HttpGet("manage/{solutionId}")]
+        public async Task<IActionResult> ManageCatalogueSolution(CatalogueItemId solutionId)
+        {
+            var solution = await solutionsService.GetSolution(solutionId);
+
+            var lastUpdatedBy = await usersService.GetUser(solution.Solution.LastUpdatedBy.ToString());
+
+            var model = new ManageCatalogueSolutionModel
+            {
+                Solution = solution,
+            };
+
+            if (lastUpdatedBy is not null)
+                model.LastUpdatedByName = $"{lastUpdatedBy.FirstName} {lastUpdatedBy.LastName}";
+
+            return View(model);
         }
     }
 }
