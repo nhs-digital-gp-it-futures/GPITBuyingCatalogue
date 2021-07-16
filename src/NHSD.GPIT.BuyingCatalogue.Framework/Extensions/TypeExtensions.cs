124a323b4cf130591883a2830baff2d609d20c14
Lee Rogers <lee.rogers@bjss.com>
2021-07-16 10:06:10 +0100

    Fix Navigation Menu Bar, and Add new AreaName Type Extension to help facilitate it.

--- src/NHSD.GPIT.BuyingCatalogue.Framework/Extensions/TypeExtensions.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Framework/Extensions/TypeExtensions.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NHSD.GPIT.BuyingCatalogue.Framework.Extensions
 {
     public static class TypeExtensions
     {
         public static string ControllerName(this Type type) => type.Name.Replace("Controller", null);
+
+        public static string AreaName(this Type type) =>
+            (type.GetCustomAttributes(typeof(AreaAttribute), false)?.FirstOrDefault() as AreaAttribute)?.RouteValue;
     }
 }
