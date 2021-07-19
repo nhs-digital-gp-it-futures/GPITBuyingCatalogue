c848b2aee525707b60d69d25292790f663d7b278
EnitanOO <Enitan61513@Mastek.Com>
2021-07-19 12:22:04 +0100

    Edit Implementation e2e tests implemented

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Admin/AddNewSolution/Implementation.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.Marketing;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.Admin.AddNewSolution
+{
+    public sealed class Implementation : TestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public Implementation(LocalWebApplicationFactory factory)
+            : base(factory, "/admin/catalogue-solutions/manage/99999-888/implementation")
+        {
+            Login();
+        }
+
+        [Fact]
+        public async Task Implementation_TitleDisplayedCorrectly()
+        {
+            await using var context = GetEndToEndDbContext();
+            var solutionName = (await context.CatalogueItems.SingleAsync(s => s.CatalogueItemId == new CatalogueItemId(99999, "888"))).Name;
+
+            PublicBrowsePages.CommonActions.PageTitle().Should().BeEquivalentTo($"Implementation - {solutionName}");
+        }
+
+        [Fact]
+        public async Task Implementation_EnterDescription()
+        {
+            var implementation = TextGenerators.TextInputAddText(CommonSelectors.Description, 1000);
+
+            CommonActions.ClickSave();
+
+            await using var context = GetEndToEndDbContext();
+            var solution = await context.Solutions.SingleAsync(s => s.Id == new CatalogueItemId(99999, "888"));
+            solution.ImplementationDetail.Should().Be(implementation);
+        }
+
+        [Fact]
+        public async Task Implementation_GoBackWithoutSaving()
+        {
+            TextGenerators.TextInputAddText(CommonSelectors.Description, 1000);
+
+            AdminPages.CommonActions.ClickGoBack();
+
+            await using var context = GetEndToEndDbContext();
+            var solution = await context.Solutions.SingleAsync(s => s.Id == new CatalogueItemId(99999, "888"));
+            var implementation = solution.ImplementationDetail;
+
+            implementation.Should().BeNullOrEmpty();
+        }
+    }
+}
