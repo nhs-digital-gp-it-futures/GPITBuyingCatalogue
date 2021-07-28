1cf9847cb396b3c5be1565318e3fd261c942ac4b
RahulJha-mastek <78419456+RahulJha-mastek@users.noreply.github.com>
2021-07-28 16:01:37 +0100

    Remove unused code from Autofixture for HostingTypeSectionModel

--- tests/NHSD.GPIT.BuyingCatalogue.Test.Framework/AutoFixtureCustomisations/HostingTypeSectionModelCustomization.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.Test.Framework/AutoFixtureCustomisations/HostingTypeSectionModelCustomization.cs
@@ -1,31 +1,16 @@
-﻿using System;
-using AutoFixture;
+﻿using AutoFixture;
 using AutoFixture.Kernel;
-using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models;
 
 namespace NHSD.GPIT.BuyingCatalogue.Test.Framework.AutoFixtureCustomisations
 {
     public sealed class HostingTypeSectionModelCustomization : ICustomization
     {
-        private static CatalogueItem catalogueItem;
-
         public void Customize(IFixture fixture)
         {
             fixture.Customize<HostingTypeSectionModel>(c => c.FromFactory(new MethodInvoker(new GreedyConstructorQuery()))
                 .Without(m => m.SolutionId)
                 .Without(m => m.SolutionName));
-        }
-
-        private sealed class HostingTypeSectionModelSpecimenBuilder : ISpecimenBuilder
-        {
-            public object Create(object request, ISpecimenContext context)
-            {
-                if (!(request as Type == typeof(HostingTypeSectionModel)))
-                    return new NoSpecimen();
-
-                return new HostingTypeSectionModel(catalogueItem);
-            }
         }
     }
 }
