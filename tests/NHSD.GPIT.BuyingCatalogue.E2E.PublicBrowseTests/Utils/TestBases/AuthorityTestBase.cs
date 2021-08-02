da20e9767b14bd43cb081fb35363319c955cd42b
Lee Rogers <lee.rogers@bjss.com>
2021-08-02 12:04:42 +0100

    Update Session Handler Back to Async
    Update SessionHander back to using Async Functions
    Move the Session Variable Setting from the controller into a test specific function.
    
    Add Test For Single Price Solution Selection

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Utils/TestBases/AuthorityTestBase.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Utils/TestBases/AuthorityTestBase.cs
@@ -1,25 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Utils.TestBases
+﻿namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Utils.TestBases
 {
     public abstract class AuthorityTestBase : TestBase
     {
         protected AuthorityTestBase(
             LocalWebApplicationFactory factory,
-            string urlArea = "",
-            bool initialiseSession = false,
-            IDictionary<string, object> sessionValues = null)
-            : base(factory, urlArea, sessionValues)
+            string urlArea = "")
+            : base(factory, urlArea)
         {
             AuthorityLogin();
-
-            if (initialiseSession)
-                InitializeSessionAndSetValuesToSession();
-
-            if (sessionValues is not null && sessionValues.Any())
-                Driver.Navigate().Refresh();
         }
     }
 }
