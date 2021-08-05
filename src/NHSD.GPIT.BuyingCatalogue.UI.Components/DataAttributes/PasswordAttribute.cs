05ae4f7090699f49e26186df31d899ac3ce516ce
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2021-08-05 13:03:40 +0100

    Sealed PasswordAttribute

--- src/NHSD.GPIT.BuyingCatalogue.UI.Components/DataAttributes/PasswordAttribute.cs
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/DataAttributes/PasswordAttribute.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace NHSD.GPIT.BuyingCatalogue.UI.Components.DataAttributes
 {
     [AttributeUsage(AttributeTargets.Property)]
-    public class PasswordAttribute : DataTypeAttribute
+    public sealed class PasswordAttribute : DataTypeAttribute
     {
         public PasswordAttribute()
             : base(DataType.Password)
