32dfa163b082f300d383b3151f6636204eede817
PhysicsJon <j.r.alsop.18@gmail.com>
2021-08-17 15:28:04 +0100

    Add Mobile or Tablet client application types e2e tests
    Remove unnecessary method
    
    Update for PR comments and move objects

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Admin/AddNewSolution/ClientApplicationTypes/MobileOrTablet/HardwareRequirements.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils.TestBases;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.Admin.AddNewSolution.ClientApplicationTypes.MobileOrTablet
+{
+    public sealed class HardwareRequirements : AuthorityTestBase, IClassFixture<LocalWebApplicationFactory>, IDisposable
+    {
+        private static readonly CatalogueItemId SolutionId = new(99999, "002");
+
+        private static readonly Dictionary<string, string> Parameters = new()
+        {
+            { nameof(SolutionId), SolutionId.ToString() },
+        };
+
+        public HardwareRequirements(LocalWebApplicationFactory factory)
+            : base(
+                  factory,
+                  typeof(MobileTabletBasedController),
+                  nameof(MobileTabletBasedController.HardwareRequirements),
+                  Parameters)
+        {
+        }
+
+        [Fact]
+        public async Task HardwareRequirements_SavePage()
+        {
+            var expectedRequirements = TextGenerators.TextInputAddText(Objects.Common.CommonSelectors.Description, 500);
+
+            CommonActions.ClickSave();
+
+            CommonActions.PageLoadedCorrectGetIndex(
+                typeof(MobileTabletBasedController),
+                nameof(MobileTabletBasedController.MobileTablet)).Should().BeTrue();
+
+            await using var context = GetEndToEndDbContext();
+            var solution = await context.Solutions.SingleAsync(s => s.CatalogueItemId == SolutionId);
+
+            var hardwareRequirements = JsonSerializer.Deserialize<ServiceContracts.Solutions.ClientApplication>(solution.ClientApplication)?.NativeMobileHardwareRequirements;
+
+            hardwareRequirements.Should().NotBeNull();
+            hardwareRequirements.Should().Be(expectedRequirements);
+        }
+
+        [Fact]
+        public void HardwareRequirements_ClickGoBack()
+        {
+            CommonActions.ClickGoBackLink();
+
+            CommonActions.PageLoadedCorrectGetIndex(
+                typeof(MobileTabletBasedController),
+                nameof(MobileTabletBasedController.MobileTablet))
+                .Should().BeTrue();
+        }
+
+        public void Dispose()
+        {
+            ClearClientApplication(SolutionId);
+        }
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Admin/AddNewSolution/ClientApplicationTypes/MobileOrTablet/SupportedOperatingSystems.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils.TestBases;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.Admin.AddNewSolution.ClientApplicationTypes.MobileOrTablet
+{
+    public sealed class SupportedOperatingSystems : AuthorityTestBase, IClassFixture<LocalWebApplicationFactory>, IDisposable
+    {
+        private static readonly CatalogueItemId SolutionId = new(99999, "002");
+
+        private static readonly Dictionary<string, string> Parameters = new()
+        {
+            { nameof(SolutionId), SolutionId.ToString() },
+        };
+
+        public SupportedOperatingSystems(LocalWebApplicationFactory factory)
+            : base(
+                  factory,
+                  typeof(MobileTabletBasedController),
+                  nameof(MobileTabletBasedController.OperatingSystems),
+                  Parameters)
+        {
+        }
+
+        [Fact]
+        public async Task SupportedOperatingSystems_SaveData()
+        {
+            var operatingSystem = CommonActions.ClickCheckbox(Objects.Common.CommonSelectors.CheckboxItem);
+
+            var description = TextGenerators.TextInputAddText(Objects.Common.CommonSelectors.Description, 1000);
+
+            CommonActions.ClickSave();
+
+            CommonActions.PageLoadedCorrectGetIndex(
+                typeof(MobileTabletBasedController),
+                nameof(MobileTabletBasedController.MobileTablet))
+                .Should().BeTrue();
+
+            await using var context = GetEndToEndDbContext();
+
+            var solution = await context.Solutions.SingleOrDefaultAsync(s => s.CatalogueItemId == SolutionId);
+
+            var mobileOperatingSystems = JsonSerializer.Deserialize<ServiceContracts.Solutions.ClientApplication>(solution.ClientApplication)?.MobileOperatingSystems;
+
+            mobileOperatingSystems.Should().NotBeNull();
+            mobileOperatingSystems.OperatingSystems.Should().ContainEquivalentOf(operatingSystem);
+            mobileOperatingSystems.OperatingSystemsDescription.Should().Be(description);
+        }
+
+        [Fact(Skip = "Error response not thrown")]
+        public void SupportedOperatingSystems_ErrorThrownMissingMandatory()
+        {
+            CommonActions.ClickSave();
+
+            CommonActions.ErrorSummaryDisplayed().Should().BeTrue();
+            CommonActions.ErrorSummaryLinksExist().Should().BeTrue();
+        }
+
+        [Fact]
+        public void SupportedOperatingSystems_ClickGoBack()
+        {
+            CommonActions.ClickGoBackLink();
+
+            CommonActions.PageLoadedCorrectGetIndex(
+                typeof(MobileTabletBasedController),
+                nameof(MobileTabletBasedController.MobileTablet))
+                .Should().BeTrue();
+        }
+
+        public void Dispose()
+        {
+            ClearClientApplication(SolutionId);
+        }
+    }
+}
