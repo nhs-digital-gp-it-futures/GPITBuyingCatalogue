4963825c405a3577d9cb80bab07fb059d9f622c1
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2021-08-20 08:53:10 +0100

    Update based on peer review

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/CatalogueSolutionsController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/CatalogueSolutionsController.cs
@@ -533,15 +533,15 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
             return model.SelectedApplicationType switch
             {
                 ServiceContracts.Solutions.ClientApplicationType.BrowserBased => RedirectToAction(
-                    model.SelectedApplicationType.ToString(),
+                    nameof(CatalogueSolutionsController.BrowserBased),
                     typeof(CatalogueSolutionsController).ControllerName(),
                     new { solutionId }),
                 ServiceContracts.Solutions.ClientApplicationType.MobileTablet => RedirectToAction(
-                    model.SelectedApplicationType.ToString(),
+                    nameof(MobileTabletBasedController.MobileTablet),
                     typeof(MobileTabletBasedController).ControllerName(),
                     new { solutionId }),
                 ServiceContracts.Solutions.ClientApplicationType.Desktop => RedirectToAction(
-                    model.SelectedApplicationType.ToString(),
+                    nameof(DesktopBasedController.Desktop),
                     typeof(DesktopBasedController).ControllerName(),
                     new { solutionId }),
                 _ => RedirectToAction(nameof(ClientApplicationType), new { solutionId }),
