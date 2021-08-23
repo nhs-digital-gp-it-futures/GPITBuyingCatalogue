77fe7bc8b6424e893f48cf3c78c69b8014668fe7
Lee Rogers <lee.rogers@bjss.com>
2021-08-23 13:19:11 +0100

    Fix Unit Tests
    Fix the Unit Tests for the CatalogueSolutions and AdditionalServices Recipient Date Back Link Unit tests and remove unneeded tests since the back link now doesn't change.

--- tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Models/CatalogueSolutionRecipientsDate/SelectSolutionServiceRecipientsDateModelTests.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Models/CatalogueSolutionRecipientsDate/SelectSolutionServiceRecipientsDateModelTests.cs
@@ -20,23 +20,6 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Models.Catalogu
 
             var model = new SelectSolutionServiceRecipientsDateModel(odsCode, state, defaultDeliveryDate);
 
-            model.BackLink.Should().Be($"/order/organisation/{odsCode}/order/{state.CallOffId}/catalogue-solutions/select/solution/price");
-            model.BackLinkText.Should().Be("Go back");
-            model.Title.Should().Be($"Planned delivery date of {state.CatalogueItemName} for {state.CallOffId}");
-            model.CommencementDate.Should().Be(state.CommencementDate);
-        }
-
-        [Theory]
-        [CommonAutoData]
-        public static void WithValidArguments_AndSkipPrice_PropertiesCorrectlySet(
-            string odsCode,
-            CreateOrderItemModel state,
-            DateTime? defaultDeliveryDate)
-        {
-            state.SkipPriceSelection = true;
-
-            var model = new SelectSolutionServiceRecipientsDateModel(odsCode, state, defaultDeliveryDate);
-
             model.BackLink.Should().Be($"/order/organisation/{odsCode}/order/{state.CallOffId}/catalogue-solutions/select/solution/price/recipients");
             model.BackLinkText.Should().Be("Go back");
             model.Title.Should().Be($"Planned delivery date of {state.CatalogueItemName} for {state.CallOffId}");
