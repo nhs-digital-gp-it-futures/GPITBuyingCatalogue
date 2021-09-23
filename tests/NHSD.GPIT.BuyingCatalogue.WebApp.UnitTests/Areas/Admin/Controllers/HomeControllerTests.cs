8ccf2572bad7ae9283354debbef5b78fc8311ef4
Colin Murphy <colin.murphy@bjss.com>
2021-09-23 10:25:45 +0100

    Remove AutoMapper from Buyer Organisations View

--- tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Admin/Controllers/HomeControllerTests.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Admin/Controllers/HomeControllerTests.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.Idioms;
-using AutoMapper;
 using FluentAssertions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -52,31 +51,16 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Admin.Controllers
         public static async Task Get_BuyerOrganisations_GetsAllOrganisations()
         {
             var mockOrganisationService = new Mock<IOrganisationsService>();
+            var mockOrganisations = new Mock<IList<Organisation>>().Object;
+            mockOrganisationService.Setup(o => o.GetAllOrganisations())
+                .ReturnsAsync(mockOrganisations);
+
             var controller = new HomeController(
-                mockOrganisationService.Object,
-                Mock.Of<IMapper>());
+                mockOrganisationService.Object);
 
             await controller.BuyerOrganisations();
 
             mockOrganisationService.Verify(o => o.GetAllOrganisations());
-        }
-
-        [Fact]
-        public static async Task Get_BuyerOrganisations_MapsOrganisationsToModels()
-        {
-            var mockOrganisationService = new Mock<IOrganisationsService>();
-            var mockOrganisations = new Mock<IList<Organisation>>().Object;
-            mockOrganisationService.Setup(o => o.GetAllOrganisations())
-                .ReturnsAsync(mockOrganisations);
-            var mockMapper = new Mock<IMapper>();
-
-            var controller = new HomeController(
-                mockOrganisationService.Object,
-                mockMapper.Object);
-
-            await controller.BuyerOrganisations();
-
-            mockMapper.Verify(m => m.Map<IList<Organisation>, IList<OrganisationModel>>(mockOrganisations));
         }
 
         [Fact]
@@ -86,27 +70,30 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Admin.Controllers
             var mockOrganisations = new Mock<IList<Organisation>>().Object;
             mockOrganisationService.Setup(o => o.GetAllOrganisations())
                 .ReturnsAsync(mockOrganisations);
-            var mockMapper = new Mock<IMapper>();
-            var mockOrganisationModels = new Mock<IList<OrganisationModel>>().Object;
-            mockMapper.Setup(m => m.Map<IList<Organisation>, IList<OrganisationModel>>(mockOrganisations))
-                .Returns(mockOrganisationModels);
+
+            var expectedOrganisationModels = mockOrganisations.Select(
+                o => new OrganisationModel
+                {
+                    Id = o.Id,
+                    Name = o.Name,
+                    OdsCode = o.OdsCode,
+                }).ToList();
+
             var controller = new HomeController(
-                mockOrganisationService.Object,
-                mockMapper.Object);
+                mockOrganisationService.Object);
 
             var actual = (await controller.BuyerOrganisations()).As<ViewResult>();
 
             actual.Should().NotBeNull();
             actual.ViewName.Should().BeNullOrEmpty();
-            Assert.Same(mockOrganisationModels, actual.Model.As<ListOrganisationsModel>().Organisations);
+            actual.Model.As<ListOrganisationsModel>().Organisations.Should().BeEquivalentTo(expectedOrganisationModels);
         }
 
         [Fact]
         public static void Get_Index_ReturnsDefaultView()
         {
             var controller = new HomeController(
-                Mock.Of<IOrganisationsService>(),
-                Mock.Of<IMapper>());
+                Mock.Of<IOrganisationsService>());
 
             var result = controller.Index().As<ViewResult>();
 
