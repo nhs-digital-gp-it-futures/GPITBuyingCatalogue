796c8bda7bbf46cb9f4f271812b915b4b68c4c2d
Lee Rogers <lee.rogers@bjss.com>
2021-09-28 09:45:22 +0100

    Fix Capability Filters for Epics
    Fix the Capability Filter Compression Logic to work with the 3 Epic Id patterns that exist within the application.
    
    Apply the fix to CatalogueSolutionsFilter.js
    
    Apply the Fix to SolutionsFilterService.cs

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/js/CatalogueSolutionsFilter.js
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/js/CatalogueSolutionsFilter.js
@@ -4,7 +4,11 @@ const querySelectedFramework = "selectedframework";
 const filterContainer = "filter-container";
 const filterCapabilitiesDetails = "filter-capabilities-details";
 const filterDetailsText = ".nhsuk-details__text";
-const EpicSplitCharacter = "E";
+const EpicSplitCharacter = 'E';
+const DFOCVCMarker = 'D';
+const SupplierSplitCharacter = 'S';
+const SupplierSolutionCharacter = 'X';
+const CapabilityMarker = 'C';
 const InputTypeHidden = "input[type='hidden']";
 const NhsukCheckboxesInput = ".nhsuk-checkboxes__input";
 const NhsukCheckboxesItem = ".nhsuk-checkboxes__item";
@@ -75,8 +79,12 @@ function generateQueryParam() {
         if (checkbox.checked) {
             const value = checkbox.parentNode.querySelector(InputTypeHidden).getAttribute("value");
 
-            if (value.includes(EpicSplitCharacter))
-                output += value.substring(value.indexOf(EpicSplitCharacter));
+            if(value.includes(SupplierSplitCharacter))
+                output += EncodeSupplierDefinedEpic(value);
+            else if(value.includes(EpicSplitCharacter) && !value.includes(CapabilityMarker))
+                output += EncodeDFOCVCEpic(value);
+            else if(value.includes(EpicSplitCharacter) && value.includes(CapabilityMarker))
+                output += EncodeNormalEpic(value)
             else
                 if (output.length > 1)
                     output += CapabilitiesDelimiter + value;
@@ -165,13 +173,17 @@ function reselectCapabilityAndEpicsFiltersAndFrameworkFilter() {
                 CheckCheckboxWithHiddenInputValue(capability);
             }
             else {
-                const epics = capability.split(EpicSplitCharacter);
+                const epics = capability.split(SupplierSplitCharacter).map(s => s.split(EpicSplitCharacter)).flat();
 
                 epics.forEach(epic => {
-                    if (epic.startsWith(CapabilitiesSplitCharacter))
+                    if(epic.startsWith(CapabilitiesSplitCharacter))
                         CheckCheckboxWithHiddenInputValue(epic);
+                    else if(epic.includes(DFOCVCMarker))
+                        CheckCheckboxWithHiddenInputValue(DecodeDFOCVCEpic(epic));
+                    else if(epic.includes(SupplierSolutionCharacter))
+                        CheckCheckboxWithHiddenInputValue(DecodeSupplierDefinedEpic(epic));
                     else
-                        CheckCheckboxWithHiddenInputValue(epics[0] + EpicSplitCharacter + epic);
+                        CheckCheckboxWithHiddenInputValue(DecodeNormalEpic(epics[0], epic));
                 });
             }
         });
@@ -208,17 +220,48 @@ function GetSelectedFramework() {
 }
 
 function RefireDomContentLoadedEvent() {
-    const DomContentLoaded = "DOMContentLoaded";
-    let event; // The custom event that will be created
-    if (document.createEvent) {
-        event = document.createEvent("HTMLEvents");
-        event.initEvent(DomContentLoaded, true, true);
-        event.eventName = DomContentLoaded;
-        document.dispatchEvent(event);
-    } else {
-        event = document.createEventObject();
-        event.eventName = DomContentLoaded;
-        event.eventType = DomContentLoaded;
-        document.fireEvent("on" + event.eventType, event);
-    }
+    window.document.dispatchEvent(new Event("DOMContentLoaded", {
+        bubbles: true,
+        cancelable: true,
+    }));
+}
+
+// Epic Id's have 3 Patterns: Normal, DFOCVC and Supplier Defined
+// Normal is the CapabilityId e.g  "C47" followed by its unique Epic Id e.g "C47E1".
+// DFOCVC does not include the capability Id it's attached too, but instead starts with "E000",
+// so you get key that look like "E00033" or "E00001" (the unique numbers are always 2 numbers e.g "01").
+// Supplier Defined have the Supplier Id, the Solution Id then a Unique Id e.g "S042X02E01"
+// where "S042" is the Supplier Id, "X01" is the Solution Id and "E01" is the unique key.
+/// Compression Logic for Each Type ///
+/// Normal
+// Normal just remove the Capability Id, so "C47E1" just becomes "E1", since they're tied to the capability in the URL e.g "C47E1E2E3E4".
+/// DFOCVC
+// DFOCVC epics gain a D on the end of their Id e.g "E00047D" so that it's not recombined with the capability Id server side
+// we then remove 3 of the zeros so "E00047D" would become "E47D".
+/// Supplier Defined
+// We strip all prepending zeros from the numbers, so "S042X01E01" becomes "S42X1E1".
+// next we change the E to an _ because E is used as a spit character for other epics, so "S42X1E1" becomes "S42X1_1".
+
+function EncodeNormalEpic(rawValue){
+    return rawValue.substring(rawValue.indexOf(EpicSplitCharacter));
+}
+
+function EncodeDFOCVCEpic(rawValue){
+    return EpicSplitCharacter + rawValue.substring(4) + DFOCVCMarker;
+}
+
+function EncodeSupplierDefinedEpic(rawValue){
+    return rawValue.replace("E0", '_').replace("S0", 'S').replace("X0", 'X');
+}
+
+function DecodeNormalEpic(capabilityId, encodedValue){
+    return capabilityId + EpicSplitCharacter + encodedValue;
+}
+
+function DecodeDFOCVCEpic(encodedValue){
+    return "E000" + encodedValue.substring(0,2);
+}
+
+function DecodeSupplierDefinedEpic(encodedValue){
+ return ("S0" + encodedValue ).replace('_',"E0").replace('X',"X0");
 }
