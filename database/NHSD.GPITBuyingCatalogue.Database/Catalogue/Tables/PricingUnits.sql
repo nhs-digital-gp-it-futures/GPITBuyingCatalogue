712282779e30f675426daf54df936899322e0f26
Colin Murphy <colin.murphy@bjss.com>
2021-09-30 11:01:21 +0100

    Refresh migration package
    Updates the SSIS package metadata and also reintroduces the Name column as it is used by the SSIS process to perform the lookup.

--- database/NHSD.GPITBuyingCatalogue.Database/Catalogue/Tables/PricingUnits.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Catalogue/Tables/PricingUnits.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE catalogue.PricingUnits
 (
     Id smallint IDENTITY(1, 1) NOT NULL,
+    [Name] nvarchar(20) NULL,
     TierName nvarchar(30) NULL,
     [Description] nvarchar(100) NOT NULL,
     [Definition] nvarchar(1000) NULL,
