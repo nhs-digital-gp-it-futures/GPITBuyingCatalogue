17280c50e9a199f8e51d1cb4893023bed43e16aa
MichaelRobinson-BJSS <57219787+MichaelRobinson-BJSS@users.noreply.github.com>
2021-09-30 17:12:12 +0100

    Feature/15028 add edit associated services auto tests (#414)
    * Add associated service tests for add and edit
    
    * Add E2E tests for add and edit associated services
    
    * Add further tests

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/Admin/AssociatedServices/AssociatedServices.cs
@@ -0,0 +1,16 @@
+﻿using NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.Common;
+using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.Admin.AssociatedServices
+{
+    public static class AssociatedServices
+    {
+        public static By EditLink => ByExtensions.DataTestId("edit-link");
+
+        public static By AddAssociatedServiceContinueButton => By.LinkText("Continue");
+
+        public static By AssociatedServicesTable => ByExtensions.DataTestId("associated-services-table");
+
+        public static By AssociatedServiceDashboardTable => ByExtensions.DataTestId("associated-service-dashboard-table");
+    }
+}
