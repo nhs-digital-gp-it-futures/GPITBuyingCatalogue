d0c10049bd3d9025bcfd3e2c3f9a7ae8bae62116
ClemClark258 <Clement.Clarke@bjss.com>
2021-10-21 13:19:15 +0100

    Task- 17068 - refactored SupplierManagement and  CatalogueSolutionsFilter script to confrom to Typescript standards

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/js/SupplierManagement.js
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/js/SupplierManagement.js
@@ -1,14 +1,22 @@
-window.onload = function () {
-    const inactiveSupplierCheckboxContainer = "inactive-supplier-checkbox";
-    const inactiveSupplierCheckbox = "ShowInactiveSuppliers";
-    document.getElementById(inactiveSupplierCheckboxContainer).style.display = 'block';
-    ChangeSupplierDisplayStyle('none');
-    const checkbox = document.getElementById(inactiveSupplierCheckbox);
-    checkbox.addEventListener("click", ChangeSupplierDisplay);
-};
-function ChangeSupplierDisplay() {
-    this.checked ? ChangeSupplierDisplayStyle('table-row') : ChangeSupplierDisplayStyle('none');
+class SupplierManagement {
+    constructor() {
+        this.SetSupplierEvents = () => {
+            const inactiveSupplierCheckboxContainer = "inactive-supplier-checkbox";
+            const inactiveSupplierCheckbox = "ShowInactiveSuppliers";
+            document.getElementById(inactiveSupplierCheckboxContainer).style.display = "block";
+            ChangeSupplierDisplayStyle("none");
+            const checkbox = document.getElementById(inactiveSupplierCheckbox);
+            checkbox.addEventListener("click", this.ChangeSupplierDisplay);
+        };
+    }
+    ChangeSupplierDisplay() {
+        this.checked ? ChangeSupplierDisplayStyle("table-row") : ChangeSupplierDisplayStyle("none");
+    }
 }
+window.onload = function () {
+    var supplierManagement = new SupplierManagement();
+    supplierManagement.SetSupplierEvents();
+};
 function ChangeSupplierDisplayStyle(style) {
     const inactiveSuppliers = document.getElementsByClassName("inactive");
     for (let i = 0; i < inactiveSuppliers.length; i++) {
