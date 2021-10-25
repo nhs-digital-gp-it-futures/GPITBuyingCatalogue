c5fb0cc9b2ff256dc098efc57a259d899b52553c
Lee Rogers <lee.rogers@bjss.com>
2021-10-25 12:53:45 +0100

    Add Standards
    Add Standards and Standards Capabilities Tables.
    
    Add Data Source for Standards and StandardCapabilities.
    
    Add Standard and StandardCapability EF Models.
    
    Add EF Core Configuration for Standard and StandardCapability.
    
    Add GetStandardsForSolution to SolutionsService

---
+++ database/NHSD.GPITBuyingCatalogue.Database/Catalogue/Tables/StandardsCapabilities.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [catalogue].[StandardsCapabilities]
+(
+    StandardId NVARCHAR(5) NOT NULL,
+    CapabilityId INT NOT NULL,
+    CONSTRAINT PK_StandardsCapabilities PRIMARY KEY (StandardId, CapabilityId),
+    CONSTRAINT FK_StandardsCapabilities_Standard FOREIGN KEY (StandardId) REFERENCES catalogue.Standards(Id) ON DELETE CASCADE,
+    CONSTRAINT FK_StandardsCapabilities_Capability FOREIGN KEY (CapabilityId) REFERENCES catalogue.Capabilities(Id) ON DELETE CASCADE,
+)
