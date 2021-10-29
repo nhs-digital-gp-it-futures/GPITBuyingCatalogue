0aa0df87ae814cc04ef92f3658641b1b27b430cb
ClemClark258 <Clement.Clarke@bjss.com>
2021-10-29 13:23:13 +0100

    Task-17068-Refactor client-side JS to TS Amendments based on  review comments for update-javescript-to-typescript branch

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/ts/SupplierManagement.ts
@@ -0,0 +1,36 @@
+﻿class SupplierManagement {
+
+    // a variable of type boolean
+    checked: boolean;
+
+    SetSupplierEvents = () => {
+        const inactiveSupplierCheckboxContainer: string = "inactive-supplier-checkbox";
+        const inactiveSupplierCheckbox: string = "ShowInactiveSuppliers";
+
+        document.getElementById(inactiveSupplierCheckboxContainer).style.display = "block";
+
+        ChangeSupplierDisplayStyle("none");
+
+        const checkbox: HTMLElement = document.getElementById(inactiveSupplierCheckbox);
+
+        checkbox.addEventListener("click", this.ChangeSupplierDisplay);
+    }
+
+    ChangeSupplierDisplay(): void {
+
+        this.checked ? ChangeSupplierDisplayStyle("table-row") : ChangeSupplierDisplayStyle("none");
+    }
+}
+
+window.onload = function (): void {
+    const supplierManagement: SupplierManagement = new SupplierManagement();
+    supplierManagement.SetSupplierEvents();
+};
+
+function ChangeSupplierDisplayStyle(style: string): void {
+    const inactiveSuppliers: HTMLCollectionOf<Element> = document.getElementsByClassName("inactive");
+    for (let i: number = 0; i < inactiveSuppliers.length; i++) {
+        const input: HTMLInputElement = inactiveSuppliers.item(i) as HTMLInputElement;
+        input.style.display = style;
+    }
+}
