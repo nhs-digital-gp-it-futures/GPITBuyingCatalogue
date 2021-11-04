55682636e081e9f2c24e76a74b3e661399913a17
Lee Rogers <lee.rogers@bjss.com>
2021-11-04 11:56:04 +0000

    fix alignment in SQL files

--- database/NHSD.GPITBuyingCatalogue.Database/Catalogue/Tables/Standards.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Catalogue/Tables/Standards.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [catalogue].[Standards]
 (
-	[Id] NVARCHAR(5) NOT NULL,
-	[Name] NVARCHAR(255) NOT NULL,
-	[Description] NVARCHAR(500) NOT NULL,
-	[Url] NVARCHAR(1000) NOT NULL,
-	StandardTypeId INT NOT NULL,
-	CONSTRAINT PK_Standards PRIMARY KEY (Id),
+    [Id] NVARCHAR(5) NOT NULL,
+    [Name] NVARCHAR(255) NOT NULL,
+    [Description] NVARCHAR(500) NOT NULL,
+    [Url] NVARCHAR(1000) NOT NULL,
+    StandardTypeId INT NOT NULL,
+    CONSTRAINT PK_Standards PRIMARY KEY (Id),
     CONSTRAINT FK_Standards_StandardType FOREIGN KEY (StandardTypeId) REFERENCES catalogue.StandardTypes(Id),
 );
