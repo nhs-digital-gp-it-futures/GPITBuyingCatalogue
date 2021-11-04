6fcd2e0f6c8e57ae747ebeb7e9bb58aabb219944
Colin Murphy <colin.murphy@bjss.com>
2021-11-04 15:52:19 +0000

    Update publication status validation for solutions

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/Admin/ManageCatalogueSolutionObjects.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/Admin/ManageCatalogueSolutionObjects.cs
@@ -8,5 +8,7 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.Admin
         internal static By CatalogueSolutionDashboardTable => ByExtensions.DataTestId("manage-solution-table");
 
         internal static By PublicationStatusInput => By.Id("manage-catalogue-solution");
+
+        internal static By PublicationStatusInputError => By.Id("manage-catalogue-solution-error");
     }
 }
