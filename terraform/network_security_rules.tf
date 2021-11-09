01c3613d74f85292d8a23b66d820e89565c08470
Steve Scott <steve.scott@bjss.com>
2021-11-09 11:09:56 +0000

    Update production firewall rules to allow public access

--- terraform/network_security_rules.tf
+++ terraform/network_security_rules.tf
@@ -1,3 +1,18 @@
+resource "azurerm_network_security_rule" "Public" {
+  name                        = "AllowPublicAccess"
+  count                       = local.shortenv == "production" ? 1 : 0 
+  resource_group_name         = azurerm_resource_group.app-gateway.name
+  network_security_group_name = azurerm_network_security_group.gateway.name
+  source_address_prefix       = "AzureCloud"
+  destination_address_prefix  = "*"
+  source_port_range           = "*"
+  destination_port_ranges     = [ "80", "443" ]
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  priority                    = 100
+  description                 = "Allow public access to this environment"
+}
 resource "azurerm_network_security_rule" "VPN_Access" {
   name                        = "AllowBjssVpn"
   resource_group_name         = azurerm_resource_group.app-gateway.name
