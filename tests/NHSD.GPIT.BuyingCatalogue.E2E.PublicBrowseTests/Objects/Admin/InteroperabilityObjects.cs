b48e13ae35b2f66ab393882223315a7ef6650083
PhysicsJon <j.r.alsop.18@gmail.com>
2021-11-15 11:19:23 +0000

    Add E2E tests for IM1 integrations

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/Admin/InteroperabilityObjects.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.Admin
+{
+    internal static class InteroperabilityObjects
+    {
+        internal static By SelectedIntegrationType => By.Id(nameof(SelectedIntegrationType));
+
+        internal static By SelectedProviderOrConsumer => By.Id(nameof(SelectedProviderOrConsumer));
+
+        internal static By IntegratesWith => By.Id(nameof(IntegratesWith));
+    }
+}
