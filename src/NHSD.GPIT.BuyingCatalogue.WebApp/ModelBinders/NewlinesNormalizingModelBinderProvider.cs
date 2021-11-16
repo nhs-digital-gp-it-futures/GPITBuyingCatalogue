d3867999d55df034a20952c811751452b464008a
MichaelRobinson-BJSS <57219787+MichaelRobinson-BJSS@users.noreply.github.com>
2021-11-16 12:21:49 +0000

    Feature/17230 stringlength issue (#523)
    * Fix carriage return new line stringlength issue
    
    * Fixes

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/ModelBinders/NewlinesNormalizingModelBinderProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.ModelBinders
+{
+    public class NewlinesNormalizingModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (context.Metadata.ModelType == typeof(string))
+                return new NewlinesNormalizingModelBinder();
+
+            return null;
+        }
+    }
+}
