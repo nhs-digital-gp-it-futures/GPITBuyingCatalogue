c21c56d51ec5f42c40d459f1633c491c4e2c8df2
Lee Rogers <lee.rogers@bjss.com>
2021-11-17 12:53:53 +0000

    Add Explicit Temportal Tables for Organisations Schema Tables

---
+++ database/NHSD.GPITBuyingCatalogue.Database/Organisations/TemporalTables/Organisations_History.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE organisations.Organisations_History
+(
+    Id INT NOT NULL,
+    [Name] nvarchar(255) NOT NULL,
+    [Address] nvarchar(max) NULL,
+    OdsCode nvarchar(8) NULL,
+    PrimaryRoleId nvarchar(8) NULL,
+    CatalogueAgreementSigned bit NOT NULL,
+    LastUpdated datetime2(7) NOT NULL,
+    LastUpdatedBy int NULL,
+    SysStartTime datetime2(0)NOT NULL,
+    SysEndTime datetime2(0) NOT NULL,
+);
+GO
+
+CREATE CLUSTERED COLUMNSTORE INDEX IX_Organisation_History ON organisations.Organisations_History;
+GO
+
+CREATE NONCLUSTERED INDEX IX_Organisations_History_Id_Period_Columns ON organisations.Organisations_History (SysEndTime, SysStartTime, Id);
---
+++ database/NHSD.GPITBuyingCatalogue.Database/Organisations/TemporalTables/RelatedOrganisations_History.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE organisations.RelatedOrganisations_History
+(
+    OrganisationId int NOT NULL,
+    RelatedOrganisationId int NOT NULL,
+    LastUpdated datetime2(7) NOT NULL,
+    LastUpdatedBy int NULL,
+    SysStartTime datetime2(0) NOT NULL,
+    SysEndTime datetime2(0) NOT NULL,
+);
+GO
+
+CREATE CLUSTERED COLUMNSTORE INDEX IX_RelatedOrganisations_History 
+ON organisations.RelatedOrganisations_History;
+GO
+
+CREATE NONCLUSTERED INDEX IX_RelatedOrganisations_History_OrganisationId_Period_Columns
+ON organisations.RelatedOrganisations_History (SysEndTime, SysStartTime, OrganisationId);
+GO
+
