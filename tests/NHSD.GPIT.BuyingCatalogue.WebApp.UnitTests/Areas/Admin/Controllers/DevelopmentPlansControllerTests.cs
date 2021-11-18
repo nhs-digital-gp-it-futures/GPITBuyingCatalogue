b6ab752cdce5437dd77d66d0be872ec0c9ccf911
Lee Rogers <lee.rogers@bjss.com>
2021-11-18 13:40:38 +0000

    Add Development Plans Unit Tests

--- tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Admin/Controllers/DevelopmentPlansControllerTests.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Admin/Controllers/DevelopmentPlansControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.Idioms;
@@ -10,6 +12,7 @@ using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
 using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.DevelopmentPlans;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Models.DevelopmentPlans;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Solutions;
 using NHSD.GPIT.BuyingCatalogue.Test.Framework.AutoFixtureCustomisations;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers;
@@ -70,6 +73,22 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Admin.Controllers
 
         [Theory]
         [CommonAutoData]
+        public static async Task Post_DevelopmentPlans_InvalidId_ReturnsBadRequestResult(
+            CatalogueItemId catalogueItemId,
+            DevelopmentPlanModel model,
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller)
+        {
+            mockService.Setup(s => s.GetSolution(catalogueItemId))
+                .ReturnsAsync(default(CatalogueItem));
+
+            var actual = (await controller.DevelopmentPlans(catalogueItemId, model)).As<BadRequestObjectResult>();
+
+            actual.Value.Should().Be($"No Solution found for Id: {catalogueItemId}");
+        }
+
+        [Theory]
+        [CommonAutoData]
         public static async Task Post_DevelopmentPlans_CallsSavesSolutionDevelopmentPlans(
             CatalogueItemId catalogueItemId,
             DevelopmentPlanModel model,
@@ -97,7 +116,7 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Admin.Controllers
 
         [Theory]
         [CommonAutoData]
-        public static async Task Post_DevelopmentPlans_InvalidId_ReturnsBadRequestResult(
+        public static async Task Post_DevelopmentPlans_InvalidId_ExpectedViewModel(
             Solution solution,
             CatalogueItemId catalogueItemId,
             [Frozen] Mock<ISolutionsService> mockService,
@@ -106,16 +125,404 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Admin.Controllers
             mockService.Setup(s => s.GetSolution(catalogueItemId))
                 .ReturnsAsync(solution.CatalogueItem);
 
-            var expected = new DevelopmentPlanModel(solution.CatalogueItem)
-            {
-                BackLink = "testUrl",
-            };
+            var expected = new DevelopmentPlanModel(solution.CatalogueItem);
 
             var actual = (await controller.DevelopmentPlans(catalogueItemId)).As<ViewResult>();
 
             mockService.Verify(s => s.GetSolution(catalogueItemId));
             actual.ViewName.Should().BeNull();
-            actual.Model.Should().BeEquivalentTo(expected);
+            actual.Model.Should().BeEquivalentTo(expected, opt => opt.Excluding(m => m.BackLink));
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_AddWorkOffPlan_InvalidId_ReturnsBadRequestResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            CatalogueItemId id)
+        {
+            mockService.Setup(s => s.GetSolution(id))
+                .ReturnsAsync(default(CatalogueItem));
+
+            var actual = (await controller.DevelopmentPlans(id)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Solution found for Id: {id}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_AddWorkOffPlan_ValidSolutionForId_ReturnsExpectedViewResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution,
+            List<Standard> standards)
+        {
+            var expectedModel = new EditWorkOffPlanModel(solution.CatalogueItem, standards);
+
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            mockService.Setup(s => s.GetSolutionStandardsForEditing(solution.CatalogueItemId))
+                .ReturnsAsync(standards);
+
+            var actual = (await controller.AddWorkOffPlan(solution.CatalogueItemId)).As<ViewResult>();
+
+            actual.Should().NotBeNull();
+            actual.ViewName.Should().NotBeNull();
+
+            actual.Model.Should().BeEquivalentTo(expectedModel, opt => opt.Excluding(m => m.BackLink));
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_AddWorkOffPlan_InvalidId_ReturnsBadRequestResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            EditWorkOffPlanModel model,
+            CatalogueItemId id)
+        {
+            mockService.Setup(s => s.GetSolution(id))
+                .ReturnsAsync(default(CatalogueItem));
+
+            var actual = (await controller.AddWorkOffPlan(id, model)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Solution found for Id: {id}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_AddWorkOffPlan_ValidIdForSolution_CallsSaveWorkOffPlans(
+            [Frozen] Mock<ISolutionsService> mockService,
+            [Frozen] Mock<IDevelopmentPlansService> mockDPService,
+            DevelopmentPlansController controller,
+            Solution solution)
+        {
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var model = new EditWorkOffPlanModel()
+            {
+                SolutionId = solution.CatalogueItemId,
+                SelectedStandard = "Standard",
+                Details = "Details",
+                Day = "1",
+                Month = "1",
+                Year = "2111",
+            };
+
+            await controller.AddWorkOffPlan(solution.CatalogueItemId, model);
+
+            mockDPService.Verify(dp => dp.SaveWorkOffPlan(solution.CatalogueItemId, It.IsAny<SaveWorkOffPlanModel>()));
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_AddWorkOffPlan_ValidIdForSolution_RedirectsToDevelopmentPlans(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution)
+        {
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var model = new EditWorkOffPlanModel()
+            {
+                SolutionId = solution.CatalogueItemId,
+                SelectedStandard = "Standard",
+                Details = "Details",
+                Day = "1",
+                Month = "1",
+                Year = "2111",
+            };
+
+            var actual = (await controller.AddWorkOffPlan(solution.CatalogueItemId, model)).As<RedirectToActionResult>();
+
+            actual.ActionName.Should().Be(nameof(DevelopmentPlansController.DevelopmentPlans));
+            actual.RouteValues["solutionId"].Should().Be(solution.CatalogueItemId);
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_EditWorkOffPlan_InvalidId_ReturnsBadRequestResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            CatalogueItemId id)
+        {
+            mockService.Setup(s => s.GetSolution(id))
+                .ReturnsAsync(default(CatalogueItem));
+
+            var actual = (await controller.EditWorkOffPlan(id, 1)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Solution found for Id: {id}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_EditWorkOffPlan_InvalidWorkOffPlanId_ReturnsBadRequest(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution,
+            int workOffPlanId)
+        {
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var actual = (await controller.EditWorkOffPlan(solution.CatalogueItemId, workOffPlanId)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Work-off Plan item found for Id: {workOffPlanId}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_EditWorkOffPlan_ValidSolutionForId_ReturnsExpectedViewResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution,
+            List<Standard> standards)
+        {
+            var workOffPlan = solution.WorkOffPlans.FirstOrDefault();
+
+            var expectedModel = new EditWorkOffPlanModel(solution.CatalogueItem, standards, workOffPlan);
+
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            mockService.Setup(s => s.GetSolutionStandardsForEditing(solution.CatalogueItemId))
+                .ReturnsAsync(standards);
+
+            var actual = (await controller.EditWorkOffPlan(solution.CatalogueItemId, workOffPlan.Id)).As<ViewResult>();
+
+            actual.Should().NotBeNull();
+            actual.ViewName.Should().BeNull();
+
+            actual.Model.Should().BeEquivalentTo(expectedModel, opt => opt.Excluding(m => m.BackLink));
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_EditWorkOffPlan_InvalidId_ReturnsBadRequestResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            EditWorkOffPlanModel model,
+            CatalogueItemId id,
+            int workOffPlanId)
+        {
+            mockService.Setup(s => s.GetSolution(id))
+                .ReturnsAsync(default(CatalogueItem));
+
+            var actual = (await controller.EditWorkOffPlan(id, workOffPlanId, model)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Solution found for Id: {id}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_EditWorkOffPlan_InvalidWorkOffPlanId_ReturnsBadRequest(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            EditWorkOffPlanModel model,
+            Solution solution,
+            int workOffPlanId)
+        {
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var actual = (await controller.EditWorkOffPlan(solution.CatalogueItemId, workOffPlanId, model)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Work-off Plan item found for Id: {workOffPlanId}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_EditWorkOffPlan_ValidIdForSolution_CallsSaveWorkOffPlans(
+            [Frozen] Mock<ISolutionsService> mockService,
+            [Frozen] Mock<IDevelopmentPlansService> mockDPService,
+            DevelopmentPlansController controller,
+            Solution solution)
+        {
+            var workOffPlan = solution.WorkOffPlans.FirstOrDefault();
+
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var model = new EditWorkOffPlanModel()
+            {
+                SolutionId = solution.CatalogueItemId,
+                SelectedStandard = "Standard",
+                Details = "Details",
+                Day = "1",
+                Month = "1",
+                Year = "2111",
+            };
+
+            await controller.EditWorkOffPlan(solution.CatalogueItemId, workOffPlan.Id, model);
+
+            mockDPService.Verify(dp => dp.UpdateWorkOffPlan(workOffPlan.Id, It.IsAny<SaveWorkOffPlanModel>()));
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_EditWorkOffPlan_ValidIdForSolution_RedirectsToDevelopmentPlans(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution)
+        {
+            var workOffPlan = solution.WorkOffPlans.FirstOrDefault();
+
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var model = new EditWorkOffPlanModel()
+            {
+                SolutionId = solution.CatalogueItemId,
+                SelectedStandard = "Standard",
+                Details = "Details",
+                Day = "1",
+                Month = "1",
+                Year = "2111",
+            };
+
+            var actual = (await controller.EditWorkOffPlan(solution.CatalogueItemId, workOffPlan.Id, model)).As<RedirectToActionResult>();
+
+            actual.ActionName.Should().Be(nameof(DevelopmentPlansController.DevelopmentPlans));
+            actual.RouteValues["solutionId"].Should().Be(solution.CatalogueItemId);
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_DeleteWorkOffPlan_InvalidId_ReturnsBadRequestResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            CatalogueItemId id)
+        {
+            mockService.Setup(s => s.GetSolution(id))
+                .ReturnsAsync(default(CatalogueItem));
+
+            var actual = (await controller.DeleteWorkOffPlan(id, 1)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Solution found for Id: {id}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_DeleteWorkOffPlan_InvalidWorkOffPlanId_ReturnsBadRequest(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution,
+            int workOffPlanId)
+        {
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var actual = (await controller.DeleteWorkOffPlan(solution.CatalogueItemId, workOffPlanId)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Work-off Plan item found for Id: {workOffPlanId}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Get_DeleteWorkOffPlan_ValidSolutionForId_ReturnsExpectedViewResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution)
+        {
+            var workOffPlan = solution.WorkOffPlans.FirstOrDefault();
+
+            var expectedModel = new EditWorkOffPlanModel(solution.CatalogueItem, workOffPlan);
+
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var actual = (await controller.DeleteWorkOffPlan(solution.CatalogueItemId, workOffPlan.Id)).As<ViewResult>();
+
+            actual.Should().NotBeNull();
+            actual.ViewName.Should().BeNull();
+
+            actual.Model.Should().BeEquivalentTo(expectedModel, opt => opt.Excluding(m => m.BackLink));
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_DeleteWorkOffPlan_InvalidId_ReturnsBadRequestResult(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            EditWorkOffPlanModel model,
+            CatalogueItemId id,
+            int workOffPlanId)
+        {
+            mockService.Setup(s => s.GetSolution(id))
+                .ReturnsAsync(default(CatalogueItem));
+
+            var actual = (await controller.DeleteWorkOffPlan(id, workOffPlanId, model)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Solution found for Id: {id}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_DeleteWorkOffPlan_InvalidWorkOffPlanId_ReturnsBadRequest(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            EditWorkOffPlanModel model,
+            Solution solution,
+            int workOffPlanId)
+        {
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var actual = (await controller.DeleteWorkOffPlan(solution.CatalogueItemId, workOffPlanId, model)).As<BadRequestObjectResult>();
+
+            actual.Should().NotBeNull();
+            actual.Value.Should().Be($"No Work-off Plan item found for Id: {workOffPlanId}");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_DeleteWorkOffPlan_ValidIdForSolution_CallsSaveWorkOffPlans(
+            [Frozen] Mock<ISolutionsService> mockService,
+            [Frozen] Mock<IDevelopmentPlansService> mockDPService,
+            DevelopmentPlansController controller,
+            Solution solution)
+        {
+            var workOffPlan = solution.WorkOffPlans.FirstOrDefault();
+
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var model = new EditWorkOffPlanModel();
+
+            await controller.DeleteWorkOffPlan(solution.CatalogueItemId, workOffPlan.Id, model);
+
+            mockDPService.Verify(dp => dp.DeleteWorkOffPlan(workOffPlan.Id));
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static async Task Post_DeleteWorkOffPlan_ValidIdForSolution_RedirectsToDevelopmentPlans(
+            [Frozen] Mock<ISolutionsService> mockService,
+            DevelopmentPlansController controller,
+            Solution solution)
+        {
+            var workOffPlan = solution.WorkOffPlans.FirstOrDefault();
+
+            mockService.Setup(s => s.GetSolution(solution.CatalogueItemId))
+                .ReturnsAsync(solution.CatalogueItem);
+
+            var model = new EditWorkOffPlanModel();
+
+            var actual = (await controller.DeleteWorkOffPlan(solution.CatalogueItemId, workOffPlan.Id, model)).As<RedirectToActionResult>();
+
+            actual.ActionName.Should().Be(nameof(DevelopmentPlansController.DevelopmentPlans));
+            actual.RouteValues["solutionId"].Should().Be(solution.CatalogueItemId);
         }
     }
 }
