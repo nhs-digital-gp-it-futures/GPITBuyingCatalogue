e3997fa6ad1a090636c034bd78a752a0d7970b92
Colin Murphy <colin.murphy@bjss.com>
2021-12-21 16:10:53 +0000

    Triage pages

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Order/Models/OrderTriage/GenericOrderTriageModel.cs
@@ -0,0 +1,9 @@
+﻿using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Order.Models.OrderTriage
+{
+    public class GenericOrderTriageModel : NavBaseModel
+    {
+        public string OrdersDashboardLink { get; set; }
+    }
+}
---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Order/Models/OrderTriage/TriageDueDiligenceModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Order.Models.OrderTriage
+{
+    public class TriageDueDiligenceModel : NavBaseModel
+    {
+        public string Title { get; set; }
+
+        public string Advice { get; set; }
+
+        public bool? Selected { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListItems => new[]
+        {
+            new SelectListItem(true.ToYesNo(), true.ToString()),
+            new SelectListItem(false.ToYesNo(), false.ToString()),
+        };
+    }
+}
