0a53839bffa78779057946a03f06a61578dff1bd
Lee Rogers <lee.rogers@bjss.com>
2022-01-13 16:56:32 +0000

    Add Autocomplete View Component
    Add Autocomplete view component and required JS/SASS
    
    Add Accessible-Autocomplete node Package and JS to packages.
    
    Add AutocompleteResult model for the returned JSON
    
    Add SASS Watch to Gulpfile.js

---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/NhsAutocomplete/NhsAutocompleteViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.Autocomplete
+{
+    public sealed class NhsAutocompleteViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string id, string ajaxUrl, string queryParameterName, string placeholderText)
+        {
+            var model = new NhsAutocompleteModel
+            {
+                Id = id,
+                AjaxUrl = ajaxUrl,
+                QueryParameterName = queryParameterName,
+                PlaceholderText = placeholderText,
+            };
+
+            return await Task.FromResult(View("NhsAutocomplete", model));
+        }
+    }
+}
---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Models/Autocomplete/AutocompleteResult.cs
@@ -0,0 +1,11 @@
+﻿namespace NHSD.GPIT.BuyingCatalogue.WebApp.Models.Autocomplete
+{
+    public sealed class AutocompleteResult
+    {
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Url { get; set; }
+    }
+}
