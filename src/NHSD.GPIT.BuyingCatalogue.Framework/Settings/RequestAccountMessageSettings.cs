db2bcfee0fcf0fbbba5d1936ab78ac6ec8f49b46
Magnus John <magnus.john@bjss.com>
2022-01-17 13:05:34 +0000

    Request an account form

---
+++ src/NHSD.GPIT.BuyingCatalogue.Framework/Settings/RequestAccountMessageSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Email;
+
+namespace NHSD.GPIT.BuyingCatalogue.Framework.Settings
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class RequestAccountMessageSettings
+    {
+        public string AdminTemplateId { get; set; }
+
+        public string UserTemplateId { get; set; }
+
+        public EmailAddressTemplate AdminRecipient { get; set; }
+    }
+}
