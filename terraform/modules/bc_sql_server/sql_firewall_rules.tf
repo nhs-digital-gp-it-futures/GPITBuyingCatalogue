0e84f8a3b34f69da6d2d92d23083b378591d23de
Steve Scott <steve.scott@bjss.com>
2022-02-01 10:30:56 +0000

    Terraform changes to support DR environment

--- terraform/modules/bc_sql_server/sql_firewall_rules.tf
+++ terraform/modules/bc_sql_server/sql_firewall_rules.tf
@@ -1,6 +1,7 @@
 #SQL Firewall rule to allow internal Azure Services to connect to DB
 resource "azurerm_sql_firewall_rule" "sql_azure_services" {
   name                = "azure_services"
+  count               = var.environment != "dr" 
   resource_group_name = var.rg_name
   server_name         = azurerm_sql_server.sql_server.name
   start_ip_address    = "0.0.0.0"
@@ -9,6 +10,7 @@ resource "azurerm_sql_firewall_rule" "sql_azure_services" {
 
 resource "azurerm_sql_firewall_rule" "sql_bjss_vpn" {
   name                = "AllowBjssVpn"
+  count               = var.environment != "dr" 
   resource_group_name = var.rg_name
   server_name         = azurerm_sql_server.sql_server.name
   start_ip_address    = var.bjssvpn
---
+++ terraform/virtual_network_peering.tf
@@ -0,0 +1,11 @@
+data "azurerm_virtual_network" "primary_virtualnet" {
+  name                = "primary-virtualnet"
+  resource_group_name = "${var.project}-${var.primaryEnv}-rg-virtualnet"
+}
+
+resource "azurerm_virtual_network_peering" "secondary-to-primary-vnet" {
+  name                      = "${var.project}-${var.environment}-vnet-peer"
+  resource_group_name       = azurerm_resource_group.virtualnet.name
+  virtual_network_name      = azurerm_virtual_network.virtualnet.name
+  remote_virtual_network_id = azurerm_virtual_network.primary_virtualnet.id
+}
