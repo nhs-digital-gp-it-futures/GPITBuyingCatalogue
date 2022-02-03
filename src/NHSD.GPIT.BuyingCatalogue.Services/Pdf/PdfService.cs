7788209b6672fa6e5244ffa91e6941a1d814ab2d
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2022-02-03 16:09:25 +0000

    Temp change to see if chrome is completing in build agent

--- src/NHSD.GPIT.BuyingCatalogue.Services/Pdf/PdfService.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Services/Pdf/PdfService.cs
@@ -21,12 +21,25 @@ namespace NHSD.GPIT.BuyingCatalogue.Services.Pdf
             var process = Process.Start(psi);
             process.WaitForExit(60000);
 
-            byte[] fileContent = File.ReadAllBytes(filePath);
-
             if (File.Exists(filePath))
-                File.Delete(filePath);
+            {
+                byte[] fileContent = File.ReadAllBytes(filePath);
 
-            return fileContent;
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+
+                return fileContent;
+            }
+            else
+            {
+                var rnd = new Random();
+                var b = new byte[10];
+#pragma warning disable CA5394 // Do not use insecure randomness
+                rnd.NextBytes(b);
+#pragma warning restore CA5394 // Do not use insecure randomness
+
+                return b;
+            }
         }
     }
 }
