162f2065673f541fd88965e0fdc83e4c94b805a0
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-02-04 11:06:08 +0000

    Migrate remaining emails to gov notify

--- src/NHSD.GPIT.BuyingCatalogue.Framework/Settings/OrderMessageSettings.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Framework/Settings/OrderMessageSettings.cs
@@ -6,8 +6,6 @@ namespace NHSD.GPIT.BuyingCatalogue.Framework.Settings
     [ExcludeFromCodeCoverage]
     public sealed class OrderMessageSettings
     {
-        public EmailMessageTemplate EmailMessageTemplate { get; set; }
-
         public string SingleCsvTemplateId { get; set; }
 
         public string DualCsvTemplateId { get; set; }
