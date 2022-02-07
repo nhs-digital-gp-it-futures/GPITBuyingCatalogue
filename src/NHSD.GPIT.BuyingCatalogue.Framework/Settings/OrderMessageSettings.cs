1d78872108f666826fce2538d4965b47f126bc3f
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-02-07 13:41:15 +0000

    Spike/gov notify platform (#624)

--- src/NHSD.GPIT.BuyingCatalogue.Framework/Settings/OrderMessageSettings.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Framework/Settings/OrderMessageSettings.cs
@@ -6,7 +6,9 @@ namespace NHSD.GPIT.BuyingCatalogue.Framework.Settings
     [ExcludeFromCodeCoverage]
     public sealed class OrderMessageSettings
     {
-        public EmailMessageTemplate EmailMessageTemplate { get; set; }
+        public string SingleCsvTemplateId { get; set; }
+
+        public string DualCsvTemplateId { get; set; }
 
         public EmailAddressTemplate Recipient { get; set; }
     }
