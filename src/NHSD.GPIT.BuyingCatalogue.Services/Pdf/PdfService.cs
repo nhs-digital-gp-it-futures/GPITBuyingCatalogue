b5948b49123b2a74fdda8114522884ff870ea4a6
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2022-02-08 13:25:44 +0000

    Refactored

--- src/NHSD.GPIT.BuyingCatalogue.Services/Pdf/PdfService.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Services/Pdf/PdfService.cs
@@ -11,8 +11,36 @@ namespace NHSD.GPIT.BuyingCatalogue.Services.Pdf
     {
         public async Task<byte[]> Convert(Uri url)
         {
-            string outputFilePath = @$"{Path.GetTempPath()}{Guid.NewGuid()}.pdf";
-            string inputFilePath = @$"{Path.GetTempPath()}{Guid.NewGuid()}.html";
+            string htmlFilePath = string.Empty;
+            string pdfFilePath = string.Empty;
+
+            try
+            {
+                htmlFilePath = await ConvertToHtml(url);
+
+                pdfFilePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.pdf");
+
+                HtmlConverter.ConvertToPdf(
+                    new FileInfo(htmlFilePath),
+                    new FileInfo(pdfFilePath));
+
+                byte[] fileContent = File.ReadAllBytes(pdfFilePath);
+
+                return fileContent;
+            }
+            finally
+            {
+                if (!string.IsNullOrEmpty(pdfFilePath) && File.Exists(pdfFilePath))
+                    File.Delete(pdfFilePath);
+
+                if (!string.IsNullOrEmpty(htmlFilePath) && File.Exists(htmlFilePath))
+                    File.Delete(htmlFilePath);
+            }
+        }
+
+        private static async Task<string> ConvertToHtml(Uri url)
+        {
+            string htmlFilePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.html");
 
             using (var httpClientHandler = new HttpClientHandler
             {
@@ -22,30 +50,16 @@ namespace NHSD.GPIT.BuyingCatalogue.Services.Pdf
                 },
             })
             {
-                using (var httpClient = new HttpClient(httpClientHandler) { BaseAddress = url })
-                {
-                    var response = await httpClient.GetAsync(url);
-                    response.EnsureSuccessStatusCode();
-                    await using var ms = await response.Content.ReadAsStreamAsync();
-                    await using var fs = File.Create(inputFilePath);
-                    ms.Seek(0, SeekOrigin.Begin);
-                    await ms.CopyToAsync(fs);
-                }
+                using var httpClient = new HttpClient(httpClientHandler) { BaseAddress = url };
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                await using var ms = await response.Content.ReadAsStreamAsync();
+                await using var fs = File.Create(htmlFilePath);
+                ms.Seek(0, SeekOrigin.Begin);
+                await ms.CopyToAsync(fs);
             }
 
-            HtmlConverter.ConvertToPdf(
-                new FileInfo(inputFilePath),
-                new FileInfo(outputFilePath));
-
-            byte[] fileContent = File.ReadAllBytes(outputFilePath);
-
-            if (File.Exists(outputFilePath))
-                File.Delete(outputFilePath);
-
-            if (File.Exists(inputFilePath))
-                File.Delete(inputFilePath);
-
-            return fileContent;
+            return htmlFilePath;
         }
     }
 }
