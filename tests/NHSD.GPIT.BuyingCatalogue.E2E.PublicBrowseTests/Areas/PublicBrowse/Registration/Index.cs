c56163b3b3f57a54ddceaf66dad77bed51b87c4f
Magnus John <97523078+magnusjohn-bjss@users.noreply.github.com>
2022-02-08 16:50:53 +0000

    Request an account form (#629)

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/PublicBrowse/Registration/Index.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.Common;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.PublicBrowse;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils.TestBases;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Identity.Controllers;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Controllers;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Areas.PublicBrowse.Registration
+{
+    public sealed class Index : AnonymousTestBase, IClassFixture<LocalWebApplicationFactory>
+    {
+        public Index(LocalWebApplicationFactory factory)
+            : base(factory, typeof(RegistrationController), nameof(RegistrationController.Index), null)
+        {
+        }
+
+        [Fact]
+        public void Index_AllSectionsDisplayed()
+        {
+            CommonActions.GoBackLinkDisplayed().Should().BeTrue();
+            CommonActions.ElementIsDisplayed(CommonSelectors.Header1).Should().BeTrue();
+            CommonActions.ElementIsDisplayed(RegistrationObjects.RequestAnAccountLink).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Index_ClickGoBackLink_ExpectedResult()
+        {
+            CommonActions.ClickGoBackLink();
+
+            CommonActions.PageLoadedCorrectGetIndex(
+                typeof(HomeController),
+                nameof(HomeController.Index)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Index_ClickNominateOrganisationLink_ExpectedResult()
+        {
+            CommonActions.ClickLinkElement(RegistrationObjects.RequestAnAccountLink);
+
+            CommonActions.PageLoadedCorrectGetIndex(
+                typeof(RegistrationController),
+                nameof(RegistrationController.Details)).Should().BeTrue();
+        }
+    }
+}
