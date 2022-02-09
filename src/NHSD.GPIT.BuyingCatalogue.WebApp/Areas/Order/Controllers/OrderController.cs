23f580892ca2e6f19a0eb3380b257129611df7e3
Steve Scott <steve.scott@bjss.com>
2022-02-09 12:02:28 +0000

    Potential fix for PDF rendering

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Order/Controllers/OrderController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Order/Controllers/OrderController.cs
@@ -216,7 +216,7 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Order.Controllers
             }
             else
             {
-                url = $"https://localhost{url}";
+                url = $"http://localhost{url}";
             }
 
             var result = pdfService.Convert(new Uri(url));
