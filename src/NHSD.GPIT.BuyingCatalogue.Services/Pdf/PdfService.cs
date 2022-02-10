9cd109d5374974f3a1f100fd851bec1855d8e23e
MichaelRobinson-BJSS <michael.robinson@bjss.com>
2022-02-10 14:25:51 +0000

    Added Chrome 64bit support

--- src/NHSD.GPIT.BuyingCatalogue.Services/Pdf/PdfService.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Services/Pdf/PdfService.cs
@@ -9,13 +9,23 @@ namespace NHSD.GPIT.BuyingCatalogue.Services.Pdf
     public sealed class PdfService : IPdfService
     {
         private const string ChromeArgs = "--no-sandbox --headless --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --ignore-certificate-errors";
-        private const string ChromeWindowsPath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+        private const string ChromeWindows32BitPath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+        private const string ChromeWindows64BitPath = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
         private const string ChromeLinuxPath = "/usr/bin/chromium-browser";
 
         public byte[] Convert(Uri url)
         {
             string filePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.pdf");
-            string chromePath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ChromeWindowsPath : ChromeLinuxPath;
+            string chromePath;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                chromePath = File.Exists(ChromeWindows64BitPath) ? ChromeWindows64BitPath : ChromeWindows32BitPath;
+            }
+            else
+            {
+                chromePath = ChromeLinuxPath;
+            }
 
             try
             {
