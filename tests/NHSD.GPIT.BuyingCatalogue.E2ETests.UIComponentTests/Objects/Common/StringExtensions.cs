5f1ea367de07c9b76b9dffb325b58120224aa803
Oke Anyatonwu <Oke.Anyatonwu@bjss.com>
2022-02-15 15:50:35 +0000

    Removed whitespace StringExtension

--- tests/NHSD.GPIT.BuyingCatalogue.E2ETests.UIComponentTests/Objects/Common/StringExtensions.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2ETests.UIComponentTests/Objects/Common/StringExtensions.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace NHSD.GPIT.BuyingCatalogue.E2ETests.UIComponentTests.Objects.Common
 {
     public static class StringExtensions
     {
-       
         public static string FormatForComparison(this string value) =>
             new(value.Where(c => !char.IsWhiteSpace(c)).ToArray());
-
-       
     }
 }
