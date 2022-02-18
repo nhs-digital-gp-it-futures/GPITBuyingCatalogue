445914f658153fc4ae281f8d431dcbf0a7149ff3
Magnus John <97523078+magnusjohn-bjss@users.noreply.github.com>
2022-02-18 14:40:48 +0000

    Procurement hub contact form (#677)

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/PublicBrowse/ProcurementHubObjects.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.PublicBrowse
+{
+    internal static class ProcurementHubObjects
+    {
+        internal static By FullNameInput => By.Id("FullName");
+
+        internal static By FullNameError => By.Id("FullName-error");
+
+        internal static By EmailAddressInput => By.Id("EmailAddress");
+
+        internal static By EmailAddressError => By.Id("EmailAddress-error");
+
+        internal static By OrganisationNameInput => By.Id("OrganisationName");
+
+        internal static By OrganisationNameError => By.Id("OrganisationName-error");
+
+        internal static By OdsCodeInput => By.Id("OdsCode");
+
+        internal static By QueryInput => By.Id("Query");
+
+        internal static By QueryError => By.Id("Query-error");
+
+        internal static By HasReadPrivacyPolicyError => By.Id("procurement-hub-details-error");
+    }
+}
