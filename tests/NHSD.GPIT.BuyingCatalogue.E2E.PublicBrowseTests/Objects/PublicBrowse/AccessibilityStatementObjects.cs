4dbfbe11246a60c61dbd999205097c0be720fd14
Magnus John <magnus.john@bjss.com>
2022-02-28 11:56:14 +0000

    New accessibility statement

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/PublicBrowse/AccessibilityStatementObjects.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.PublicBrowse
+{
+    internal static class AccessibilityStatementObjects
+    {
+        public static By ContactUsLink => By.LinkText("Contact the Buying Catalogue Team");
+    }
+}
