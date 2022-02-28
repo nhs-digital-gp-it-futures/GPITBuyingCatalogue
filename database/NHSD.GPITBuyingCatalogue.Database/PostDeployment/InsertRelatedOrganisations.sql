967513230187ef72ce263075452afe489d6401eb
MichaelRobinson-BJSS <57219787+MichaelRobinson-BJSS@users.noreply.github.com>
2022-02-28 10:58:23 +0000

    Final ODS Code changes (#707)

--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertRelatedOrganisations.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertRelatedOrganisations.sql
@@ -1,8 +1,8 @@
 ﻿IF '$(INSERT_TEST_DATA)' = 'True'
 AND NOT EXISTS ( SELECT * FROM organisations.RelatedOrganisations )
 BEGIN
-    DECLARE @northLincsOrgId INT = (SELECT Id FROM organisations.Organisations WHERE PrimaryRoleId = 'RO98' AND OdsCode = '03K');
-    DECLARE @hullOrgId INT = (SELECT Id FROM organisations.Organisations WHERE PrimaryRoleId = 'RO98' AND OdsCode = '03F');
+    DECLARE @northLincsOrgId INT = (SELECT Id FROM organisations.Organisations WHERE PrimaryRoleId = 'RO98' AND ExternalIdentifier = '03K');
+    DECLARE @hullOrgId INT = (SELECT Id FROM organisations.Organisations WHERE PrimaryRoleId = 'RO98' AND ExternalIdentifier = '03F');
     DECLARE @userId INT = (SELECT Id FROM users.AspNetUsers WHERE Email = N'BobSmith@email.com');
 
     INSERT organisations.RelatedOrganisations ( OrganisationId, RelatedOrganisationId, LastUpdated, LastUpdatedBy )
