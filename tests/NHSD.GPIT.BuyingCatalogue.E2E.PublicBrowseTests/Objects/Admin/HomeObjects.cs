cff096fba6274e373c7331cce096356e89291170
Magnus John <magnus.john@bjss.com>
2022-03-04 17:03:42 +0000

    Admin homepage & banner menu

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Objects/Admin/HomeObjects.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Objects.Admin
+{
+    internal static class HomeObjects
+    {
+        internal static By ManageCatalogueSolutionsLink => By.LinkText("Manage Catalogue Solutions");
+
+        internal static By ManageSupplierDefinedEpicsLink => By.LinkText("Manage supplier defined Epics");
+
+        internal static By ManageBuyerOrganisationsLink => By.LinkText("Manage buyer organisations");
+
+        internal static By ManageSupplierOrganisationsLink => By.LinkText("Manage supplier organisations");
+
+        internal static By ManageAllUsersLink => By.LinkText("Manage all users");
+
+        internal static By ManageAllOrdersLink => By.LinkText("Manage all orders");
+    }
+}
