d1c4ca3bacb5d368114c010108bbfc2e7e601cff
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-03-09 15:45:22 +0000

    Manage Orders

---
+++ src/NHSD.GPIT.BuyingCatalogue.ServiceContracts/Orders/IOrderAdminService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Models;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Models.AdminManageOrders;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Models.FilterModels;
+
+namespace NHSD.GPIT.BuyingCatalogue.ServiceContracts.Orders
+{
+    public interface IOrderAdminService
+    {
+        Task<Order> GetOrder(CallOffId callOffId);
+
+        Task<PagedList<AdminManageOrder>> GetPagedOrders(PageOptions options, string search = null);
+
+        Task<IList<SearchFilterModel>> GetOrdersBySearchTerm(string search);
+    }
+}
