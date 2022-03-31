1e220098ccc38cbdd4d94c9be6ddc2e899065138
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-03-31 15:44:02 +0100

    Remove redundant attribute

--- src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Catalogue/Models/TimeUnit.cs
+++ src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Catalogue/Models/TimeUnit.cs
@@ -7,12 +7,10 @@ namespace NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models
     {
         [Display(Name = "month")]
         [Description("per month")]
-        [AmountInYear(12)]
         PerMonth = 1,
 
         [Display(Name = "year")]
         [Description("per year")]
-        [AmountInYear(1)]
         PerYear = 2,
     }
 }
---
+++ src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EnumsNET;
+
+namespace NHSD.GPIT.BuyingCatalogue.EntityFramework.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string Description<TEnum>(this TEnum value)
+            where TEnum : struct, Enum => value.AsString(EnumFormat.Description);
+
+        public static string Name<TEnum>(this TEnum value)
+            where TEnum : struct, Enum => value.AsString(EnumFormat.DisplayName);
+
+        public static string EnumMemberName<TEnum>(this TEnum value)
+            where TEnum : struct, Enum => value.ToString();
+    }
+}
