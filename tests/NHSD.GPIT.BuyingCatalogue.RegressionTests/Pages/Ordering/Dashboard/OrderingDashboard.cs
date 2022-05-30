0752cd0b1902ddfeccd997167483894fa047d0f4
Lee Rogers <lee.rogers@bjss.com>
2022-05-30 14:13:34 +0100

    Initial Commit of Building Blocks

---
+++ tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Pages/Ordering/Dashboard/OrderingDashboard.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Actions.Common;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.Ordering;
+using NHSD.GPIT.BuyingCatalogue.RegressionTests.Utils;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Order.Controllers;
+using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.RegressionTests.Pages.Ordering.Dashboard
+{
+    public sealed class OrderingDashboard : PageBase
+    {
+        public OrderingDashboard(IWebDriver driver, CommonActions commonActions) : base(driver,commonActions)
+        {
+        }
+
+        public void CreateNewOrder()
+        {
+            CommonActions.ClickLinkElement(OrganisationDashboard.CreateOrderLink);
+
+            CommonActions.PageLoadedCorrectGetIndex(
+                typeof(OrderTriageController),
+                nameof(OrderTriageController.OrderItemType)).Should().BeTrue();
+        }
+    }
+}
