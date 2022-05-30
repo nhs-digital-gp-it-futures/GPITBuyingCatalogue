d2e0efb9b81a3df2eab4dc9c33a5644006647cd0
Lee Rogers <lee.rogers@bjss.com>
2022-05-30 14:13:34 +0100

    Initial Commit of Building Blocks

---
+++ tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Utils/PageBase.cs
@@ -0,0 +1,18 @@
+﻿using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Actions.Common;
+using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.RegressionTests.Utils
+{
+    public class PageBase
+    {
+        public PageBase(IWebDriver driver, CommonActions commonActions)
+        {
+            CommonActions = commonActions;
+            Driver = driver;
+        }
+
+        internal CommonActions CommonActions { get; }
+
+        internal IWebDriver Driver { get; private set; }
+    }
+}
