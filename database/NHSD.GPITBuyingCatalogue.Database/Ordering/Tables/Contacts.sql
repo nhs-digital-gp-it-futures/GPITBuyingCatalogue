91dc57b5579b093f36a5b651bff6968fbe93084a
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-06-07 11:23:35 +0100

    Re-add SupplierContactId but without constraints (#810)

--- database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/Contacts.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/Contacts.sql
@@ -1,16 +1,17 @@
 ﻿CREATE TABLE ordering.Contacts
 (
     Id int IDENTITY(1, 1) NOT NULL,
-    FirstName nvarchar(100) NULL, 
+    FirstName nvarchar(100) NULL,
     LastName nvarchar(100) NULL,
-    Email nvarchar(256) NULL, 
+    Email nvarchar(256) NULL,
     Phone nvarchar(35) NULL,
     LastUpdated datetime2(7) DEFAULT GETUTCDATE() NOT NULL,
     LastUpdatedBy int NULL,
     SysStartTime datetime2(0) GENERATED ALWAYS AS ROW START NOT NULL,
     SysEndTime datetime2(0) GENERATED ALWAYS AS ROW END NOT NULL,
     PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime),
-    [Department] NVARCHAR(50) NULL, 
+    [Department] NVARCHAR(50) NULL,
+    [SupplierContactId] INT NULL,
     CONSTRAINT PK_Contacts PRIMARY KEY (Id),
     CONSTRAINT FK_Contacts_LastUpdatedBy FOREIGN KEY (LastUpdatedBy) REFERENCES users.AspNetUsers(Id)
 ) WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = ordering.Contacts_History));
--- database/NHSD.GPITBuyingCatalogue.Database/Ordering/TemporalTables/Contacts_History.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Ordering/TemporalTables/Contacts_History.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE ordering.Contacts_History
 (
     Id int NOT NULL,
-    FirstName nvarchar(100) NULL, 
+    FirstName nvarchar(100) NULL,
     LastName nvarchar(100) NULL,
-    Email nvarchar(256) NULL, 
+    Email nvarchar(256) NULL,
     Phone nvarchar(35) NULL,
     LastUpdated datetime2(7) NOT NULL,
     LastUpdatedBy int NULL,
     SysStartTime datetime2(0) NOT NULL,
-    SysEndTime datetime2(0) NOT NULL, 
-    [Department] NVARCHAR(50) NULL
+    SysEndTime datetime2(0) NOT NULL,
+    [Department] NVARCHAR(50) NULL,
+    [SupplierContactId] INT NULL,
 );
