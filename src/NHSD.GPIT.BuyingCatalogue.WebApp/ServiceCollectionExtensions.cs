cb3ead7c3be0b86dcf47c92161eca35facd27d57
Lee Rogers <lee.rogers@bjss.com>
2022-06-09 14:47:36 +0100

    Fix 400 on too many service recipients and fix slow service recipient loading
    Fix 400 on too many service recipients by bumping the Form ValueCountLimit from 1024 to 16384
    Fix exception handling on commencement date in orderitemrecipientsmodel causing huge slowdowns when the dates are null

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/ServiceCollectionExtensions.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
@@ -326,6 +327,14 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp
                         options.ValidatorOptions.DefaultClassLevelCascadeMode = FluentValidation.CascadeMode.Continue;
                         options.ValidatorOptions.DefaultRuleLevelCascadeMode = FluentValidation.CascadeMode.Stop;
                     }).AddSingleton<IValidatorInterceptor, FluentValidatorInterceptor>();
+        }
+
+        public static void ConfigureFormOptions(this IServiceCollection services)
+        {
+            services.Configure<FormOptions>(options =>
+            {
+                options.ValueCountLimit = 16384;
+            });
         }
 
         public static IServiceCollection AddHangFire(this IServiceCollection services)
