30d9452de525b4d9e487cf8dd04461840c37e92c
Magnus John <magnus.john@bjss.com>
2022-07-05 22:57:41 +0100

    Fix line ids on review pdf

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Extensions/TempDataExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Extensions
+{
+    public static class TempDataExtensions
+    {
+        public const string LineIdKey = "temp-data-line-id";
+
+        public static int NextLineId(this ITempDataDictionary tempData)
+        {
+            var output = (int)(tempData[LineIdKey] ?? 0);
+
+            tempData[LineIdKey] = ++output;
+
+            return output;
+        }
+    }
+}
