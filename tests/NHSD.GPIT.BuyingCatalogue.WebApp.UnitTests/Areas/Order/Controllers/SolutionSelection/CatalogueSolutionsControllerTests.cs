5a0fd7ec42224b651a3ae5c4e13c821d0e92e89f
Magnus John <magnus.john@bjss.com>
2022-07-05 16:59:10 +0100

    Additional test

--- tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Controllers/SolutionSelection/CatalogueSolutionsControllerTests.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Controllers/SolutionSelection/CatalogueSolutionsControllerTests.cs
@@ -244,6 +244,44 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Controllers.Sol
 
         [Theory]
         [CommonAutoData]
+        public static async Task Get_SelectSolutionAssociatedServicesOnly_NonNullSolutionId_FromSelectAssociatedServices_ReturnsExpectedResult(
+            string internalOrgId,
+            CallOffId callOffId,
+            EntityFramework.Ordering.Models.Order order,
+            CatalogueItemId solutionId,
+            List<CatalogueItem> supplierSolutions,
+            [Frozen] Mock<IOrderService> mockOrderService,
+            [Frozen] Mock<ISolutionsService> mockSolutionsService,
+            CatalogueSolutionsController controller)
+        {
+            order.AssociatedServicesOnly = true;
+            order.SolutionId = solutionId;
+
+            mockOrderService
+                .Setup(x => x.GetOrderThin(callOffId, internalOrgId))
+                .ReturnsAsync(order);
+
+            mockSolutionsService
+                .Setup(x => x.GetSupplierSolutions(order.SupplierId))
+                .ReturnsAsync(supplierSolutions);
+
+            var result = await controller.SelectSolutionAssociatedServicesOnly(internalOrgId, callOffId, RoutingSource.SelectAssociatedServices);
+
+            mockOrderService.VerifyAll();
+            mockSolutionsService.VerifyAll();
+
+            var actualResult = result.Should().BeOfType<ViewResult>().Subject;
+
+            var expected = new SelectSolutionModel(order, supplierSolutions, Enumerable.Empty<CatalogueItem>())
+            {
+                SelectedCatalogueSolutionId = $"{solutionId}",
+            };
+
+            actualResult.Model.Should().BeEquivalentTo(expected, x => x.Excluding(o => o.BackLink));
+        }
+
+        [Theory]
+        [CommonAutoData]
         public static async Task Get_SelectSolutionAssociatedServicesOnly_ReturnsExpectedResult(
             string internalOrgId,
             CallOffId callOffId,
