3132df3d0bcde09f2ebf7fb5ff2df1200121baf7
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-07-07 14:49:31 +0100

    Only use the practice list size for Per Patient (#839)

--- tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Models/SolutionSelection/Quantity/SelectServiceRecipientQuantityModelTests.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Models/SolutionSelection/Quantity/SelectServiceRecipientQuantityModelTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Extensions;
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
 using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
@@ -23,6 +24,8 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Models.Solution
         [CommonAutoData]
         public static void WithValidOrderItem_PropertiesCorrectlySet(OrderItem item)
         {
+            item.OrderItemPrice.ProvisioningType = ProvisioningType.Patient;
+
             var model = new SelectServiceRecipientQuantityModel(item);
 
             model.ItemName.Should().Be(item.CatalogueItem.Name);
@@ -35,6 +38,24 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Models.Solution
 
                 serviceRecipient.Name.Should().Be(recipient.Recipient?.Name);
                 serviceRecipient.InputQuantity.Should().Be($"{recipient.Quantity}");
+            }
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static void WithValidOrderItem_PerServiceRecipient_QuantityCorrectlySet(OrderItem item)
+        {
+            item.OrderItemPrice.ProvisioningType = ProvisioningType.PerServiceRecipient;
+
+            var model = new SelectServiceRecipientQuantityModel(item);
+
+            foreach (var serviceRecipient in model.ServiceRecipients)
+            {
+                var recipient = item.OrderItemRecipients.Single(x => x.OdsCode == serviceRecipient.OdsCode);
+
+                serviceRecipient.Name.Should().Be(recipient.Recipient?.Name);
+                serviceRecipient.InputQuantity.Should().BeNull();
+                serviceRecipient.Quantity.Should().Be(1);
             }
         }
     }
