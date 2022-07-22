f8586abc3d264dabf8f4c3bed09eecbb1c64a026
Magnus John <97523078+magnusjohn-bjss@users.noreply.github.com>
2022-07-22 11:38:10 +0100

    Contracts backing field updates (#860)

--- src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Ordering/Configuration/ContractBillingItemEntityTypeConfiguration.cs
+++ src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Ordering/Configuration/ContractBillingItemEntityTypeConfiguration.cs
@@ -15,7 +15,7 @@ namespace NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Configuration
             builder.Property(x => x.ContractId).IsRequired();
             builder.Property(x => x.OrderId).IsRequired();
             builder.Property(x => x.CatalogueItemId).IsRequired();
-            builder.Property(x => x.MilestoneId).HasField("ImplementationPlanMilestoneId");
+            builder.Property(x => x.MilestoneId).HasColumnName("ImplementationPlanMilestoneId");
             builder.Property(x => x.Quantity).IsRequired();
 
             builder.HasOne(x => x.LastUpdatedByUser)
