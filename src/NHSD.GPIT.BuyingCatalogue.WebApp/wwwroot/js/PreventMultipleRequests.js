7d84086fb8a8f4c84ed3cab4bdb42b2224617d4a
Magnus John <97523078+magnusjohn-bjss@users.noreply.github.com>
2022-08-15 12:37:01 +0100

    Prevent multiple requests (#880)

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/js/PreventMultipleRequests.js
@@ -0,0 +1,11 @@
+﻿window.onload = function() {
+    const button = document.getElementById('Submit');
+    const form = document.getElementsByTagName("form")[0];
+
+    if (button && form) {
+        button.onclick = function() {
+            button.disabled = true;
+            form.submit();
+        }
+    }
+};
