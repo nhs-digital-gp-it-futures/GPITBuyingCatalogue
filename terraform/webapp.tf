ef25e2ac6484f49f6f90873a2ff5810369494f74
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-09-28 16:49:37 +0100

    Cleanup unused variables

--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -4,14 +4,10 @@ module "webapp" {
   environment      = var.environment
   region           = var.region
   project          = var.project
-  pjtcode          = var.pjtcode
   rg_name          = azurerm_resource_group.webapp.name
   webapp_name      = "${var.project}-${var.environment}-webapp"
   sku_tier         = local.shortenv == "preprod" || local.shortenv == "production" ? "PremiumV2" : "Standard"
   sku_size         = local.shortenv == "preprod" || local.shortenv == "production" ? "P2v2" : "S1"
-  acr_name         = "gpitfuturesdevacr"
-  acr_pwd          = data.azurerm_container_registry.acr.admin_password
-  acr_rg           = "gpitfutures-dev-rg-acr"
   repository_name  = "nhsd/buying-catalogue/nhsdgpitbuyingcataloguewebapp"
   always_on        = local.shortenv != "production" ? "false" : "true"
   cert_name        = var.certname
@@ -19,9 +15,6 @@ module "webapp" {
   instrumentation_key = azurerm_application_insights.appinsights.instrumentation_key
   primary_vpn = var.primary_vpn
   app_gateway_ip = module.appgateway.appgateway_pip_ipaddress
-  ssl_cert = data.azurerm_key_vault_secret.ssl_cert.value
-  customer_network_range = var.nhsd_network_range
-  vnet_subnet_id = azurerm_subnet.gateway.id
   app_dns_url = var.app_url
   docker_registry_server_url = data.azurerm_container_registry.acr.login_server
   docker_registry_server_username = data.azurerm_container_registry.acr.admin_username
