2fc0419f8f9050faa05c872f51105df974802ac0
Danielle Sherlock <danielle.sherlock@bjss.com>
2022-10-19 14:06:08 +0100

    Add new manage account pages. Add Layouts. Links to additional pages exist but not implemented

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/AccountManagement/Controllers/ManageAccountController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Organisations.Models;
+using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Identity;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Organisations;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Users;
+using NHSD.GPIT.BuyingCatalogue.Services.Organisations;
+using NHSD.GPIT.BuyingCatalogue.Services.Users;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.AccountManagement.Models;
+//using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models.OrganisationModels;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.AccountManagement.Controllers
+{
+    [Authorize(Policy = "AccountManager")]
+    [Area("AccountManagement")]
+    [Route("accountmanagement/manageaccount")]
+    public sealed class ManageAccountController : Controller
+    {
+        private readonly IOrganisationsService organisationsService;
+        private readonly IOdsService odsService;
+        private readonly ICreateUserService createBuyerService;
+        private readonly IUsersService userService;
+
+        public ManageAccountController(
+            IOrganisationsService organisationsService,
+            IOdsService odsService,
+            ICreateUserService createBuyerService,
+            IUsersService userService)
+        {
+            this.organisationsService = organisationsService ?? throw new ArgumentNullException(nameof(organisationsService));
+            this.odsService = odsService ?? throw new ArgumentNullException(nameof(odsService));
+            this.createBuyerService = createBuyerService ?? throw new ArgumentNullException(nameof(createBuyerService));
+            this.userService = userService ?? throw new ArgumentNullException(nameof(userService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var internalOrgId = User.GetPrimaryOrganisationInternalIdentifier();
+            var organisation = await organisationsService.GetOrganisationByInternalIdentifier(internalOrgId);
+            var users = await userService.GetAllUsersForOrganisation(organisation.Id);
+            var relatedOrganisations = await organisationsService.GetRelatedOrganisations(organisation.Id);
+
+            var model = new ManageAccountModel(organisation, users, relatedOrganisations);
+
+            return View(model);
+        }
+
+        [HttpGet("{organisationId}/users")]
+        public async Task<IActionResult> Users(int organisationId)
+        {
+            //var organisation = await organisationsService.GetOrganisation(organisationId);
+            //var users = await userService.GetAllUsersForOrganisation(organisationId);
+
+            //var model = new UsersModel
+            //{
+            //    BackLink = Url.Action(nameof(Details), new { organisationId }),
+            //    OrganisationId = organisationId,
+            //    OrganisationName = organisation.Name,
+            //    Users = users.OrderBy(x => x.LastName).ThenBy(x => x.FirstName),
+            //};
+
+            return View();
+        }
+
+        [HttpGet("{organisationId}/related")]
+        public async Task<IActionResult> RelatedOrganisations(int organisationId)
+        {
+            //var organisation = await organisationsService.GetOrganisation(organisationId);
+            //var relatedOrganisations = await organisationsService.GetRelatedOrganisations(organisationId);
+
+            //var model = new RelatedOrganisationsModel
+            //{
+            //    OrganisationId = organisationId,
+            //    OrganisationName = organisation.Name,
+            //    RelatedOrganisations = relatedOrganisations,
+            //};
+
+            return View();
+        }
+
+        [HttpGet("{organisationId}/nominated")]
+        public async Task<IActionResult> NominatedOrganisations(int organisationId)
+        {
+            //var organisation = await organisationsService.GetOrganisation(organisationId);
+            //var nominatedOrganisations = await organisationsService.GetNominatedOrganisations(organisationId);
+
+            //var model = new NominatedOrganisationsModel
+            //{
+            //    OrganisationId = organisationId,
+            //    OrganisationName = organisation.Name,
+            //    NominatedOrganisations = nominatedOrganisations,
+            //};
+
+            return View();
+        }
+    }
+}
---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/AccountManagement/Models/ManageAccountModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Addresses.Models;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Organisations.Models;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Users.Models;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.AccountManagement.Models
+{
+    public sealed class ManageAccountModel : NavBaseModel
+    {
+        public ManageAccountModel()
+        {
+        }
+
+        public ManageAccountModel(Organisation organisation, List<AspNetUser> users, List<Organisation> relatedOrganisations)
+        {
+            Organisation = organisation ?? throw new ArgumentNullException(nameof(organisation));
+            CatalogueAgreementText = organisation.CatalogueAgreementSigned ? "Organisation End User Agreement has been signed" : "Organisation End User Agreement has not been signed";
+            Users = users;
+            RelatedOrganisations = relatedOrganisations;
+        }
+
+        public Organisation Organisation { get; set; }
+
+        public List<AspNetUser> Users { get; set; }
+
+        public List<Organisation> RelatedOrganisations { get; set; }
+
+        public Address OrganisationAddress => Organisation.Address;
+
+        public string CatalogueAgreementText { get; set; }
+    }
+}
