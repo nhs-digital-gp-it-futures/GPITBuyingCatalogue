e0996c3d269d1a06ae404b9b002773a6a7c69633
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-11-03 14:04:28 +0000

    Refactor OrgImport app to use WebJob SDK

--- src/OrganisationImporter/Program.cs
+++ src/OrganisationImporter/Program.cs
@@ -1,33 +1,58 @@
-﻿using CommandLine;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using OrganisationImporter.Models;
+using Microsoft.Extensions.Hosting;
 using OrganisationImporter.Services;
 using Serilog;
 
-Log.Logger = new LoggerConfiguration()
-          .WriteTo.Console()
-          .CreateLogger();
-
-var services = new ServiceCollection()
-    .AddSingleton<OrganisationImportService>()
-    .AddLogging(builder => builder.AddSerilog());
-
-var serviceProvider = services.BuildServiceProvider();
-
-Parser
-    .Default
-    .ParseArguments<CommandLineArgs>(args)
-    .WithNotParsed(errors =>
+namespace OrganisationImporter
+{
+    public class Program
     {
-        var logger = serviceProvider.GetService<ILogger<Program>>();
+        private static IConfigurationRoot _configuration;
+        static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
 
-        logger.LogError("Failed to parse command line args.");
-    })
-    .WithParsedAsync(parsedArgs =>
-    {
-        var organisationImportService = serviceProvider
-            .GetService<OrganisationImportService>();
+            var builder = new HostBuilder();
 
-        return organisationImportService!.ImportFromUrl(parsedArgs.Url);
-    });
+            builder.ConfigureWebJobs(_ => { });
+            builder.ConfigureAppConfiguration(configBuilder =>
+            {
+                configBuilder.AddEnvironmentVariables();
+                _configuration = configBuilder.Build();
+            });
+            builder.ConfigureLogging(loggingBuilder => loggingBuilder.AddSerilog());
+            builder.ConfigureServices(ConfigureServices);
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                var jobHost = host.Services.GetService(typeof(IJobHost)) as JobHost;
+                var organisationImportService = host.Services.GetService<OrganisationImportService>();
+                var inputs = new Dictionary<string, object>
+                {
+                    { "service", organisationImportService },
+                    { "importUrl", _configuration.GetValue<Uri>("TRUD_URL") }
+                };
+
+                await host.StartAsync();
+                await jobHost!.CallAsync(nameof(DoImport), inputs);
+                await host.StopAsync();
+            }
+        }
+
+        [NoAutomaticTrigger]
+        public static async Task DoImport(OrganisationImportService service, Uri importUrl)
+        {
+            await service.ImportFromUrl(importUrl);
+        }
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<OrganisationImportService>();
+        }
+    }
+}
