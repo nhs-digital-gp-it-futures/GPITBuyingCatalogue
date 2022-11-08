9c68504d872aaf7ba8ac9a60dc5aafc183a93d11
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2022-11-08 12:54:33 +0000

    Remove CommandLineArgs as no longer necessary

--- src/OrganisationImporter/Models/CommandLineArgs.cs
+++
@@ -1,9 +0,0 @@
-using CommandLine;
-
-namespace OrganisationImporter.Models;
-
-public class CommandLineArgs
-{
-    [Option('u', "url", Required = true, HelpText = "The URL of the file to import from TRUD")]
-    public Uri Url { get; set; }
-}
