f0acd4d12b5690887a4ee9e22a3d366010872e44
Magnus John <magnus.john@bjss.com>
2022-12-02 10:54:46 +0000

    Review changes

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Order/Models/DeleteOrder/DeleteOrderModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Order/Models/DeleteOrder/DeleteOrderModel.cs
@@ -9,10 +9,12 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Order.Models.DeleteOrder
         public const string Amendment = "amendment";
         public const string AmendmentAdvice = "Deleting this amendment will revert this order to the previous version.";
         public const string AmendmentNoOptionText = "No, I do not want to delete this amendment";
+        public const string AmendmentWarning = "Deleting an amendment is permanent and any information you’ve already changed will be lost. The order will revert to its previous version.";
         public const string AmendmentYesOptionText = "Yes, I want to delete this amendment";
         public const string Order = "order";
         public const string OrderAdvice = "The order will be permanently deleted from your organisation’s dashboard.";
         public const string OrderNoOptionText = "No, I do not want to delete this order";
+        public const string OrderWarning = "Deleting an order is permanent and any information you’ve already input will be lost. Once you’ve deleted your order, you’ll not be able to retrieve it and will have to start a new one.";
         public const string OrderYesOptionText = "Yes, I want to delete this order";
 
         private readonly IList<SelectableRadioOption<bool>> amendmentOptions = new List<SelectableRadioOption<bool>>
@@ -48,6 +50,10 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Order.Models.DeleteOrder
         public string OrderType => IsAmendment
             ? Amendment
             : Order;
+
+        public string Warning => IsAmendment
+            ? AmendmentWarning
+            : OrderWarning;
 
         public bool? SelectedOption { get; set; }
 
