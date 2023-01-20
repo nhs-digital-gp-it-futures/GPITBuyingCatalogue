38294ae20d127ffbd453765e325950cefd3935b2
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2023-01-20 15:24:16 +0000

    Comment indicating AccountKey is not a real key

--- start_azurite.bat
+++ start_azurite.bat
@@ -1,3 +1,7 @@
 start /B azurite
 
+REM The following AccountKey is a dev key used by Azurite.
+REM It is not used in production or any actual environments.
+REM See https://github.com/Azure/Azurite
+REM Also see https://learn.microsoft.com/en-us/azure/storage/common/storage-configure-connection-string
 az storage container create -n capabilities-update --connection-string "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
