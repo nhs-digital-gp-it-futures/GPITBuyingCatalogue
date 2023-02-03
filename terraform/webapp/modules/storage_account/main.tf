5e73f6d1f2b68d5e8f726840d9a6ea80c0808c7f
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2023-02-03 15:30:37 +0000

    Add storage account to terraform

---
+++ terraform/webapp/modules/storage_account/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                = var.storage_account_name
+  resource_group_name = var.resource_group
+
+  location                 = var.region
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "GRS"
+
+  min_tls_version           = "TLS1_2"
+  enable_https_traffic_only = "true"
+
+  tags = {
+    environment  = var.environment,
+    architecture = "new"
+  }
+}
+
+resource "azurerm_key_vault_secret" "storageaccount_connectionstring" {
+  name         = azurerm_storage_account.storage_account.name
+  value        = azurerm_storage_account.storage_account.primary_connection_string
+  key_vault_id = var.key_vault_id
+
+  tags = {
+    environment  = var.environment,
+    architecture = "new"
+  }
+
+  depends_on = [azurerm_storage_account.storage_account]
+}
+
+output "storage_account_name" {
+  description = "Storage account name"
+  value       = azurerm_storage_account.storage_account.name
+}
+
+output "primary_connection_string" {
+  description = "Storage account connection string"
+  value       = azurerm_storage_account.storage_account.primary_connection_string
+  sensitive   = true
+}
---
+++ terraform/webapp/modules/storage_account/variables.tf
@@ -0,0 +1,19 @@
+variable "storage_account_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "resource_group" {
+  type = string
+}
+
+variable "key_vault_id" {
+  type = string
+}
