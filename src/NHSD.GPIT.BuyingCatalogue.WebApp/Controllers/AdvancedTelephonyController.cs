1cadf17c5877bf7e2b310b4640d422ecce86d5be
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2023-02-07 16:18:14 +0000

    Content changes for Advanced Telephony (#1033)

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Controllers/AdvancedTelephonyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Controllers
+{
+    public class AdvancedTelephonyController : Controller
+    {
+        [ExcludeFromCodeCoverage]
+        public IActionResult ThinkHealthcarePdf()
+        {
+            const string fileName = "Think Healthcare.pdf";
+            var resourceStream =
+                typeof(HomeController).Assembly.GetManifestResourceStream(
+                    $"NHSD.GPIT.BuyingCatalogue.WebApp.Files.{fileName}");
+
+            return File(resourceStream!, "application/pdf", fileName);
+        }
+
+        [ExcludeFromCodeCoverage]
+        public IActionResult DaisyPatientLinePdf()
+        {
+            const string fileName = "Daisy Patient Line.pdf";
+            var resourceStream =
+                typeof(HomeController).Assembly.GetManifestResourceStream(
+                    $"NHSD.GPIT.BuyingCatalogue.WebApp.Files.{fileName}");
+
+            return File(resourceStream!, "application/pdf", fileName);
+        }
+    }
+}
