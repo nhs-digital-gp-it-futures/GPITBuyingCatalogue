a5bf543de48c64d8aabad491acccd973b443a9c1
Deepika Dhanabalan Kannan <DEDH2@hscic.gov.uk>
2023-03-09 13:50:27 +0000

    Adding Framework in the homepage and added cards below it.

--- tests/NHSD.GPIT.BuyingCatalogue.E2ETests.Framework/Objects/PublicBrowse/TechInnovationObjects.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenQA.Selenium;
-
-namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.PublicBrowse
-{
-    public static class TechInnovationObjects
-    {
-        public static By HomepageButton => By.LinkText("Back to homepage");
-    }
-}
