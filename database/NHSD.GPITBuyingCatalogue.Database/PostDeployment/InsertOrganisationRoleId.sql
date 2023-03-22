6df9e418ee408c9979dee1eb41dd89ccb09e362b
Danielle Sherlock <danielle.sherlock@bjss.com>
2023-03-22 13:00:40 +0000

    Remove db name

--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertOrganisationRoleId.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertOrganisationRoleId.sql
@@ -2,6 +2,6 @@
 AND COL_LENGTH('organisations.Organisations', 'PrimaryRoleId') IS NOT NULL
 BEGIN
     --Populate OrganisationRoleId column
-    UPDATE [GPITBuyingCatalogue].[organisations].[Organisations] SET [OrganisationRoleId] = [PrimaryRoleId]
+    UPDATE [organisations].[Organisations] SET [OrganisationRoleId] = [PrimaryRoleId]
 END;
 GO
