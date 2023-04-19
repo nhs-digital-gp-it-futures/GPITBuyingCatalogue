3b4ab0edc991a8a60e67e82896affddefb487c3b
Danielle Sherlock <danielle.sherlock@bjss.com>
2023-04-19 15:37:00 +0100

    Format

--- tests/NHSD.GPIT.BuyingCatalogue.UnitTest.Framework/AutoFixtureCustomisations/OdsSettingsCustomization.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.UnitTest.Framework/AutoFixtureCustomisations/OdsSettingsCustomization.cs
@@ -13,9 +13,9 @@ namespace NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations
             ISpecimenBuilder ComposerTransformation(ICustomizationComposer<OdsSettings> composer) => composer
                 .With(d => d.BuyerOrganisationRoles, new List<OrganisationRoleSettings>()
                 {
-                    new OrganisationRoleSettings{ PrimaryRoleId = "RO213" },
-                    new OrganisationRoleSettings{ PrimaryRoleId = "RO261", SecondaryRoleId = "RO318", },
-                    new OrganisationRoleSettings{ PrimaryRoleId = "RO177", SecondaryRoleId = "RO76", },
+                    new OrganisationRoleSettings { PrimaryRoleId = "RO213" },
+                    new OrganisationRoleSettings { PrimaryRoleId = "RO261", SecondaryRoleId = "RO318", },
+                    new OrganisationRoleSettings { PrimaryRoleId = "RO177", SecondaryRoleId = "RO76", },
                 });
 
             fixture.Customize<OdsSettings>(ComposerTransformation);
