3010989503194bfaf9ee22121e0449baf0578ed1
Danielle Sherlock <danielle.sherlock@bjss.com>
2023-04-28 17:29:44 +0100

    POC Filter table

---
+++ database/NHSD.GPITBuyingCatalogue.Database/Catalogue/Tables/Filters.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE catalogue.Filters
+(
+    Id nvarchar(14) NOT NULL,
+    [Name] nvarchar(255) NOT NULL,
+    [Description] nvarchar(255) NOT NULL,
+    OrganisationId int NOT NULL,
+    FrameworkId int NULL,
+    Created datetime2(7) CONSTRAINT DF_CatalogueItem_Created DEFAULT GETUTCDATE() NOT NULL,
+    LastUpdated datetime2(7) DEFAULT GETUTCDATE() NOT NULL,
+    LastPublished datetime2(7) NULL,
+    LastUpdatedBy int NULL,
+    SysStartTime datetime2(0) GENERATED ALWAYS AS ROW START NOT NULL,
+    SysEndTime datetime2(0) GENERATED ALWAYS AS ROW END NOT NULL,
+    PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime),
+    CONSTRAINT PK_Filters PRIMARY KEY (Id),
+    CONSTRAINT FK_Filters_OrganisationId FOREIGN KEY (OrganisationId) REFERENCES organisations.Organisations(Id),
+    CONSTRAINT FK_Filters_FrameworkId FOREIGN KEY (FrameworkId) REFERENCES catalogue.Frameworks(Id),
+    CONSTRAINT FK_Filters_LastUpdatedBy FOREIGN KEY (LastUpdatedBy) REFERENCES users.AspNetUsers(Id),
+) WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = catalogue.Filters_History));
