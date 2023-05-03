91496c1360c383caa51cb9e43257ca2f6cf09cc4
martinkimmings <martin.kimmings@bjss.com>
2023-05-03 11:18:21 +0100

    Implementation Plan content changes and fix some failing integration tests.

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Models/Orders/OrderModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Models/Orders/OrderModel.cs
@@ -16,7 +16,7 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Orders
             { OrderSummaryField.SolutionsAndServices, "Add Service Recipients to items already included in this order or add Additional Services." },
             { OrderSummaryField.PlannedDeliveryDates, "Enter the planned delivery dates for the items you're ordering." },
             { OrderSummaryField.FundingSources, "Allocate funding sources for items in this order." },
-            { OrderSummaryField.ImplementationPlan, "Download the implementation plan milestones template for the supplier to complete." },
+            { OrderSummaryField.ImplementationPlan, "Review the default implementation milestones that will act as payment triggers and add bespoke ones." },
             { OrderSummaryField.DataProcessing, "Download the data processing information template for the supplier to complete." },
             { OrderSummaryField.ReviewAndComplete, "Check the information you’ve provided is correct and complete this amended order." },
         };
@@ -30,7 +30,7 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Orders
             { OrderSummaryField.SolutionsAndServices, "Add Service Recipients, prices and quantities for what you’re ordering." },
             { OrderSummaryField.PlannedDeliveryDates, "Enter the planned delivery dates for the items you're ordering." },
             { OrderSummaryField.FundingSources, "Review how you’ll be paying for your order." },
-            { OrderSummaryField.ImplementationPlan, "Select if you’re using the standard implementation plan milestones or have agreed bespoke ones." },
+            { OrderSummaryField.ImplementationPlan, "Review the default implementation milestones that will act as payment triggers and add bespoke ones." },
             { OrderSummaryField.AssociatedServicesBilling, "Select how you’d like to be billed for any Associated Services in your order." },
             { OrderSummaryField.DataProcessing, "Select if you want to make any changes to the default data processing information." },
             { OrderSummaryField.ReviewAndComplete, "Check the information you’ve provided is correct and complete your order." },
