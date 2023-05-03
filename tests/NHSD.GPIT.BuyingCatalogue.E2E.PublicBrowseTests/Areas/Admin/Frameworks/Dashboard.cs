c54ab617a2b3b1b636d80b320359e89a64e52e2b
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2023-05-03 17:20:39 +0100

    Content and tests

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Admin/Frameworks/Dashboard.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Areas/Admin/Frameworks/Dashboard.cs
@@ -1,4 +1,6 @@
 ﻿using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.Admin.Frameworks;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.Common;
 using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils;
 using NHSD.GPIT.BuyingCatalogue.E2ETests.Utils.TestBases;
 using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
@@ -19,6 +21,39 @@ public sealed class Dashboard : AuthorityTestBase
     [Fact]
     public void AllElementsDisplayed()
     {
-        CommonActions.PageTitle().Should().BeEquivalentTo("Manage Frameworks".FormatForComparison());
+        CommonActions.PageTitle().Should().BeEquivalentTo("Manage frameworks".FormatForComparison());
+        CommonActions.LedeText()
+            .Should()
+            .BeEquivalentTo("Add new frameworks or mark existing ones as expired.".FormatForComparison());
+
+        CommonActions.ElementIsDisplayed(BreadcrumbObjects.HomeBreadcrumbLink).Should().BeTrue();
+
+        CommonActions.ElementIsDisplayed(DashboardFrameworkObjects.FrameworksTable).Should().BeTrue();
+
+        CommonActions.ContinueButtonDisplayed().Should().BeTrue();
+    }
+
+    [Fact]
+    public void ClickHome_RedirectsToHomepage()
+    {
+        CommonActions.ClickLinkElement(BreadcrumbObjects.HomeBreadcrumbLink);
+
+        CommonActions.PageLoadedCorrectGetIndex(
+                typeof(HomeController),
+                nameof(HomeController.Index))
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void ClickContinue_RedirectsToHomepage()
+    {
+        CommonActions.ClickContinue();
+
+        CommonActions.PageLoadedCorrectGetIndex(
+                typeof(HomeController),
+                nameof(HomeController.Index))
+            .Should()
+            .BeTrue();
     }
 }
