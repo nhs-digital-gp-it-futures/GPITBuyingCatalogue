b4f0e58d42184d5eb09a257ac0be755119ab9461
BJSS\Sam.Dasari <sam.dasari@bjss.com>
2023-05-05 16:52:34 +0100

    local commit

---
+++ src/NHSD.GPIT.BuyingCatalogue.ServiceContracts/Models/FilterModels/FrameworkFilterInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace NHSD.GPIT.BuyingCatalogue.ServiceContracts.Models.FilterModels
+{
+    public sealed class FrameworkFilterInfo
+    {
+        public string Id { get; init; }
+
+        public string ShortName { get; init; }
+
+        public int CountOfActiveSolutions { get; set; }
+    }
+}
