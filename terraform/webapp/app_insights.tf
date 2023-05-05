fd7b2a3f68f597e7c8e69ff4090e077ed96eb00b
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2023-05-05 10:26:28 +0100

    Add app insights availability test

--- terraform/webapp/app_insights.tf
+++ terraform/webapp/app_insights.tf
@@ -10,6 +10,24 @@ resource "azurerm_application_insights" "appinsights" {
   }
 }
 
+resource "azurerm_application_insights_standard_web_test" "app_webtest" {
+  count                   = local.core_env != "dev" ? 1 : 0
+  name                    = "health-check"
+  resource_group_name     = azurerm_resource_group.app-insights.name
+  location                = var.region
+  application_insights_id = azurerm_application_insights.appinsights.id
+  geo_locations           = [
+    "emea-se-sto-edge", // UK West
+    "emea-ru-msa-edge", // UK South. Note: Contrary to the naming, this is not based in Russia. https://stackoverflow.com/a/57629988
+    "emea-gb-db3-azr", // North Europe
+    "emea-nl-ams-azr"  // West Europe
+  ]
+
+  request {
+    url = "https://${var.app_url}"
+  }
+}
+
 output "instrumentation_key" {
   value               = azurerm_application_insights.appinsights.instrumentation_key
   sensitive           = true
