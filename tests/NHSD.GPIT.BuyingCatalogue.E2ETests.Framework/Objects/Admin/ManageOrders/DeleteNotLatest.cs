189638cf58f6a090a687e1561aa06f6c35ff2a1b
martinkimmings <martin.kimmings@bjss.com>
2023-05-09 20:57:22 +0100

    You can only delete the latest amendment/order

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2ETests.Framework/Objects/Admin/ManageOrders/DeleteNotLatest.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.Admin.ManageOrders
+{
+    public static class DeleteNotLatestObjects
+    {
+        public static By ReturnToOrdersDashboard => By.LinkText("Return to orders dashboard");
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Admin/Models/ManageOrders/DeleteNotLatestModelTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models.ManageOrders;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Admin.Models.ManageOrders
+{
+    public static class DeleteNotLatestModelTest
+    {
+        [Theory]
+        [CommonAutoData]
+        public static void WithValidArguments_PropertiesCorrectlySet(
+            CallOffId callOffId)
+        {
+            var model = new DeleteNotLatestModel(callOffId);
+
+            model.CallOffId.Should().Be(callOffId);
+            model.IsAmendment.Should().Be(callOffId.IsAmendment);
+        }
+    }
+}
