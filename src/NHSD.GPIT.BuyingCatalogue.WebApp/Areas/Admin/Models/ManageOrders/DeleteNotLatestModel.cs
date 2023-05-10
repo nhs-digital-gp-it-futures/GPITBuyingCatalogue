c0f65d20285a849f0ff06a9c1bac71d2ee9140a5
martinkimmings <63051516+martinkimmings@users.noreply.github.com>
2023-05-10 14:42:17 +0100

    Restrict admin delete to the latest amendment only (#1109)
    * You can only delete the latest amendment/order

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Models/ManageOrders/DeleteNotLatestModel.cs
@@ -0,0 +1,17 @@
+﻿using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models.ManageOrders
+{
+    public sealed class DeleteNotLatestModel : NavBaseModel
+    {
+        public DeleteNotLatestModel(CallOffId callOffId)
+        {
+            CallOffId = callOffId;
+        }
+
+        public CallOffId CallOffId { get; set; }
+
+        public bool IsAmendment => CallOffId.IsAmendment;
+    }
+}
