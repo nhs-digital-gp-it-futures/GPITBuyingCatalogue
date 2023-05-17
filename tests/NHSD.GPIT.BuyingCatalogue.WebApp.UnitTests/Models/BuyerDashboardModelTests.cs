936f20bddbfd953206ab7d148ffce8ef497a2641
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2023-05-17 15:10:46 +0100

    Add buyer dashboard

---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Models/BuyerDashboardModelTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Models;
+
+public static class BuyerDashboardModelTests
+{
+    [Theory]
+    [CommonInlineAutoData(true, BuyerDashboardModel.AccountManagerAdvice)]
+    [CommonInlineAutoData(false, BuyerDashboardModel.BuyerAdvice)]
+    public static void Construct_SetsAdvice(
+        bool isAccountManager,
+        string expectedAdvice,
+        string organisationName)
+    {
+        var model = new BuyerDashboardModel(organisationName, isAccountManager);
+
+        model.Advice.Should().Be(expectedAdvice);
+    }
+}
