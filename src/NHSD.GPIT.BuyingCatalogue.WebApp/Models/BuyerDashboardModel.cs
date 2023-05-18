60dbf297f4711e37edfc491b4a9728c572a92dc0
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2023-05-18 11:45:10 +0100

    Add buyer dashboard (#1120)

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Models/BuyerDashboardModel.cs
@@ -0,0 +1,24 @@
+﻿namespace NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+
+public class BuyerDashboardModel
+{
+    internal const string AccountManagerAdvice =
+        "Create and manage orders, competitions, filters and user accounts for your organisation.";
+
+    internal const string BuyerAdvice = "Create and manage orders, competitions and filters for your organisation.";
+
+    public BuyerDashboardModel(
+        string organisationName,
+        bool isAccountManager)
+    {
+        OrganisationName = organisationName;
+
+        Advice = isAccountManager
+            ? AccountManagerAdvice
+            : BuyerAdvice;
+    }
+
+    public string OrganisationName { get; set; }
+
+    public string Advice { get; set; }
+}
