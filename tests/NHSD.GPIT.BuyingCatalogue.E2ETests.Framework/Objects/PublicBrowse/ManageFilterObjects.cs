bdd7f99547c092c8ab49dbd3151e8705a96992ac
dssherlock <113588987+dssherlock@users.noreply.github.com>
2023-05-19 14:17:01 +0100

    Feature/21806 manage filters summary (#1125)
    * Manage filters summary page
    
    * Integration test
    
    * Link to Buyers dashboard
    
    * Link from Buyer dashboard
    
    * Format
    
    * Code review fixes
    
    * Code review and Demo fixes
    
    * Unit test

---
+++ tests/NHSD.GPIT.BuyingCatalogue.E2ETests.Framework/Objects/PublicBrowse/ManageFilterObjects.cs
@@ -0,0 +1,24 @@
+﻿using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.Common;
+using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.PublicBrowse
+{
+    public static class ManageFilterObjects
+    {
+        public static By FiltersTable => ByExtensions.DataTestId("filters-table");
+
+        public static By NoFiltersMessage => ByExtensions.DataTestId("no-filters");
+
+        public static By CreateNewFilterLink => By.LinkText("Create a new filter");
+
+        public static By ManageFilterLink => By.LinkText("Go to saved filters");
+
+        public static By FilterName => ByExtensions.DataTestId("filter-name");
+
+        public static By FilterDescription => ByExtensions.DataTestId("filter-description");
+
+        public static By FilterLastUpdated => ByExtensions.DataTestId("filter-last-updated");
+
+        public static By FilterViewLink => ByExtensions.DataTestId("filter-view-link");
+    }
+}
