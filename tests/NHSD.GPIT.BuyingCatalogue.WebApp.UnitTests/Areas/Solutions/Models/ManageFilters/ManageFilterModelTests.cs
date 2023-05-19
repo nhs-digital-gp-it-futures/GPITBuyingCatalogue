fe65e7404c81006ca7615c0aa9307934e3a832c2
dssherlock <113588987+dssherlock@users.noreply.github.com>
2023-05-19 14:17:01 +0100

    Feature/21806 manage filters summary (#1125)
    * Manage filters summary page
    
    * Integration test
    
    * Link to Buyers dashboard
    
    * Link from Buyer dashboard
    
    * Format
    
    * Code review fixes
    
    * Code review and Demo fixes
    
    * Unit test

---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Solutions/Models/ManageFilters/ManageFilterModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Filtering.Models;
+using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models.ManageFilters;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Solutions.Models.ManageFilters
+{
+    public static class ManageFilterModelTests
+    {
+        [Theory]
+        [CommonAutoData]
+        public static void Constructor_PropertiesAreSetCorrectly(
+            List<Filter> filters,
+            string orgName)
+        {
+            var model = new ManageFiltersModel(filters, orgName);
+
+            model.Filters.Count.Should().Be(filters.Count);
+            model.Filters.Should().BeEquivalentTo(filters);
+            model.OrganisationName.Should().Be(orgName);
+        }
+    }
+}
