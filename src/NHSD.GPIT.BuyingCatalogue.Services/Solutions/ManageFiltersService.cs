a88bfbac3be987c9a10e23b6d69dd64dd92f6a1e
dssherlock <113588987+dssherlock@users.noreply.github.com>
2023-05-19 14:17:01 +0100

    Feature/21806 manage filters summary (#1125)
    * Manage filters summary page
    
    * Integration test
    
    * Link to Buyers dashboard
    
    * Link from Buyer dashboard
    
    * Format
    
    * Code review fixes
    
    * Code review and Demo fixes
    
    * Unit test

--- src/NHSD.GPIT.BuyingCatalogue.Services/Solutions/ManageFiltersService.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Services/Solutions/ManageFiltersService.cs
@@ -72,13 +72,7 @@ namespace NHSD.GPIT.BuyingCatalogue.Services.Solutions
         public async Task<List<Filter>> GetFilters(int organisationId)
         {
             return await dbContext.Filters.Where(o => o.OrganisationId == organisationId)
-                .Include(x => x.Framework)
-                .Include(x => x.FilterCapabilities)
-                .ThenInclude(y => y.Capability)
-                .Include(x => x.FilterEpics)
-                .ThenInclude(y => y.Epic)
-                .Include(x => x.FilterHostingTypes)
-                .Include(x => x.FilterClientApplicationTypes)
+                .AsNoTracking()
                 .ToListAsync();
         }
 
--- tests/NHSD.GPIT.BuyingCatalogue.E2ETests.Framework/Objects/Common/BreadcrumbObjects.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2ETests.Framework/Objects/Common/BreadcrumbObjects.cs
@@ -6,6 +6,8 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Objects.Common
     {
         public static By HomeBreadcrumbLink => By.LinkText("Home");
 
+        public static By BuyerDashboardBreadcrumbLink => By.LinkText("Buyer Dashboard");
+
         public static By OrganisationDetailsBreadcrumbLink => By.LinkText("Organisation details");
     }
 }
