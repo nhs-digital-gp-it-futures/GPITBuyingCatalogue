d485b8fc708dd5f0a3a64ce33c1624241a12cbcd
dssherlock <113588987+dssherlock@users.noreply.github.com>
2023-05-19 14:17:01 +0100

    Feature/21806 manage filters summary (#1125)
    * Manage filters summary page
    
    * Integration test
    
    * Link to Buyers dashboard
    
    * Link from Buyer dashboard
    
    * Format
    
    * Code review fixes
    
    * Code review and Demo fixes
    
    * Unit test

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Utils/LocalWebApplicationFactory.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Utils/LocalWebApplicationFactory.cs
@@ -192,6 +192,7 @@ namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Utils
                     OrderSeedData.Initialize(bcDb);
                     ContractSeedData.Initialize(bcDb);
                     EmailDomainSeedData.Initialize(bcDb);
+                    FiltersSeedData.Initialize(bcDb);
                 }
                 catch (Exception ex)
                 {
