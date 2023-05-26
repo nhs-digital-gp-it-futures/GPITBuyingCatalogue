811157410ddce2270975d9fbffb7aa2345c821db
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2023-05-26 21:27:08 +0100

    Remove temporal tracking from filter tables

--- database/NHSD.GPITBuyingCatalogue.Database/Filtering/TemporalTables/FilterEpics_History.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE filtering.FilterEpics_History
-(
-     FilterId int NOT NULL,
-     EpicId nvarchar(10) NOT NULL,
-     LastUpdated datetime2(7) NOT NULL,
-     LastUpdatedBy int NULL,
-     SysStartTime datetime2(0) NOT NULL,
-     SysEndTime datetime2(0) NOT NULL
-);
