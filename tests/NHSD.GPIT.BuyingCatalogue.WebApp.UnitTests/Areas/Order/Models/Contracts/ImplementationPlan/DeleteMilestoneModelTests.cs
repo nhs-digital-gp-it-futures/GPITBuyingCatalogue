2a135d096a21cfd106c9fb31bd6e00343e4f430b
Danielle Sherlock <danielle.sherlock@bjss.com>
2023-06-16 14:09:37 +0100

    Test coverage complete

---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Models/Contracts/ImplementationPlan/DeleteMilestoneModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Contracts.ImplementationPlans;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Models.Contracts.ImplementationPlan
+{
+    public static class DeleteMilestoneModelTests
+    {
+        [Theory]
+        [CommonAutoData]
+        public static void WithValidArguments_PropertiesCorrectlySet(
+            CallOffId callOffId, string internalOrgId, int milestoneId)
+        {
+            var model = new DeleteMilestoneModel(callOffId, internalOrgId, milestoneId);
+
+            model.CallOffId.Should().Be(callOffId);
+            model.InternalOrgId.Should().Be(internalOrgId);
+            model.MilestoneId.Should().Be(milestoneId);
+        }
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Models/Contracts/ImplementationPlan/MilestoneModelTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Contracts.ImplementationPlans;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Models.Contracts.ImplementationPlan
+{
+    public static class MilestoneModelTests
+    {
+        [Theory]
+        [CommonAutoData]
+        public static void WithValidArguments_Add_PropertiesCorrectlySet(
+            CallOffId callOffId, string internalOrgId)
+        {
+            var model = new MilestoneModel(callOffId, internalOrgId);
+
+            model.CallOffId.Should().Be(callOffId);
+            model.InternalOrgId.Should().Be(internalOrgId);
+            model.IsEdit.Should().BeFalse();
+            model.Title.Should().Be("Add a bespoke implementation milestone");
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static void WithValidArguments_Edit_PropertiesCorrectlySet(
+            CallOffId callOffId, string internalOrgId, ImplementationPlanMilestone milestone)
+        {
+            var model = new MilestoneModel(milestone, callOffId, internalOrgId);
+
+            model.Name.Should().Be(milestone.Title);
+            model.PaymentTrigger.Should().Be(milestone.PaymentTrigger);
+            model.IsEdit.Should().BeTrue();
+            model.Title.Should().Be("Edit a bespoke implementation milestone");
+        }
+    }
+}
