c508cefb32c5dd1acc918c37d09103765fb39914
Deepika Dhanabalan Kannan <DEDH2@hscic.gov.uk>
2023-07-10 14:36:26 +0100

    Marketing : menu item 3 List Price and menu item 4 Capabilities and Epics

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/CapabilitiesViewModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/CapabilitiesViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
+using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Solutions.Models;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
 
@@ -12,6 +13,8 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models
             : base(solution, contentStatus)
         {
             RowViewModels = solution.CatalogueItemCapabilities.Select(cic => new RowViewModel(cic)).ToList();
+            SupplierName = solution.Supplier.Name;
+            IsFoundation = solution.Solution.FrameworkSolutions.Any(fs => fs.IsFoundation).ToYesNo();
         }
 
         public CapabilitiesViewModel(CatalogueItem solution, CatalogueItem additionalService, CatalogueItemContentStatus contentStatus)
@@ -23,6 +26,10 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models
         public CapabilitiesViewModel()
         {
         }
+
+        public string SupplierName { get; }
+
+        public string IsFoundation { get; }
 
         public string CapabilitiesHeading { get; set; } = "Capabilities met";
 
@@ -37,5 +44,9 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models
         public string BackLink { get; set; }
 
         public IList<RowViewModel> RowViewModels { get; } = new List<RowViewModel>();
+
+        public string FrameworkTitle() => Frameworks is not null && Frameworks.Any() && Frameworks.Count > 1
+            ? "Frameworks"
+            : "Framework";
     }
 }
--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/ListPriceModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/ListPriceModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Solutions.Models;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models
@@ -20,11 +22,22 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models
                     cp.CataloguePriceType == CataloguePriceType.Flat
                     && cp.PublishedStatus == PublicationStatus.Published)
                 .Select(cp => new PriceViewModel(cp)).ToList();
+
+            IsFoundation = item.Solution.FrameworkSolutions.Any(fs => fs.IsFoundation).ToYesNo();
+            SupplierName = item.Supplier.Name;
         }
+
+        public string SupplierName { get; }
+
+        public string IsFoundation { get; }
 
         public override int Index => 2;
 
         public IList<PriceViewModel> FlatListPrices { get; set; }
+
+        public string FrameworkTitle() => Frameworks is not null && Frameworks.Any() && Frameworks.Count > 1
+            ? "Frameworks"
+            : "Framework";
 
         public bool HasFlatListPrices() => FlatListPrices?.Any() == true;
     }
