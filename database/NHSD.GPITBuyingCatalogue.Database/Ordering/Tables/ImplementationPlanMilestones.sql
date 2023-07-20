a499d3ee1740bffedd2aee4bb08897ede4443abf
Danielle Sherlock <danielle.sherlock@bjss.com>
2023-07-20 17:24:50 +0100

    Fix

--- database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/ImplementationPlanMilestones.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/ImplementationPlanMilestones.sql
@@ -13,5 +13,6 @@
     PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime),
     CONSTRAINT PK_ImplementationPlanMilestones PRIMARY KEY (Id),
     CONSTRAINT FK_ImplementationPlanMilestones_ImplementationPlan FOREIGN KEY (ImplementationPlanId) REFERENCES ordering.ImplementationPlans(Id) ON DELETE CASCADE,
+    CONSTRAINT FK_ImplementationPlanMilestones_ContractBillingItem FOREIGN KEY (ContractBillingItemId) REFERENCES ordering.ContractBillingItems(Id) ON DELETE CASCADE,
     CONSTRAINT FK_ImplementationPlanMilestones_LastUpdatedBy FOREIGN KEY (LastUpdatedBy) REFERENCES users.AspNetUsers(Id)
 ) WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = ordering.ImplementationPlanMilestones_History));
