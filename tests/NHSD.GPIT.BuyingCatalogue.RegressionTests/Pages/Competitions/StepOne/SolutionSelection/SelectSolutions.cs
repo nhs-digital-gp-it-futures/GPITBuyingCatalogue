90236dee7c802ef65879ffcaf6af6b4bfd7f137f
imranbashir01 <107404445+imranbashir01@users.noreply.github.com>
2023-07-26 17:42:33 +0100

    Regression tests further competition step one (#1189)
    Further competition step one building blocks

---
+++ tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Pages/Competitions/StepOne/SolutionSelection/SelectSolutions.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Actions.Common;
+using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
+using NHSD.GPIT.BuyingCatalogue.RegressionTests.Utils;
+using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.RegressionTests.Pages.Competitions.StepOneCreateCompetition.SolutionSelection
+{
+    public class SelectSolutions : PageBase
+    {
+        public SelectSolutions(IWebDriver driver, CommonActions commonActions)
+            : base(driver, commonActions)
+        {
+        }
+
+        public void AddSolutions(int numberofSolutions)
+        {
+            CommonActions.LedeText().Should().Be("These are the results from your chosen filter. You must provide a reason if any of the solutions listed are not taken through to your competition shortlist.".FormatForComparison());
+
+            CommonActions.ClickMultipleCheckboxes(numberofSolutions);
+            CommonActions.ClickSave();
+        }
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Pages/Competitions/StepOne/SolutionSelection/SolutionNotShortlisted.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.E2ETests.Framework.Actions.Common;
+using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
+using NHSD.GPIT.BuyingCatalogue.RegressionTests.Utils;
+using OpenQA.Selenium;
+
+namespace NHSD.GPIT.BuyingCatalogue.RegressionTests.Pages.Competitions.StepOneCreateCompetition.SolutionSelection
+{
+    public class SolutionNotShortlisted : PageBase
+    {
+        public SolutionNotShortlisted(IWebDriver driver, CommonActions commonActions)
+            : base(driver, commonActions)
+        {
+        }
+
+        public void SolutionNotIncludedInShortlisting()
+        {
+            CommonActions.LedeText().Should().Be("Provide a justification for why these solutions were not included in your shortlist.".FormatForComparison());
+
+            var textInput = TextGenerators.TextInput(100);
+            CommonActions.EnterTextInTextBoxes(textInput);
+
+            CommonActions.ClickSave();
+        }
+    }
+}
