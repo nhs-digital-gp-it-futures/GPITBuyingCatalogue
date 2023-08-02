1ca83320ae7468a3bd52e1db8b17e0bfc9ec7dd3
Deepika Dhanabalan Kannan <DEDH2@hscic.gov.uk>
2023-08-02 14:41:14 +0100

    removed unwanted statements under with using keyword.

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/SolutionFeaturesModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/SolutionFeaturesModel.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
-using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
-using NHSD.GPIT.BuyingCatalogue.Framework.Extensions;
+﻿using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Solutions.Models;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models
