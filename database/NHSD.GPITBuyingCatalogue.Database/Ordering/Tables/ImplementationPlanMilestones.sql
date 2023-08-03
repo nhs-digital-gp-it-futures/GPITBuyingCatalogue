fd67e72e07be49184c6c6566ea97b65eff1b8081
Danielle Sherlock <danielle.sherlock@bjss.com>
2023-08-03 15:08:22 +0100

    Remove reference to decommissioned OrderStatusId column

--- database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/ImplementationPlanMilestones.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/ImplementationPlanMilestones.sql
@@ -12,7 +12,7 @@
     SysEndTime datetime2(0) GENERATED ALWAYS AS ROW END NOT NULL,
     PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime),
     CONSTRAINT PK_ImplementationPlanMilestones PRIMARY KEY (Id),
-    CONSTRAINT FK_ImplementationPlanMilestones_ImplementationPlan FOREIGN KEY (ImplementationPlanId) REFERENCES ordering.ImplementationPlans(Id) ON DELETE CASCADE,
+    CONSTRAINT FK_ImplementationPlanMilestones_ImplementationPlan FOREIGN KEY (ImplementationPlanId) REFERENCES ordering.ImplementationPlans(Id),
     CONSTRAINT FK_ImplementationPlanMilestones_ContractBillingItem FOREIGN KEY (ContractBillingItemId) REFERENCES ordering.ContractBillingItems(Id) ON DELETE CASCADE,
     CONSTRAINT FK_ImplementationPlanMilestones_LastUpdatedBy FOREIGN KEY (LastUpdatedBy) REFERENCES users.AspNetUsers(Id)
 ) WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = ordering.ImplementationPlanMilestones_History));
--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/MigrateContracts.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/MigrateContracts.sql
@@ -9,7 +9,7 @@
     inner join ordering.[Contracts] c on cf.OrderId = c.OrderId
     inner join ordering.[Orders] o on cf.OrderId = o.Id
     WHERE cf.UseDefaultImplementationPlan = 1
-    OR (cf.UseDefaultImplementationPlan = 0 AND o.OrderStatusId = 1)
+    OR (cf.UseDefaultImplementationPlan = 0 AND o.Completed is not NULL)
 GO
 
 IF NOT EXISTS (SELECT 1 FROM ordering.[ContractBilling])
@@ -19,11 +19,11 @@ IF NOT EXISTS (SELECT 1 FROM ordering.[ContractBilling])
     inner join ordering.[Contracts] c on cf.OrderId = c.OrderId
     inner join ordering.[Orders] o on cf.OrderId = o.Id
     WHERE cf.UseDefaultBilling = 1
-    OR (cf.UseDefaultBilling = 0 AND o.OrderStatusId = 1)
+    OR (cf.UseDefaultBilling = 0 AND o.Completed is not NULL)
 
     UPDATE ordering.[ContractBilling] SET [HasConfirmedRequirements] = 1
     FROM ordering.ContractFlags cf
     inner join ordering.[Orders] o on cf.OrderId = o.Id
     WHERE cf.HasSpecificRequirements = 0
-    OR (cf.HasSpecificRequirements = 1 AND o.OrderStatusId = 1) 
+    OR (cf.HasSpecificRequirements = 1 AND o.Completed is not NULL) 
 GO
