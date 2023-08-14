43416bd3886b9d4f7d49071039407bcd4fdf5030
imranbashir01 <107404445+imranbashir01@users.noreply.github.com>
2023-08-14 15:13:05 +0100

    building blocks for single and no solution results (#1203)
    * building blocks for single and no solution results

--- tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Pages/Competitions/StepOne/StartCompetition.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Pages/Competitions/StepOne/StartCompetition.cs
@@ -34,9 +34,9 @@ namespace NHSD.GPIT.BuyingCatalogue.RegressionTests.Pages.Competitions.StepOneCr
 
         public string GetDescription(string competition)
         {
-            var randomText = TextGenerators.TextInput(150);
+            var randomText = TextGenerators.TextInput(25);
 
-            var newDescription = competition + randomText;
+            var newDescription = $"{competition, 10}  {randomText}";
             return newDescription;
         }
     }
