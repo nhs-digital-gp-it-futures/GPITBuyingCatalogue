ea3a36b650fa5218b4bb3cd2b9a85ed04a6b6d96
dssherlock <113588987+dssherlock@users.noreply.github.com>
2023-09-07 11:32:37 +0100

    Feature/Terminate Contract (#1224)
    * Feature/22273 collect termination info (#1204)
    
    * UI
    
    * Summary page
    
    * UI
    
    * POC
    
    * Format
    
    * Update tag
    
    * Dev complete
    
    * Unit tests
    
    * Format
    
    * Demo and review feedback
    
    * Revert sql proj change
    
    * Merge error
    
    * Validation summary
    
    * Summary page (#1217)
    
    * Summary page
    
    * Format
    
    * Format
    
    * Feature/22286 Send Email to Finance and Buyer with CSV (#1220)
    
    * Initial POC
    
    * Update CSV format
    
    * App settings
    
    * Update PDF
    
    * Bug fix
    
    * Unit test fix
    
    * Additional unit tests
    
    * Cascade delete

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Models/Orders/SummaryModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Models/Orders/SummaryModel.cs
@@ -8,11 +8,12 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Orders
 {
     public sealed class SummaryModel : OrderingBaseModel
     {
-        public SummaryModel(OrderWrapper orderWrapper, string internalOrgId, ImplementationPlan defaultPlan)
+        public SummaryModel(OrderWrapper orderWrapper, string internalOrgId, bool hasSubsequentRevisions, ImplementationPlan defaultPlan)
         {
             InternalOrgId = internalOrgId;
             OrderWrapper = orderWrapper;
             DefaultPlan = defaultPlan;
+            HasSubsequentRevisions = hasSubsequentRevisions;
         }
 
         public OrderWrapper OrderWrapper { get; }
@@ -27,7 +28,11 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Orders
 
         public string AdviceText { get; set; }
 
-        public bool CanBeAmended { get; set; }
+        public bool CanBeTerminated => Order.OrderStatus == OrderStatus.Completed && !HasSubsequentRevisions;
+
+        public bool CanBeAmended => !Order.AssociatedServicesOnly && CanBeTerminated;
+
+        public bool HasSubsequentRevisions { get; init; }
 
         public ImplementationPlan DefaultPlan { get; set; }
 
