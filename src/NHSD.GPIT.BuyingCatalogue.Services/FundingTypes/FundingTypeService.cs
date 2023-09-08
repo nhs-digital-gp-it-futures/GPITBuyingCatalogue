277ee618573fc12e8183dc17450f6e2757419e3e
Danielle Sherlock <danielle.sherlock@bjss.com>
2023-09-08 16:59:59 +0100

    Update CSV

--- src/NHSD.GPIT.BuyingCatalogue.Services/FundingTypes/FundingTypeService.cs
+++ src/NHSD.GPIT.BuyingCatalogue.Services/FundingTypes/FundingTypeService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using EnumsNET;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.FundingTypes;
 
@@ -7,56 +9,30 @@ namespace NHSD.GPIT.BuyingCatalogue.Services.FundingTypes
 {
     public class FundingTypeService : IFundingTypeService
     {
-        public OrderItemFundingType GetFundingType(List<OrderItemFundingType> allFundingTypes, OrderItemFundingType current)
+        public OrderItemFundingType GetFundingType(List<OrderItemFundingType> allFundingTypes, OrderItemFundingType fundingType)
         {
             if (allFundingTypes == null)
             {
                 throw new ArgumentNullException(nameof(allFundingTypes));
             }
 
+            if (Enum.IsDefined(typeof(FundingType), fundingType.ToString()) || fundingType == OrderItemFundingType.CentralFunding)
+                return fundingType;
+
+            return fundingType switch
+            {
+                OrderItemFundingType.LocalFundingOnly => OrderItemFundingType.LocalFunding,
+                OrderItemFundingType.MixedFunding => GetMixed(allFundingTypes),
+                _ => OrderItemFundingType.None,
+            };
+        }
+
+        private static OrderItemFundingType GetMixed(List<OrderItemFundingType> allFundingTypes)
+        {
             var containsCentral = allFundingTypes.Contains(OrderItemFundingType.CentralFunding);
             var containsLocal = allFundingTypes.Contains(OrderItemFundingType.LocalFunding) || allFundingTypes.Contains(OrderItemFundingType.LocalFundingOnly);
 
-            if (containsCentral)
-            {
-                return DefaultToCentral(current);
-            }
-
-            return containsLocal
-                ? DefaultToLocal(current)
-                : DefaultToCentral(current);
+            return (!containsCentral && containsLocal) ? OrderItemFundingType.LocalFunding : OrderItemFundingType.CentralFunding;
         }
-
-        private static OrderItemFundingType DefaultToCentral(OrderItemFundingType input)
-        {
-            var output = input switch
-            {
-                OrderItemFundingType.CentralFunding => OrderItemFundingType.CentralFunding,
-                OrderItemFundingType.LocalFunding => OrderItemFundingType.LocalFunding,
-                OrderItemFundingType.LocalFundingOnly => OrderItemFundingType.LocalFunding,
-                OrderItemFundingType.MixedFunding => OrderItemFundingType.CentralFunding,
-                OrderItemFundingType.NoFundingRequired => OrderItemFundingType.CentralFunding,
-                OrderItemFundingType.None => OrderItemFundingType.CentralFunding,
-                _ => throw new ArgumentOutOfRangeException(nameof(input), input, null),
-            };
-
-            return output;
-        }
-
-        private static OrderItemFundingType DefaultToLocal(OrderItemFundingType input)
-        {
-            var output = input switch
-            {
-                OrderItemFundingType.CentralFunding => OrderItemFundingType.CentralFunding,
-                OrderItemFundingType.LocalFunding => OrderItemFundingType.LocalFunding,
-                OrderItemFundingType.LocalFundingOnly => OrderItemFundingType.LocalFunding,
-                OrderItemFundingType.MixedFunding => OrderItemFundingType.LocalFunding,
-                OrderItemFundingType.NoFundingRequired => OrderItemFundingType.LocalFunding,
-                OrderItemFundingType.None => OrderItemFundingType.LocalFunding,
-                _ => throw new ArgumentOutOfRangeException(nameof(input), input, null),
-            };
-
-            return output;
-        }
-    }
+}
 }
