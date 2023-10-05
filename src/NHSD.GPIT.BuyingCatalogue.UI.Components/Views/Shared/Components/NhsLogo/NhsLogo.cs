aca38ff08db0a0c07bb7d819ab4d4e9f661fdb3e
danielle.sherlock <danielle.sherlock@bjss.com>
2023-10-05 14:43:51 +0100

    Adding components

---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/NhsLogo/NhsLogo.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.NhsLogo
+{
+    public sealed class NhsLogo : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("NhsLogo"));
+        }
+    }
+}
---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/NhsNavigationPanel/NhsNavigationPanelViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.NhsNavigationPanel
+{
+    public sealed class NhsNavigationPanelViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string title, List<KeyValuePair<string, string>> items)
+        {
+            return await Task.FromResult(View("NhsNavigationPanel", new NhsNavigationPanelModel
+            {
+                Title = title,
+                Items = items,
+            }));
+        }
+    }
+}
