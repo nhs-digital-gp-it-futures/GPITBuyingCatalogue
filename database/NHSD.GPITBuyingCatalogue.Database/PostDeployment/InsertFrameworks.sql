22256237d526a45a780b98cb5dc08fb2c4c015c7
imranbashir01 <imran.bashir@bjss.com>
2023-10-10 11:58:30 +0100

    reverting the change for the framework type

--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertFrameworks.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertFrameworks.sql
@@ -12,7 +12,7 @@
 INSERT INTO @frameworks (Id, [Name], ShortName, [Owner], FundingTypes, IsExpired)
 VALUES ('NHSDGP001', 'NHS Digital GP IT Futures Framework 1', 'GP IT Futures', 'NHS Digital', '1,2', 1),
        ('DFOCVC001', 'Digital First Online Consultation and Video Consultation Framework 1', 'DFOCVC', 'NHS England', '2', 0),
-       ('TIF001', 'Tech Innovation Framework', 'Tech Innovation', 'NHS Digital', '1,2', 0),
+       ('TIF001', 'Tech Innovation Framework', 'Tech Innovation', 'NHS Digital', '1', 0),
        ('ATP001', 'Advanced Telephony Framework', 'Advanced Telephony', 'NHS England', '2', 0);
 
 MERGE INTO catalogue.Frameworks AS TARGET
