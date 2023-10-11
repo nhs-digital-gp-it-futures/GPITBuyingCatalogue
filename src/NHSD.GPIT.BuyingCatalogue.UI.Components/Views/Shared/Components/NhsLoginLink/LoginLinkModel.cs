11e38e54082c1b792478e222eff8778765415bc3
dssherlock <113588987+dssherlock@users.noreply.github.com>
2023-10-11 17:23:46 +0100

    Feature/22742 Home page UI redesign (#1250)

---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/NhsLoginLink/LoginLinkModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.NhsLoginLink
+{
+    public sealed class LoginLinkModel
+    {
+        public bool IsLoggedIn { get; set; }
+
+        public string Url { get; set; }
+
+        public string LinkText { get; set; }
+
+        public string Text { get; set; }
+    }
+}
