b4c25b5683bb28de7a7b5e19ee65fd8e8e65737c
martinkimmings <63051516+martinkimmings@users.noreply.github.com>
2023-11-07 11:54:24 +0000

    Change Service recipient validation for mergers and splits to at least 2 (#1276)
    * Change Service recipient validation for mergers and splits to at least 2.
    
    * Fix disappearing ICB name and add more tests.
    
    * Simplify code.

--- tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Validation/Shared/SelectRecipientsModelValidatorTests.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Validation/Shared/SelectRecipientsModelValidatorTests.cs
@@ -2,12 +2,11 @@
 using FluentValidation.TestHelper;
 using MoreLinq.Extensions;
 using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
-using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Competitions.Validators;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.ServiceRecipientModels;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Validation.Shared;
 using Xunit;
 
-namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Competitions.Validators;
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Validation.Shared;
 
 public static class SelectRecipientsModelValidatorTests
 {
@@ -17,6 +16,7 @@ public static class SelectRecipientsModelValidatorTests
         SelectRecipientsModel model,
         SelectRecipientsModelValidator systemUnderTest)
     {
+        model.SelectAtLeast = null;
         model.GetServiceRecipients().ForEach(x => x.Selected = false);
 
         var result = systemUnderTest.TestValidate(model);
@@ -27,10 +27,30 @@ public static class SelectRecipientsModelValidatorTests
 
     [Theory]
     [CommonAutoData]
-    public static void Validate_SelectionMade_NoValidationErrors(
+    public static void Validate_AtLeastSelectionMade_ThrowsValidationError(
         SelectRecipientsModel model,
         SelectRecipientsModelValidator systemUnderTest)
     {
+        model.SelectAtLeast = 2;
+        model.GetServiceRecipients().ForEach(x => x.Selected = false);
+        model.GetServiceRecipients().First().Selected = true;
+
+        var result = systemUnderTest.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor("SubLocations[0].ServiceRecipients[0].Selected")
+            .WithErrorMessage(string.Format(SelectRecipientsModelValidator.SelectAtLeastErrorMessage, model.SelectAtLeast.Value));
+    }
+
+    [Theory]
+    [CommonInlineAutoData(null)]
+    [CommonInlineAutoData(0)]
+    [CommonInlineAutoData(1)]
+    public static void Validate_SelectionMade_NoValidationErrors(
+        int? selectAtLeast,
+        SelectRecipientsModel model,
+        SelectRecipientsModelValidator systemUnderTest)
+    {
+        model.SelectAtLeast = selectAtLeast;
         model.GetServiceRecipients().ForEach(x => x.Selected = false);
         model.GetServiceRecipients().First().Selected = true;
 
