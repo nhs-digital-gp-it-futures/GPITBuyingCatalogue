1e1a4ba8e279106bf9393664569be6d5199f6a74
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2023-11-13 14:53:22 +0000

    Security: Implement Google's reCAPTCHA (#1280)

---
+++ src/NHSD.GPIT.BuyingCatalogue.ServiceContracts/Security/Models/RecaptchaSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace NHSD.GPIT.BuyingCatalogue.ServiceContracts.Security.Models;
+
+[ExcludeFromCodeCoverage]
+public class RecaptchaSettings
+{
+    private const string GoogleApiUrl = "https://www.google.com/recaptcha/api/";
+
+    public static Uri GoogleRecaptchaApiUri => new(GoogleApiUrl);
+
+    public bool IsEnabled { get; set; }
+
+    public string SiteKey { get; set; }
+
+    public string SecretKey { get; set; }
+}
--- terraform/webapp/modules/bc_webapp_pri/main.tf
+++ terraform/webapp/modules/bc_webapp_pri/main.tf
@@ -34,6 +34,9 @@ resource "azurerm_linux_web_app" "webapp" {
     # Settings for sql
     BC_DB_CONNECTION                    = "Server=tcp:${data.azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${var.db_name_main};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${var.sql_admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"    
     AZUREBLOBSETTINGS__CONNECTIONSTRING = var.blob_storage_connection_string
+
+    RECAPTCHASETTINGS__SITEKEY          = var.recaptcha_site_key
+    RECAPTCHASETTINGS__SECRETKEY        = var.recaptcha_secret_key
     
     NOTIFY_API_KEY                      = var.notify_api_key
 
