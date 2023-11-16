85fc3a32a781decfe1c411c828d9b18f4204e72f
martinkimmings <63051516+martinkimmings@users.noreply.github.com>
2023-11-16 11:06:51 +0000

    Add select recipient page for merger and splits (#1287)

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Validators/SolutionSelection/ServiceRecipients/RecipientForPracticeReorganisationModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.SolutionSelection.ServiceRecipients;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Validators.SolutionSelection.ServiceRecipients
+{
+    public class RecipientForPracticeReorganisationModelValidator : AbstractValidator<RecipientForPracticeReorganisationModel>
+    {
+        internal const string ErrorMessage = "Select a Service Recipient";
+
+        public RecipientForPracticeReorganisationModelValidator()
+        {
+            RuleFor(m => m.SelectedOdsCode)
+                .NotNull()
+                .WithMessage(ErrorMessage);
+        }
+    }
+}
