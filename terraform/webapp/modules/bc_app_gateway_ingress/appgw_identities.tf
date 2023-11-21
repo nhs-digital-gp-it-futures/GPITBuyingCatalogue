9c343702c149048a552c621262bd6ccf6c46c9fe
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2023-11-21 10:31:35 +0000

    Security: Add WAFv2 App Gateway instance (#1288)

--- terraform/webapp/modules/bc_app_gateway_ingress/appgw_identities.tf
+++ terraform/webapp/modules/bc_app_gateway_ingress/appgw_identities.tf
@@ -1,4 +1,4 @@
 data "azurerm_user_assigned_identity" "managed_identity_aad" {
-  name                = "${var.ag_name_fragment}-managed-id"
-  resource_group_name = "${var.ag_name_fragment}-rg-webapp"
+  name                = "${local.name_fragment}-managed-id"
+  resource_group_name = "${local.name_fragment}-rg-webapp"
 }
