b8e9701552b1b69381d39b8a3de8c64e21d2cdba
martinkimmings <martin.kimmings@bjss.com>
2023-11-23 14:52:53 +0000

    fix terraform

--- terraform/webapp/log_analytics.tf
+++ terraform/webapp/log_analytics.tf
@@ -1,7 +1,7 @@
 resource "azurerm_log_analytics_workspace" "log_analytics" {
   name                = "${var.project}-${var.environment}-log-analytics"
   location            = var.region
-  resource_group_name = azurerm_resource_group.log-analytics
+  resource_group_name = azurerm_resource_group.log-analytics.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 
