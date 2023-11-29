38451cbd3bd6d42190d8393e944027494ea11eb9
martinkimmings <martin.kimmings@bjss.com>
2023-11-29 14:36:53 +0000

    Only add the diagnostic setting for the app gateway if we have one.

--- terraform/webapp/modules/bc_app_gateway_ingress/appgw_diagnostics.tf
+++ terraform/webapp/modules/bc_app_gateway_ingress/appgw_diagnostics.tf
@@ -4,4 +4,5 @@ module "diagnostics" {
   enable_logs                = local.gateway_logs
   log_analytics_workspace_id = var.log_analytics_workspace_id
   target_resource_id         = azurerm_application_gateway.app_gateway[0].id
+  count                      = var.core_env != "dev" ? 1 : 0
 } 
