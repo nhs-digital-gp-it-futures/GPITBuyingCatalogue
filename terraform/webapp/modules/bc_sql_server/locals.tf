9b14d94d2eb4bd4084ad39cb8dfe4762779d61ff
martinkimmings <martin.kimmings@bjss.com>
2023-11-29 15:58:50 +0000

    Add azurerm_mssql_server_extended_auditing_policy for server auditing The sql diagnostic setting should target the master database

--- terraform/webapp/modules/bc_sql_server/locals.tf
+++ terraform/webapp/modules/bc_sql_server/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  diagnostics_name = "${var.project}-${var.environment}-sqldiag"
-  enable_logs = [
+  diagnostics_name   = "${var.project}-${var.environment}-sqldiag"
+  target_resource_id = "${azurerm_mssql_server.sql_server.id}/databases/master"
+  enable_logs        = [
     "Deadlocks",
     "Errors",
     "SQLSecurityAuditEvents",
