c4fc7925dd8cd75263cd00470b092aee1a7dd459
Deepika Dhanabalan Kannan <DEDH2@hscic.gov.uk>
2024-01-08 21:22:15 +0000

    Tests.

---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Validation/UploadOrSelectServiceRecipientModelValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.TestHelper;
+using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.AutoFixtureCustomisations;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.ServiceRecipientModels;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Validation;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Validation
+{
+    public static class UploadOrSelectServiceRecipientModelValidatorTests
+    {
+        [Theory]
+        [CommonAutoData]
+        public static void Validate_NoSelectedOrderItemType_SetsModelError(
+            UploadOrSelectServiceRecipientModel model,
+            UploadOrSelectServiceRecipientModelValidator validator)
+        {
+            model.SelectedServiceRecipientOptions = null;
+
+            var result = validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(m => m.SelectedServiceRecipientOptions)
+                .WithErrorMessage(UploadOrSelectServiceRecipientModelValidator.SelectedServiceRecipientOptionsError);
+        }
+
+        [Theory]
+        [CommonAutoData]
+        public static void Validate_SelectedOrderItemType_NoModelError(
+            UploadOrSelectServiceRecipientModel model,
+            UploadOrSelectServiceRecipientModelValidator validator)
+        {
+            var result = validator.TestValidate(model);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
