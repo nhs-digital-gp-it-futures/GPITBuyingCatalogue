93e00982568d9f2df2c367472f5c92df37bcd06b
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2024-01-19 10:11:26 +0000

    Function app - Gen2 hybrid (#1325)

--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertStandardTypes.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InsertStandardTypes.sql
@@ -1,8 +1,19 @@
-﻿IF NOT EXISTS (SELECT * FROM catalogue.StandardTypes)
-    INSERT INTO catalogue.StandardTypes(Id, [Name])
-    VALUES
+﻿MERGE INTO catalogue.StandardTypes AS TARGET
+USING (
+VALUES
     (1, 'Overarching'),
     (2, 'Interoperability'),
     (3, 'Capability'),
-    (4, 'ContextSpecific');
+    (4, 'Context Specific')
+)
+AS SOURCE([Id], [Name])
+ON TARGET.[Id] = SOURCE.[Id]
+
+WHEN MATCHED AND TARGET.[Name] <> SOURCE.[Name]
+THEN UPDATE SET
+TARGET.[Name] = SOURCE.[Name]
+
+WHEN NOT MATCHED BY TARGET THEN
+INSERT ([Id], [Name])
+VALUES (SOURCE.[Id], SOURCE.[Name]);
 GO
