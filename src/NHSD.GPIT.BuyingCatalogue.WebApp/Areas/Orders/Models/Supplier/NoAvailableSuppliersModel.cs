927e170760a51b55b97f7284bda93a2251780c7a
martinkimmings <63051516+martinkimmings@users.noreply.github.com>
2024-01-25 10:34:25 +0000

    Merger and split journey should handle when we have no suppliers. (#1328)
    * Merger and split journey should handle when we have no suppliers.
    
    * PR feedback.
    
    * content changes

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Models/Supplier/NoAvailableSuppliersModel.cs
@@ -0,0 +1,23 @@
+﻿using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Supplier
+{
+    public class NoAvailableSuppliersModel : OrderingBaseModel
+    {
+        public NoAvailableSuppliersModel(string internalOrgId, CallOffId callOffId, OrderType orderType)
+        {
+            InternalOrgId = internalOrgId;
+            CallOffId = callOffId;
+            OrderTypeText = orderType.Value switch
+            {
+                OrderTypeEnum.AssociatedServiceMerger => "Merger",
+                OrderTypeEnum.AssociatedServiceSplit => "Split",
+                _ => "Unknown",
+            };
+        }
+
+        public CallOffId CallOffId { get; set; }
+
+        public string OrderTypeText { get; set; }
+    }
+}
