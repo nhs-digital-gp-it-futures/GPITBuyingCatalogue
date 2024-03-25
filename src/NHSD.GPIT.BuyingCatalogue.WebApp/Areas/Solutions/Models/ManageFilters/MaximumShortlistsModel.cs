db14509bdbd3a0be7b247724aea17ce15e26e26a
Patrick-M-BJSS <130750948+Patrick-M-BJSS@users.noreply.github.com>
2024-03-25 11:15:38 +0000

    Feature/23422 view all shortlists (#1366)
    * Design changes
    
    * Test work
    
    * Move changes to partial
    
    * Clean up extra card
    
    * Fix links and date format
    
    * Line break
    
    * Changes from pull request
    
    * Change test

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/ManageFilters/MaximumShortlistsModel.cs
@@ -0,0 +1,15 @@
+﻿using NHSD.GPIT.BuyingCatalogue.WebApp.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models.ManageFilters
+{
+    public class MaximumShortlistsModel : NavBaseModel
+    {
+        public MaximumShortlistsModel(
+            string organisationName)
+        {
+            OrganisationName = organisationName;
+        }
+
+        public string OrganisationName { get; set; }
+    }
+}
