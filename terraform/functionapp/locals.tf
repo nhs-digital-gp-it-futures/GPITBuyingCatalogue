a32a4ffe3fb1f3e43775faeaa5faa631ee48d1a3
martinkimmings <63051516+martinkimmings@users.noreply.github.com>
2024-04-18 16:26:02 +0100

    fix: Notification queues on the wrong storage account (#1392)

--- terraform/functionapp/locals.tf
+++ terraform/functionapp/locals.tf
@@ -3,4 +3,6 @@ locals {
   project_environment    = "${var.project}-${var.environment}"
   project_short_code     = substr(var.project, 0, 5)
   project_alt_code       = substr(var.project, 0, 4)
+  send_notification      = "send-email-notification"
+  complete_notification  = "complete-email-notification"
 }
