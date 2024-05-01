3e94ea22d8ebd2db94eb75d096082c4f15d138b2
danielle.sherlock <danielle.sherlock@bjss.com>
2024-05-01 10:41:16 +0100

    Bug fix

--- src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/TagHelpers/GDSPagination/GDSPaginationTagHelper.cs
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/TagHelpers/GDSPagination/GDSPaginationTagHelper.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.WebUtilities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.TagHelpers.GDSPagination
 {
@@ -12,8 +17,7 @@ namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.TagHelpers.GDSPag
     {
         public const string TagHelperName = "govuk-pagination";
 
-        public const string NextUrlName = "next-url";
-        public const string PreviousUrlName = "previous-url";
+        public const string BaseUrlName = "base-url";
         public const string CurrentPageNumberName = "current-page-number";
         public const string TotalNumberOfPagesName = "total-number-of-pages";
 
@@ -21,11 +25,8 @@ namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.TagHelpers.GDSPag
         [ViewContext]
         public ViewContext ViewContext { get; set; }
 
-        [HtmlAttributeName(NextUrlName)]
-        public string NextUrl { get; set; }
-
-        [HtmlAttributeName(PreviousUrlName)]
-        public string PreviousUrl { get; set; }
+        [HtmlAttributeName(BaseUrlName)]
+        public string BaseUrl { get; set; }
 
         [HtmlAttributeName(CurrentPageNumberName)]
         public int CurrentPageNumber { get; set; }
@@ -82,51 +83,44 @@ namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.TagHelpers.GDSPag
 
         private TagBuilder GetPreviousLinkBuilder()
         {
-            if (CurrentPageNumber < 2
-                || CurrentPageNumber == 0
-                || TotalNumberOfPages == 0)
-                return null;
-
-            return GdsPaginationBuilders.GetPreviousArrowBuilder(GeneratePreviousLinkHref());
+            return CurrentPageNumber < 2
+                ? null
+                : GdsPaginationBuilders.GetPreviousArrowBuilder(GeneratePreviousLinkHref());
         }
 
         private TagBuilder GetNextLinkBuilder()
         {
-            if (CurrentPageNumber == 0
-                || TotalNumberOfPages == 0
-                || CurrentPageNumber == TotalNumberOfPages)
-                return null;
-
-            return GdsPaginationBuilders.GetNextArrowBuilder(GenerateNextLinkHref());
+            return CurrentPageNumber == TotalNumberOfPages
+                ? null
+                : GdsPaginationBuilders.GetNextArrowBuilder(GenerateNextLinkHref());
         }
 
         private string GeneratePreviousLinkHref()
         {
-            if (!string.IsNullOrWhiteSpace(PreviousUrl))
-                return PreviousUrl;
-
             return GenerateLinkHrefForPage(CurrentPageNumber - 1);
         }
 
         private string GenerateNextLinkHref()
         {
-            if (!string.IsNullOrWhiteSpace(NextUrl))
-                return NextUrl;
-
             return GenerateLinkHrefForPage(CurrentPageNumber + 1);
         }
 
         private string GenerateLinkHrefForPage(int pageNumber)
         {
-            var builder = new UriBuilder(ViewContext.HttpContext.Request.GetEncodedUrl())
+            string ModifyPageParameter(string url)
             {
-                Query = new QueryBuilder(ViewContext.HttpContext.Request.Query.Where(q => q.Key != "page"))
-                {
-                    { "page", pageNumber.ToString() },
-                }.ToString(),
-            };
+                var builder = new UriBuilder(url);
+                var query = HttpUtility.ParseQueryString(builder.Query);
+                query.Set("page", pageNumber.ToString());
+                builder.Query = query.ToString() ?? string.Empty;
+                return builder.Uri.ToString();
+            }
 
-            return builder.Uri.PathAndQuery;
+            var url = string.IsNullOrWhiteSpace(BaseUrl)
+                ? ViewContext.HttpContext.Request.GetEncodedUrl()
+                : BaseUrl;
+
+            return ModifyPageParameter(url);
         }
     }
 }
