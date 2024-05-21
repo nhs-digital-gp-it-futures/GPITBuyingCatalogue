f18b7d6c8b68822d4b5ceb585d07d95d4fdd0d20
Patrick Maloney <130750948+Patrick-M-BJSS@users.noreply.github.com>
2024-05-21 14:12:50 +0100

    adding page breaks for frameworks

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/SolutionsModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Solutions/Models/SolutionsModel.cs
@@ -10,18 +10,6 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Solutions.Models
             Title = "Catalogue Solutions",
         };
 
-        public static readonly PageTitleModel SearchNoResultsFilterPageTitle = new()
-        {
-            Title = "Catalogue Solutions found",
-            Advice = "These are the Catalogue Solutions that meet your search criteria.",
-        };
-
-        public static readonly PageTitleModel SearchResultsFilterPageTitle = new()
-        {
-            Title = "Catalogue Solutions found",
-            Advice = "These are the Catalogue Solutions currently available for your saved filter.",
-        };
-
         public AdditionalFiltersModel AdditionalFilters { get; set; }
 
         public bool SearchCriteriaApplied => ResultsModel.Filters.SearchCriteriaApplied;
