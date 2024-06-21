2101b3eabbdfe9fc97430d4362d90f952628143b
danielle.sherlock <danielle.sherlock@bjss.com>
2024-06-21 12:34:03 +0100

    Code review

--- src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Catalogue/Configuration/InteropNhsAppIntegrationType.cs
+++ src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Catalogue/Configuration/InteropNhsAppIntegrationType.cs
@@ -5,11 +5,11 @@ namespace NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Configuration;
 public enum InteropNhsAppIntegrationType
 {
     [Display(Name = "Online Consultations")]
-    Online_Consultations,
+    Online_Consultations = 0,
     [Display(Name = "Personal Health Records")]
-    Personal_Health_Records,
+    Personal_Health_Records = 1,
     [Display(Name = "Primary Care Notifications")]
-    Primary_Care_Notifications,
+    Primary_Care_Notifications = 2,
     [Display(Name = "Secondary Care Notifications")]
-    Secondary_Care_Notifications,
+    Secondary_Care_Notifications = 3,
 }
