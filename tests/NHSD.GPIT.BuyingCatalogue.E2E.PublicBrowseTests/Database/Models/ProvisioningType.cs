10a21bbe421f78c3b9cabe7ca57ee093b71f891b
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2024-07-02 09:04:49 +0100

    Remove integration tests project

--- tests/NHSD.GPIT.BuyingCatalogue.E2E.PublicBrowseTests/Database/Models/ProvisioningType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NHSD.GPIT.BuyingCatalogue.E2ETests.Database.Models
-{
-    public sealed class ProvisioningType
-    {
-        public int Id { get; init; }
-
-        public string Name { get; init; }
-    }
-}
