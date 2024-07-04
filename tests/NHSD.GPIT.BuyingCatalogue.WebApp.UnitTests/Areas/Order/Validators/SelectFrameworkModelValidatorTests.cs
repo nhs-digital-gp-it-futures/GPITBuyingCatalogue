bc866c84e59b557aee170653af6d08053cf0775d
danielle.sherlock <danielle.sherlock@bjss.com>
2024-07-04 10:55:06 +0100

    Unit test fix

--- tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Validators/SelectFrameworkModelValidatorTests.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Areas/Order/Validators/SelectFrameworkModelValidatorTests.cs
@@ -1,7 +1,10 @@
-﻿using FluentValidation.TestHelper;
+﻿using AutoFixture.Xunit2;
+using FluentValidation.TestHelper;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Frameworks;
 using NHSD.GPIT.BuyingCatalogue.UnitTest.Framework.Attributes;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.OrderTriage;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Validators;
+using NSubstitute;
 using Xunit;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Validators
@@ -24,10 +27,30 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Areas.Order.Validators
 
         [Theory]
         [MockAutoData]
-        public static void Validate_SelectedFramework_NoModelError(
+        public static void Validate_ExpiredFramework_SetsModelError(
             SelectFrameworkModel model,
+            EntityFramework.Catalogue.Models.Framework framework,
+            [Frozen] IFrameworkService frameworkService,
             SelectFrameworkModelValidator validator)
         {
+            framework.IsExpired = true;
+            frameworkService.GetFramework(model.SelectedFrameworkId).Returns(framework);
+            var result = validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(m => m.SelectedFrameworkId)
+                .WithErrorMessage(SelectFrameworkModelValidator.FrameworkExpiredErrorMessage);
+        }
+
+        [Theory]
+        [MockAutoData]
+        public static void Validate_SelectedFramework_NoModelError(
+            SelectFrameworkModel model,
+            EntityFramework.Catalogue.Models.Framework framework,
+            [Frozen] IFrameworkService frameworkService,
+            SelectFrameworkModelValidator validator)
+        {
+            framework.IsExpired = false;
+            frameworkService.GetFramework(model.SelectedFrameworkId).Returns(framework);
             var result = validator.TestValidate(model);
 
             result.ShouldNotHaveAnyValidationErrors();
