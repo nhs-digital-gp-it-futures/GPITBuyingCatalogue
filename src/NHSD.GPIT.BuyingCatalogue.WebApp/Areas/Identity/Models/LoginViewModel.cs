562d3f843b24759ef23e508bc6455a9aed9cf9ab
danielle.sherlock <danielle.sherlock@bjss.com>
2024-07-04 13:25:22 +0100

    Fix ordering

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Identity/Models/LoginViewModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Identity/Models/LoginViewModel.cs
@@ -21,17 +21,16 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Identity.Models
 
         public string DisabledError { get; set; }
 
+#pragma warning disable SA1300
+        // Required to force recaptcha errors to appear in the correct order
+        public string recaptcha { get; set; }
+#pragma warning restore SA1300
+
         public static class ErrorMessages
         {
             public const string EmailAddressRequired = "Enter your email address";
             public const string EmailAddressInvalid = "Enter a valid email address";
             public const string PasswordRequired = "Enter your password";
         }
-
-#pragma warning disable SA1201
-#pragma warning disable SA1300
-        public string recaptcha { get; set; }
-#pragma warning restore SA1201
-#pragma warning restore SA1300
     }
 }
