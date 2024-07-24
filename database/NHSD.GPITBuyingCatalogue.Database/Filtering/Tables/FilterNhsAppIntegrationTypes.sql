3f31ebc0420bb14df06d6fb525bfc16dd23ac236
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2024-07-24 13:39:59 +0100

    Database cleanup (#1454)

--- database/NHSD.GPITBuyingCatalogue.Database/Filtering/Tables/FilterNhsAppIntegrationTypes.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE filtering.FilterNhsAppIntegrationTypes -- TODO: Remove in next release
-(
-	Id int IDENTITY(1, 1) NOT NULL,
-    FilterId int NOT NULL,
-    NhsAppIntegrationsTypeId int NOT NULL,
-    CONSTRAINT PK_FilterNhsAppIntegrationsTypes PRIMARY KEY (Id),
-    CONSTRAINT FK_FilterNhsAppIntegrationsTypes_Filter FOREIGN KEY (FilterId) REFERENCES filtering.Filters(Id),
-)
