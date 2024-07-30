2214b19b9fce7bda290e1ca0cc8cd17fd1b4a978
danielle.sherlock <danielle.sherlock@bjss.com>
2024-07-30 10:46:18 +0100

    Code coverage exclusion

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Validators/Contracts/DeliveryDates/EditDatesModelValidator.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Validators/Contracts/DeliveryDates/EditDatesModelValidator.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using FluentValidation;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Contracts.DeliveryDates;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Validators.Contracts.DeliveryDates
 {
+    [ExcludeFromCodeCoverage(Justification = "Configures a sub-validator to use on a property and doesn't have validation logic")]
     public class EditDatesModelValidator : AbstractValidator<EditDatesModel>
     {
         public EditDatesModelValidator()
--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Validators/Contracts/DeliveryDates/RecipientLocationModelValidator.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Validators/Contracts/DeliveryDates/RecipientLocationModelValidator.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using FluentValidation;
 using NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Models.Contracts.DeliveryDates;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Validators.Contracts.DeliveryDates
 {
+    [ExcludeFromCodeCoverage(Justification = "Configures a sub-validator to use on a property and doesn't have validation logic")]
     public class RecipientLocationModelValidator : AbstractValidator<KeyValuePair<string, RecipientDateModel[]>>
     {
         public RecipientLocationModelValidator()
