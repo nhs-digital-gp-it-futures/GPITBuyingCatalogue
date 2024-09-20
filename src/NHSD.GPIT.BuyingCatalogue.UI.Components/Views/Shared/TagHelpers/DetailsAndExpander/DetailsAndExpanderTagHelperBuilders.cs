5572ae61d3d2396cc06d536030ec0134ae6bf871
dssherlock <113588987+dssherlock@users.noreply.github.com>
2024-09-20 17:03:00 +0100

    Feature/23803 Select Service Recipient page improvements (#1476)

--- src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/TagHelpers/DetailsAndExpander/DetailsAndExpanderTagHelperBuilders.cs
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/TagHelpers/DetailsAndExpander/DetailsAndExpanderTagHelperBuilders.cs
@@ -131,7 +131,7 @@ namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.TagHelpers.Detail
             return summaryBuilder;
         }
 
-        public static TagBuilder GetSummaryLabelBuilderWithSecondaryInformation(string labelText, string secondaryTextTitle, string secondaryText, string secondaryUrlTitle, string secondaryUrl, bool bold, bool addedSticker = false)
+        public static TagBuilder GetSummaryLabelBuilderWithSecondaryInformation(string labelText, string secondaryTextTitle, string secondaryText, string secondaryUrlTitle, string secondaryUrl, bool bold, bool addedSticker = false, bool jsOnly = false)
         {
             var summaryBuilder = new TagBuilder("summary");
             summaryBuilder.AddCssClass(DetailsSummaryClass);
@@ -162,6 +162,10 @@ namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.TagHelpers.Detail
             }
 
             var summaryClass = secondaryUrlTitle is not null ? SummaryTextSecondaryUrlClass : SummaryTextSecondaryClass;
+
+            if (jsOnly)
+                summaryClass += $" {TagHelperConstants.NhsJsOnly}";
+
             var secondarySpanBuilder = new TagBuilder(TagHelperConstants.Span);
             secondarySpanBuilder.AddCssClass(summaryClass);
 
---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Models/Shared/SublocationModelTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.Quantities;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.ServiceRecipientModels;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Models.Shared;
+
+public static class SublocationModelTests
+{
+    [Theory]
+    [MockAutoData]
+    public static void Constructor_SetsPropertiesAsExpected(
+        string name,
+        List<ServiceRecipientModel> serviceRecipients)
+    {
+        var model = new SublocationModel(
+            name,
+            serviceRecipients);
+
+        model.Name.Should().Be(name);
+        model.ServiceRecipients.Should().BeEquivalentTo(serviceRecipients);
+    }
+
+    [Theory]
+    [MockInlineAutoData(true)]
+    [MockInlineAutoData(false)]
+    public static void AllRecipientsSelected_SetsPropertiesAsExpected(
+        bool selected,
+        string name,
+        List<ServiceRecipientModel> serviceRecipients)
+    {
+        serviceRecipients.ForEach(x => x.Selected = selected);
+        var model = new SublocationModel(
+            name,
+            serviceRecipients);
+
+        model.Name.Should().Be(name);
+        model.ServiceRecipients.Should().BeEquivalentTo(serviceRecipients);
+        model.AllRecipientsSelected.Should().Be(selected);
+    }
+}
