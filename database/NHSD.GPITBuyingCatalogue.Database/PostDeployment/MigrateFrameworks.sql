b1acb7d2e18246ef82566349cda4c90fd9f8963d
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2024-10-18 12:20:58 +0100

    Migrate standards to new standard type format

--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/MigrateFrameworks.sql
+++
@@ -1,5 +0,0 @@
-IF NOT EXISTS(SELECT 1 FROM catalogue.Frameworks WHERE MaximumTerm IS NOT NULL)
-BEGIN
-  UPDATE catalogue.Frameworks
-    SET MaximumTerm = 36
-END
--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/MigrateOrderItemRecipients.sql
+++
@@ -1,7 +0,0 @@
-﻿IF NOT EXISTS(SELECT 1 FROM ordering.OrderRecipients)
-BEGIN
-    INSERT INTO [ordering].[OrderRecipients] (OrderId, OdsCode)
-    SELECT DISTINCT OrderId, OdsCode FROM ordering.OrderItemRecipients
-
-    DELETE FROM [ordering].[OrderItemRecipients] WHERE Quantity IS NULL AND DeliveryDate IS NULL
-END
