4870d0af154a2c024fad2b5315c891f879c0b931
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2024-12-11 10:39:44 +0000

    Update Terraform versions

--- terraform/core/provider.tf
+++ terraform/core/provider.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.10.1"
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.2.0"
+      version = "=4.13.0"
     }
   }
 
