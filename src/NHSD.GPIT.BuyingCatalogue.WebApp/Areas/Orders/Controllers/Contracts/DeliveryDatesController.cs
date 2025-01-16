7830ef605a2ea3cf3339f8c5151c57211d70ac4d
Ashley Winter <ashley.winter@bjss.com>
2025-01-16 14:11:56 +0000

    23996 - use rollup for previous date

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Controllers/Contracts/DeliveryDatesController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Controllers/Contracts/DeliveryDatesController.cs
@@ -44,7 +44,7 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Controllers.Contracts
         {
             var orderWrapper = await orderService.GetOrderThin(callOffId, internalOrgId);
 
-            var originalOrder = orderWrapper.First;
+            var originalOrder = orderWrapper.RolledUp;
 
             var order = orderWrapper.Order;
 
