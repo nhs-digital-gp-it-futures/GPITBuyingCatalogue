0b6c1fb37752c5bc702fdb38ab778c6653c5dcf1
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2025-01-27 10:30:08 +0000

    Import build agent vnet

--- terraform/webapp/import_acr.tf
+++ terraform/webapp/import_acr.tf
@@ -3,3 +3,16 @@ data "azurerm_container_registry" "acr" {
   name                = "gpitfuturesdevacr"
   resource_group_name = "gpitfutures-dev-rg-acr"
 }
+
+data "azurerm_virtual_network" "infrastructure_vnet" {
+  name                = "${var.project}-infra-vnet"
+  resource_group_name = "${var.project}-rg-sa"
+  provider            = azurerm.infrastructure
+}
+
+data "azurerm_subnet" "default-subnet" {
+  resource_group_name  = data.azurerm_virtual_network.infrastructure_vnet.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.infrastructure_vnet.name
+  name                 = "default"
+  provider             = azurerm.infrastructure
+}
