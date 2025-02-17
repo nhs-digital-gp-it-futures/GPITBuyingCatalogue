dc2444b99cb623dec9446b38cba8f30307b87fe4
Ashley Winter <137426818+ashleywntrbjss@users.noreply.github.com>
2025-02-17 09:47:46 +0000

    Feature/24038 search modal accessibility improvements (#1524)
    * Prevent close on enter keydown
    
    * Move advice to header
    
    * Improve header
    
    * Better search bar
    
    * Remove background overhang
    
    * Update indent padding
    
    * Remove excess header padding
    
    * Limit overflow to y to prevent space lost to thick webkit scrollbar
    
    * 24038 - add announcement for no search results
    
    * 24038 - accessible announcements for modal searching
    
    * 24038 - refactor
    
    * 24038 - Ensure headers persist in results
    
    * 24038 - use sub group to hide headers (not functional)
    
    * 24038 - Ensure headers persist in results - partially working
    
    * 24038 - refactor hide method
    
    * 24038 - Hide headers working
    
    * 24038 - add subgroup params for epics searching
    
    * 24038 - Refactor display logic

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/ts/ModalSearch.ts
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/wwwroot/ts/ModalSearch.ts
@@ -1,53 +1,63 @@
-class modalSearchConfig {
-    noRecordsFound: HTMLElement;
+class ModalSearchConfig {
+    recordsFound: HTMLElement;
+    notFoundText: string;
     dialog: HTMLDialogElement;
     showDialogButton: HTMLElement;
     searchInput: HTMLInputElement;
     applyCallback: () => void;
     dialogId: string;
+    debounceTime: number = 300;
+
     constructor(dialogId: string,
         showDialogButtonId: string,
         applyCallback: () => void,
         shouldClearSearch: boolean,
         shouldClearSelection: boolean,
-        tableContent: () => Promise<string>) {
+        tableContent: () => Promise<string>,
+        notFoundText: string) {
         this.dialogId = dialogId;
         this.dialog = document.getElementById(dialogId) as HTMLDialogElement;
         this.showDialogButton = document.getElementById(showDialogButtonId);
         this.applyCallback = applyCallback;
         this.searchInput = document.getElementById(dialogId + "-filter-term") as HTMLInputElement;
-        this.noRecordsFound = document.getElementById(dialogId + "-no-records-found");
+        this.recordsFound = document.getElementById(dialogId + "-records-found");
+        this.notFoundText = notFoundText;
 
-        this.showDialogButton.addEventListener("click", async (event: Event) => {
+        this.showDialogButton.addEventListener("click", async () => {
             if (tableContent != null) {
-                var content = await tableContent();
-                var tableContainer = document.getElementById(this.dialogId + "-search-table");
+                const content = await tableContent();
+                const tableContainer = document.getElementById(this.dialogId + "-search-table");
                 tableContainer.innerHTML = content;
-                this.noRecordsFound.style.display = "none";
             }
 
             this.dialog.showModal();
         });
 
+        document.addEventListener("keypress", (event) => this.enterKeyEventListener(event))
+
         this.dialog.addEventListener('close', () => {
             if (this.dialog.returnValue === 'apply') { applyCallback() }
             if (shouldClearSearch) { this.clearSearch() }
             if (shouldClearSelection) { this.clearSelection() }
+            document.removeEventListener('keypress', (event) => this.enterKeyEventListener(event))
         });
 
-        this.searchInput.addEventListener("input", () => this.tableSearch());
+        this.searchInput.addEventListener("input", this.debounce(this.tableSearch, this.debounceTime));
     }
 
     tableSearch() {
-        var searchTerm = this.searchInput.value.toLowerCase();
-        var tableContainer = document.getElementById(this.dialogId + "-search-table");
-        var table = tableContainer.getElementsByTagName("table")[0];
-        var rows = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+        const searchTerm = this.searchInput.value.toLowerCase();
+        const tableContainer = document.getElementById(this.dialogId + "-search-table");
+        const table = tableContainer.getElementsByTagName("table")[0];
+        const rows = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
 
-        let hasMatch = false;
+        let matches = 0;
 
         for (let i = 0; i < rows.length; i++) {
             const row = rows[i];
+
+            let isHeader = row.getElementsByTagName("th").length !== 0;
+
             const columns = row.getElementsByTagName("td");
             let rowMatch = false;
 
@@ -61,17 +71,51 @@ class modalSearchConfig {
 
             if (rowMatch) {
                 row.style.display = "";
-                hasMatch = true;
+                matches++;
             } else {
-                row.style.display = "none";
+                if(!isHeader) {
+                    row.style.display = "none";
+                }
             }
         }
 
-        if (hasMatch) {
-            this.noRecordsFound.style.display = "none";
+        this.recordsFound.style.display = "block";
+
+        if (matches > 0) {
+            this.recordsFound.innerText = `${matches} ${matches === 1 ? 'result' : 'results'} found`;
         } else {
-            this.noRecordsFound.style.display = "block";
+            this.recordsFound.innerText = this.notFoundText;
         }
+
+        this.hideEmptyHeaders();
+    }
+
+    hideEmptyHeaders = () => {
+        let allSubGroupElements = document.querySelectorAll('[subgroup]')
+
+        let subGroupSet: Array<string> = [];
+
+        allSubGroupElements.forEach(element => {
+            let elementSubgroup = element.getAttribute("subgroup");
+            if (!subGroupSet.includes(elementSubgroup)) {
+                subGroupSet.push(elementSubgroup);
+            }
+        })
+
+        subGroupSet.forEach(subGroupName => {
+                let spanElementsInSubgroup = document.querySelectorAll(`span[subgroup=${subGroupName}]`);
+
+                let inputElementsInSubgroup = document.querySelectorAll(`input[subgroup=${subGroupName}]`);
+
+                let visibleInputElementsInSubgroup = Array.from(inputElementsInSubgroup).filter((el) => {
+                    return el.checkVisibility({checkVisibilityCSS: true});
+                });
+
+                let liveElement = document.getElementById(spanElementsInSubgroup[0].id)
+
+                liveElement.parentElement.parentElement.style.display = visibleInputElementsInSubgroup.length === 0 ? "none" : "";
+            }
+        )
     }
 
     clearSearch() {
@@ -80,9 +124,25 @@ class modalSearchConfig {
     }
 
     clearSelection() {
-        var checkedboxes = document.querySelectorAll('.modal-checkbox:checked');
-        checkedboxes.forEach(function (item) {
+        let checkedBoxes = document.querySelectorAll('.modal-checkbox:checked');
+        checkedBoxes.forEach(function (item) {
             (item as HTMLInputElement).checked = false;
         });
     }
+
+    enterKeyEventListener(event: KeyboardEvent) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+        }
+    }
+
+    debounce(func: () => void, delay: number) {
+        let timeout: ReturnType<typeof setTimeout>;
+
+        return () => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this), delay);
+        };
+    }
+
 }
