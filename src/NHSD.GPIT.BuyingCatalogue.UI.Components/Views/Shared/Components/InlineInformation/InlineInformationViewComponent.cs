c56acf69b465cceac0edd8567e0f40746c6a10f1
Ashley Winter <ashley.winter@bjss.com>
2025-02-25 14:59:50 +0000

    24064 - add view component for information

---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/InlineInformation/InlineInformationViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.InlineInformation
+{
+    public sealed class InlineInformationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string text)
+        {
+            var model = new InlineInformationModel { Text = text };
+
+            return await Task.FromResult(View("InlineInformation", model));
+        }
+    }
+}
