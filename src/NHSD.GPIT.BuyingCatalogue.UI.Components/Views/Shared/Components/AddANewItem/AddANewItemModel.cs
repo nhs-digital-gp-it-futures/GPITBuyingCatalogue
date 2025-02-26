53715520fb02ab8b44e08e940a149b7d0ba1be76
Ashley Winter <ashley.winter@bjss.com>
2025-02-26 11:10:48 +0000

    24064 - add a new item view component

---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/AddANewItem/AddANewItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.AddANewItem
+{
+    public sealed class AddANewItemModel
+    {
+        public string Href { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/AddANewItem/AddANewItemViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.AddANewItem
+{
+    public sealed class AddANewItemViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string href, string text)
+        {
+            var model = new AddANewItemModel { Href = href, Text = text };
+
+            return await Task.FromResult(View("AddANewItem", model));
+        }
+    }
+}
