ac9179c3e9502681c1c32594fdefa9da3d1761a6
Ashley Winter <ashley.winter@bjss.com>
2025-02-26 15:56:24 +0000

    24064 - add change an item

---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Views/Shared/Components/ChangeAnItem/ChangeAnItemViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.Components.ChangeAnItem;
+
+public sealed class ChangeAnItemViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(string href, string text)
+    {
+        var model = new ChangeAnItemModel { Href = href, Text = text };
+
+        return await Task.FromResult(View("ChangeAnItem", model));
+    }
+}
