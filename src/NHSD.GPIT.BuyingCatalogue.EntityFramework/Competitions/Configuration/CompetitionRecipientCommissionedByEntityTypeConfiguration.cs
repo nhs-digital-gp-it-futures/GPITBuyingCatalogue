2068185a2c173f295014817c0bbba358c068c63d
Ashley Winter <ashley.winter@bjss.com>
2025-03-04 15:49:14 +0000

    24065 - add competitionrecipientcomissionedby entity config and model

---
+++ src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Competitions/Configuration/CompetitionRecipientCommissionedByEntityTypeConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Competitions.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.EntityFramework.Competitions.Configuration;
+
+public class
+    CompetitionRecipientCommissionedByEntityTypeConfiguration : IEntityTypeConfiguration<
+    CompetitionRecipientCommissionedBy>
+{
+    public void Configure(EntityTypeBuilder<CompetitionRecipientCommissionedBy> builder)
+    {
+        builder.ToTable("CompetitionRecipientCommissionedBy", Schemas.Competitions);
+
+        builder.HasKey(x => new { x.CompetitionId, x.RecipientOdsCode });
+
+        builder.Property(x => x.CompetitionId).IsRequired();
+
+        builder.Property(x => x.CommissionedByOdsCode).IsRequired();
+
+        builder.HasOne(x => x.Competition)
+            .WithMany()
+            .HasForeignKey(x => x.CompetitionId)
+            .HasConstraintName("FK_CompetitionRecipientCommissionedBy_Competitions")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.CompetitionRecipient)
+            .WithMany()
+            .HasForeignKey(x => new { x.CompetitionId, x.RecipientOdsCode })
+            .HasConstraintName("FK_CompetitionRecipientCommissionedBy_CompetitionRecipients")
+            .OnDelete(DeleteBehavior.NoAction);
+
+        builder.HasOne(x => x.CommissionedByCompetitionSublocations)
+            .WithMany()
+            .HasForeignKey(x => new { x.CompetitionId, x.CommissionedByOdsCode })
+            .HasConstraintName("FK_CompetitionRecipientCommissionedBy_CompetitionSublocations")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // TODO: Add OdsOrganisation entities for recipient and sublocation
+    }
+}
---
+++ src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Competitions/Models/CompetitionRecipientCommissionedBy.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NHSD.GPIT.BuyingCatalogue.EntityFramework.Competitions.Models;
+
+public record CompetitionRecipientCommissionedBy
+{
+    public int CompetitionId { get; set; }
+
+    [MaxLength(10)]
+    public string RecipientOdsCode { get; set; }
+
+    [MaxLength(10)]
+    public string CommissionedByOdsCode { get; set; }
+
+    public Competition Competition { get; set; }
+
+    public CompetitionRecipient CompetitionRecipient { get; set; }
+
+    public CompetitionSublocations CommissionedByCompetitionSublocations { get; set; }
+}
