10aef3c47c9ace70ea4d918e125c1f46383a9eaf
Ashley Winter <ashley.winter@bjss.com>
2025-03-04 12:40:07 +0000

    24065 - update constraints

--- database/NHSD.GPITBuyingCatalogue.Database/Competitions/Tables/CompetitionRecipients.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Competitions/Tables/CompetitionRecipients.sql
@@ -3,6 +3,7 @@
     [CompetitionId] INT NOT NULL,
     [OdsCode] NVARCHAR(10) NOT NULL,
     CONSTRAINT PK_CompetitionRecipients PRIMARY KEY ([CompetitionId], [OdsCode]),
-    CONSTRAINT FK_CompetitionRecipients_Competition FOREIGN KEY ([CompetitionId]) REFERENCES [competitions].[Competitions] (Id),
-    CONSTRAINT FK_CompetitionRecipients_ServiceRecipient FOREIGN KEY ([OdsCode]) REFERENCES [ods_organisations].[OdsOrganisations] (Id)
+    CONSTRAINT FK_CompetitionRecipients_Competition FOREIGN KEY ([CompetitionId]) REFERENCES [competitions].[Competitions] ([Id]),
+    CONSTRAINT FK_CompetitionRecipients_ServiceRecipient FOREIGN KEY ([OdsCode]) REFERENCES [ods_organisations].[OdsOrganisations] ([Id]),
+    CONSTRAINT FK_CompetitionRecipients_CompetitionRecipientsComissionedBy FOREIGN KEY ([CompetitionId], [OdsCode]) REFERENCES [competitions].[CompetitonRecipientCommissionedBy] ([CompetitionId],[RecipientOdsCode])
 );
--- database/NHSD.GPITBuyingCatalogue.Database/Competitions/Tables/CompetitonRecipientCommissionedBy.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Competitions/Tables/CompetitonRecipientCommissionedBy.sql
@@ -6,5 +6,5 @@ CREATE TABLE [competitions].[CompetitonRecipientCommissionedBy]
     CONSTRAINT PK_CompetitonRecipientCommissionedBy PRIMARY KEY ([CompetitionId],[RecipientOdsCode]),
     CONSTRAINT FK_CompetitonRecipientCommissionedBy_Competitions FOREIGN KEY ([CompetitionId]) REFERENCES [competitions].[Competitions] ([Id]) ON DELETE CASCADE,
     CONSTRAINT FK_CompetitonRecipientCommissionedBy_OdsOrganisations_Recipient FOREIGN KEY ([RecipientOdsCode]) REFERENCES [ods_organisations].[OdsOrganisations]([Id]),
-    CONSTRAINT FK_CompetitonRecipientCommissionedBy_CompetitionSublocationOwners FOREIGN KEY ([CommissionedByOdsCode]) REFERENCES [competitions].[CompetitionSublocationOwners]([SublocationOdsCode])
+    CONSTRAINT FK_CompetitonRecipientCommissionedBy_CompetitionSublocationOwners FOREIGN KEY ([CompetitionId], [CommissionedByOdsCode]) REFERENCES [competitions].[CompetitionSublocationOwners]([CompetitionId],[SublocationOdsCode])
 )
