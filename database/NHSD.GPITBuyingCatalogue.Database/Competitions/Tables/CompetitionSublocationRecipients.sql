9467014278ac051efb741fa5eb0efc1718ce4c0f
Ashley Winter <ashley.winter@bjss.com>
2025-03-05 11:47:02 +0000

    24065 - remove link to recipients

--- database/NHSD.GPITBuyingCatalogue.Database/Competitions/Tables/CompetitionSublocationRecipients.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Competitions/Tables/CompetitionSublocationRecipients.sql
@@ -5,7 +5,6 @@ CREATE TABLE [competitions].[CompetitionSublocationRecipients]
     [ParentSublocationOdsCode] nvarchar(10) NOT NULL,
     CONSTRAINT PK_CompetitionSublocationRecipients PRIMARY KEY ([CompetitionId],[RecipientOdsCode]),
     CONSTRAINT FK_CompetitionSublocationRecipients_Competitions FOREIGN KEY ([CompetitionId]) REFERENCES [competitions].[Competitions]([Id]) ON DELETE CASCADE,
-    CONSTRAINT FK_CompetitionSublocationRecipients_CompetitionRecipients FOREIGN KEY ([CompetitionId], [RecipientOdsCode]) REFERENCES [competitions].[CompetitionRecipients]([CompetitionId],[OdsCode]) ON DELETE CASCADE,
     CONSTRAINT FK_CompetitionSublocationRecipients_CompetitionSublocations FOREIGN KEY ([CompetitionId], [ParentSublocationOdsCode]) REFERENCES [competitions].[CompetitionSublocations]([CompetitionId],[SublocationOdsCode]),
     CONSTRAINT FK_CompetitionSublocationRecipients_OdsOrganisations_Recipient FOREIGN KEY ([RecipientOdsCode]) REFERENCES [ods_organisations].[OdsOrganisations]([Id]),
     CONSTRAINT FK_CompetitionSublocationRecipients_OdsOrganisations_ParentSublocation FOREIGN KEY ([ParentSublocationOdsCode]) REFERENCES [ods_organisations].[OdsOrganisations]([Id])
