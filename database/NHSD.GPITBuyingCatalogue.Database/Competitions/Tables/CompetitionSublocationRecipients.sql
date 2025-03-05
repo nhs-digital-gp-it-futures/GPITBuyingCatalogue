480bcee66994dcab33c4b2fd2c97d608687dc0a2
Ashley Winter <ashley.winter@bjss.com>
2025-03-05 11:45:22 +0000

    24065 - Rename to sublocation recipients

---
+++ database/NHSD.GPITBuyingCatalogue.Database/Competitions/Tables/CompetitionSublocationRecipients.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [competitions].[CompetitionSublocationRecipients]
+(
+    [CompetitionId] int NOT NULL,
+    [RecipientOdsCode] nvarchar(10) NOT NULL,
+    [ParentSublocationOdsCode] nvarchar(10) NOT NULL,
+    CONSTRAINT PK_CompetitionSublocationRecipients PRIMARY KEY ([CompetitionId],[RecipientOdsCode]),
+    CONSTRAINT FK_CompetitionSublocationRecipients_Competitions FOREIGN KEY ([CompetitionId]) REFERENCES [competitions].[Competitions]([Id]) ON DELETE CASCADE,
+    CONSTRAINT FK_CompetitionSublocationRecipients_CompetitionRecipients FOREIGN KEY ([CompetitionId], [RecipientOdsCode]) REFERENCES [competitions].[CompetitionRecipients]([CompetitionId],[OdsCode]) ON DELETE CASCADE,
+    CONSTRAINT FK_CompetitionSublocationRecipients_CompetitionSublocations FOREIGN KEY ([CompetitionId], [ParentSublocationOdsCode]) REFERENCES [competitions].[CompetitionSublocations]([CompetitionId],[SublocationOdsCode]),
+    CONSTRAINT FK_CompetitionSublocationRecipients_OdsOrganisations_Recipient FOREIGN KEY ([RecipientOdsCode]) REFERENCES [ods_organisations].[OdsOrganisations]([Id]),
+    CONSTRAINT FK_CompetitionSublocationRecipients_OdsOrganisations_ParentSublocation FOREIGN KEY ([ParentSublocationOdsCode]) REFERENCES [ods_organisations].[OdsOrganisations]([Id])
+)
