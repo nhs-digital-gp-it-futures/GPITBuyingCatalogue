534c4ed946cb2ec250115b377bc0167137bc2c98
Ashley Winter <ashley.winter@bjss.com>
2025-03-10 14:54:32 +0000

    24065 - add new competitionsublocation repo

---
+++ src/NHSD.GPIT.BuyingCatalogue.ServiceContracts/Competitions/ICompetitionSublocationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Competitions.Models;
+
+namespace NHSD.GPIT.BuyingCatalogue.ServiceContracts.Competitions
+{
+    public interface ICompetitionSublocationService
+    {
+        Task<CompetitionSublocation> GetCompetitionSublocationWithRecipients(string internalOrgId, int competitionId);
+    }
+}
---
+++ src/NHSD.GPIT.BuyingCatalogue.Services/Competitions/CompetitionSublocationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Competitions.Models;
+using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Competitions;
+
+namespace NHSD.GPIT.BuyingCatalogue.Services.Competitions
+{
+    public class CompetitionSublocationService(BuyingCatalogueDbContext dbContext) : ICompetitionSublocationService
+    {
+        private readonly BuyingCatalogueDbContext dbContext =
+            dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+        public async Task<CompetitionSublocation> GetCompetitionSublocationWithRecipients(
+            string internalOrgId,
+            int competitionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
