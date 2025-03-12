a3c55226c5ce9b55de81d77303a043c9e897e629
Ashley Winter <ashley.winter@bjss.com>
2025-03-12 13:37:10 +0000

    24065 - implement form content binder and filter

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/FormContent/FilterFormContentAttribute.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.FormContent;
+
+public class FilteredFormContentAttribute() : ModelBinderAttribute(typeof(FilteredFormContentBinder));
---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/FormContent/FilteredFormContentBinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.FormContent
+{
+    public class FilteredFormContentBinder : IModelBinder
+    {
+        private const string RequestVerificationTokenKey = "__RequestVerificationToken";
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var formData = bindingContext.HttpContext.Request.Form;
+            var filteredFormData = formData.Where(kvp => kvp.Key != RequestVerificationTokenKey)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToString());
+
+            bindingContext.Result = ModelBindingResult.Success(filteredFormData);
+            return Task.CompletedTask;
+        }
+    }
+
+}
