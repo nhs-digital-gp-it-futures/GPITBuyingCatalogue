b7377a2f719f16ed21cfc6ba4546ee8473fda51a
Ashley Winter <ashley.winter@bjss.com>
2025-03-17 14:25:29 +0000

    24065 - update name and value model

--- src/NHSD.GPIT.BuyingCatalogue.UI.Components/Models/CheckboxNameAndValueModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/Models/CheckboxNameAndValueModel.cs
@@ -5,7 +5,7 @@ public record CheckboxNameAndValueModel
     public const bool Selected = true;
     public const bool NotSelected = false;
 
-    public string Name { get; set; }
+    public string Name { get; init; }
 
-    public bool Value { get; set; }
+    public bool Value { get; init; }
 }
