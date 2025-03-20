96b8e72f3e433fafb7288d299060c3fe2c30e948
Ashley Winter <ashley.winter@bjss.com>
2025-03-20 09:22:11 +0000

    24065 - remove form content attribute

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/FormContent/FilterFormContentAttribute.cs
+++
@@ -1,5 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace NHSD.GPIT.BuyingCatalogue.WebApp.FormContent;
-
-public class FilteredFormContentAttribute() : ModelBinderAttribute(typeof(FilteredFormContentBinder));
