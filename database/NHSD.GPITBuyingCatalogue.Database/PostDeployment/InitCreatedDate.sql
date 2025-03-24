b01b8f15f860710d1cceb0d8f4ae8934e5e84444
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2025-03-24 10:10:16 +0000

    Remove created date post-deployment script (#1530)

--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/InitCreatedDate.sql
+++
@@ -1,9 +0,0 @@
-﻿DECLARE @UsersDateMap AS TABLE ([ID] INT, [Created] DATETIME2);
-
-INSERT INTO @UsersDateMap
-SELECT Id, MIN(SysStartTime) As Created FROM users.AspNetUsers FOR SYSTEM_TIME ALL GROUP BY ID ORDER BY ID ASC
-
-UPDATE ANU
-SET ANU.Created = UDM.Created
-FROM users.AspNetUsers ANU
-INNER JOIN @UsersDateMap UDM ON ANU.Id = UDM.Id
