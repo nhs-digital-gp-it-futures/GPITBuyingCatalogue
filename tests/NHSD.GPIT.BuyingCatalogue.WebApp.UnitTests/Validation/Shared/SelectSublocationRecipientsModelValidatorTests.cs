1730f779441d5aa053692978516c83315f184a95
Ashley Winter <ashley.winter@bjss.com>
2025-04-01 16:54:56 +0100

    24065 - add test outlines - validators

---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Validation/Shared/SelectSublocationRecipientsModelValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.ServiceRecipientModels;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Validation.Shared.ServiceRecipients;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Validation.Shared
+{
+    public static class SelectSublocationRecipientsModelValidatorTests
+    {
+        [Theory]
+        [MockAutoData]
+        public static void Validate_NotApplicable_NoValidationErrors(
+            SelectSublocationRecipientsModel model,
+            SelectSublocationRecipientsModelValidator systemUnderTest)
+        {
+            Assert.Fail("not implemented");
+        }
+
+        [Theory]
+        [MockAutoData]
+        public static void Validate_SelectionMade_NoValidationErrors(
+            SelectSublocationRecipientsModel model,
+            SelectSublocationRecipientsModelValidator systemUnderTest)
+        {
+            Assert.Fail("not implemented");
+        }
+
+        [Theory]
+        [MockAutoData]
+        public static void Validate_NoSelectionMade_ThrowsValidationError(
+            SelectSublocationRecipientsModel model,
+            SelectSublocationRecipientsModelValidator systemUnderTest)
+        {
+            Assert.Fail("not implemented");
+        }
+    }
+}
---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Validation/Shared/SelectSublocationsModelValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.ServiceRecipientModels;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Validation.Shared.ServiceRecipients;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Validation.Shared
+{
+    public static class SelectSublocationsModelValidatorTests
+    {
+        [Theory]
+        [MockAutoData]
+        public static void Validate_NotApplicable_NoValidationErrors(
+            SelectSublocationsModel model,
+            SelectSublocationsModelValidator systemUnderTest)
+        {
+            Assert.Fail("not implemented");
+        }
+
+        [Theory]
+        [MockAutoData]
+        public static void Validate_SelectionMade_NoValidationErrors(
+            SelectSublocationsModel model,
+            SelectSublocationsModelValidator systemUnderTest)
+        {
+            Assert.Fail("not implemented");
+        }
+
+        [Theory]
+        [MockAutoData]
+        public static void Validate_NoSelectionMade_ThrowsValidationError(
+            SelectSublocationsModel model,
+            SelectSublocationsModelValidator systemUnderTest)
+        {
+            Assert.Fail("not implemented");
+        }
+    }
+}
