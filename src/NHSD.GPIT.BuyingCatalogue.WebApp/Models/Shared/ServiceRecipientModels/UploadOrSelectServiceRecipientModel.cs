5cf16291553fe9862f7169585528f32bc66d647f
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2025-04-02 09:53:46 +0100

    Content improvements for service recipients page

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Models/Shared/ServiceRecipientModels/UploadOrSelectServiceRecipientModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Models/Shared/ServiceRecipientModels/UploadOrSelectServiceRecipientModel.cs
@@ -5,16 +5,21 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.ServiceRecipientModels;
 
 public class UploadOrSelectServiceRecipientModel : NavBaseModel
 {
-    public const string YesUploadRecipientsOption = "Upload Service Recipients using a CSV file";
-    public const string NoUploadRecipientsOption = "Select Service Recipients manually";
+    private const string CsvRecipientsOption = "Upload a CSV file";
+    private const string CsvRecipientsHintText = "Create a list of service recipients and upload using a CSV file";
+
+    private const string ManualRecipientsOption = "Add service recipients manually";
+    private const string ManualRecipientsHintText = "Select the service recipients from a list of organisations";
 
     public bool? ShouldUploadRecipients { get; set; }
 
-    public override string Title => "Service Recipients";
+    public override string Title => "Service recipients";
+
+    public override string Advice => "Select how you want to add service recipients.";
 
     public IEnumerable<SelectOption<bool>> ServiceRecipientOptions => new List<SelectOption<bool>>
         {
-            new(YesUploadRecipientsOption, true),
-            new(NoUploadRecipientsOption, false),
+            new(CsvRecipientsOption, CsvRecipientsHintText, true),
+            new(ManualRecipientsOption, ManualRecipientsHintText, false),
         };
 }
