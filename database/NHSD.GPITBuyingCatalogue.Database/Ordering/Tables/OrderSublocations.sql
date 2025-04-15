eee0dbd013e1232bd2ff722b5f686b96c4da666d
Ashley Winter <ashley.winter@bjss.com>
2025-04-15 16:57:03 +0100

    24065 - remove cascade from sublocation

--- database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/OrderSublocations.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/Ordering/Tables/OrderSublocations.sql
@@ -4,7 +4,7 @@ CREATE TABLE [ordering].[OrderSublocations]
     [SublocationOdsCode] NVARCHAR(10) NOT NULL,
     [OwnerOdsCode] NVARCHAR(10) NOT NULL,
     CONSTRAINT PK_OrderSublocations PRIMARY KEY ([OrderId], [SublocationOdsCode]),
-    CONSTRAINT FK_OrderSublocations_Order FOREIGN KEY ([OrderId]) REFERENCES [ordering].[Orders]([Id]) ON DELETE CASCADE,
+    CONSTRAINT FK_OrderSublocations_Order FOREIGN KEY ([OrderId]) REFERENCES [ordering].[Orders]([Id]),
     CONSTRAINT FK_OrderSublocations_OdsOrganisations_Owner FOREIGN KEY ([OwnerOdsCode]) REFERENCES [ods_organisations].[OdsOrganisations]([Id]),
     CONSTRAINT FK_OrderSublocations_OdsOrganisations_Sublocation FOREIGN KEY ([SublocationOdsCode]) REFERENCES [ods_organisations].[OdsOrganisations]([Id])
 )
