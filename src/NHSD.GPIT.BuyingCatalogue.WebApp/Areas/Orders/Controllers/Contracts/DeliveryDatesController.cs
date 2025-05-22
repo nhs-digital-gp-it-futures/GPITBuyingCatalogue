969749532283a1bfd4a6ad10e6c38aca2d485993
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2025-05-22 13:15:01 +0100

    Bug fixing around the RolledUp object

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Controllers/Contracts/DeliveryDatesController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Orders/Controllers/Contracts/DeliveryDatesController.cs
@@ -44,11 +44,7 @@ namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Orders.Controllers.Contracts
         {
             var orderWrapper = await orderService.GetOrderThin(callOffId, internalOrgId);
 
-            var orderRollUp = orderWrapper.RolledUp;
-
-            var order = orderWrapper.Order;
-
-            var model = new SelectDateModel(internalOrgId, callOffId, order, setAllPDD, orderRollUp.DeliveryDate)
+            var model = new SelectDateModel(internalOrgId, callOffId, orderWrapper.Order, setAllPDD, orderWrapper.Previous.DeliveryDate)
             {
                 BackLink = returnUrl ?? Url.Action(
                     nameof(OrderController.Order),
