b3a30fc9ad1cce0180e897cdcca7ee62651ebf89
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2025-06-09 14:00:31 +0100

    Insert distinct workoff plans

--- database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/MigrateWorkOffPlans.sql
+++ database/NHSD.GPITBuyingCatalogue.Database/PostDeployment/MigrateWorkOffPlans.sql
@@ -1,5 +1,5 @@
 ﻿IF NOT EXISTS(SELECT * FROM catalogue.InProgressSolutionStandards)
 BEGIN
     INSERT INTO catalogue.InProgressSolutionStandards([SolutionId], [StandardId])
-    SELECT [SolutionId], [StandardId] FROM catalogue.WorkOffPlans
+    SELECT DISTINCT [SolutionId], [StandardId] FROM catalogue.WorkOffPlans
 END
