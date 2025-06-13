40e16b6b4ec50e414927bb52d397d7574d4d1230
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2025-06-13 12:10:15 +0100

    Add missing config

--- terraform/webapp/modules/bc_webapp_pri/deployment_slot.tf
+++ terraform/webapp/modules/bc_webapp_pri/deployment_slot.tf
@@ -36,6 +36,8 @@ resource "azurerm_linux_web_app_slot" "slot" {
     ip_restriction_default_action = "Deny"
     ftps_state                    = "Disabled"
     http2_enabled                 = true
+    container_registry_managed_identity_client_id = azurerm_user_assigned_identity.web_app_identity.client_id
+    container_registry_use_managed_identity = true
 
     application_stack {
       docker_image_name        = "${var.repository_name}:latest"
