d69d26891c7f460c8d50661a787939dd6f9684cf
Colin Murphy <90630744+bjss-colin@users.noreply.github.com>
2025-06-27 14:24:42 +0100

    Resolve SonarQube security issues

---
+++ src/NHSD.GPIT.BuyingCatalogue.UI.Components/RegularExpressions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NHSD.GPIT.BuyingCatalogue.UI.Components;
+
+internal static partial class RegularExpressions
+{
+    private const int MillisecondsTimeout = 500;
+
+    [GeneratedRegex(@"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+", RegexOptions.None, MillisecondsTimeout)]
+    internal static partial Regex KebabNameRegex();
+}
