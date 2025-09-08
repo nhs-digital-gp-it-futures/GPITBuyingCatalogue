0373babd6059fa6db97c55371f60d6ae2141b0d1
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2025-09-08 09:42:55 +0100

    Update regression tests

--- tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Pages/Admin/ManageSolutions/SolutionAssociatedServices/SolutionAssociatedService.cs
+++ tests/NHSD.GPIT.BuyingCatalogue.RegressionTests/Pages/Admin/ManageSolutions/SolutionAssociatedServices/SolutionAssociatedService.cs
@@ -14,7 +14,6 @@ namespace NHSD.GPIT.BuyingCatalogue.RegressionTests.Pages.Admin.ManageSolutions.
 {
     public class SolutionAssociatedService : PageBase
     {
-        private const decimal MaxPrice = 0.07M;
         private string service = string.Empty;
 
         public SolutionAssociatedService(IWebDriver driver, CommonActions commonActions, LocalWebApplicationFactory factory)
@@ -28,88 +27,15 @@ namespace NHSD.GPIT.BuyingCatalogue.RegressionTests.Pages.Admin.ManageSolutions.
 
         public LocalWebApplicationFactory Factory { get; }
 
-        public void AddAssociatedService(string solutionId, string priceType)
+        public void SelectAssociatedServices(string solutionId)
         {
             CommonActions.ClickLinkElement(AddSolutionObjects.AssociatedServiceLink(solutionId));
-            CommonActions.PageLoadedCorrectGetIndex(
-                typeof(AssociatedServicesController),
-                nameof(AssociatedServicesController.AssociatedServices))
-                .Should().BeTrue();
-
-            CommonActions.ClickLinkElement(AssociatedServicesObjects.AddAssociatedServiceLink);
-            CommonActions.PageLoadedCorrectGetIndex(
-                typeof(AssociatedServicesController),
-                nameof(AssociatedServicesController.AddAssociatedService))
-                .Should().BeTrue();
-
-            AddAssociatedServiceDetails();
-            AddListPrices(priceType, service);
-        }
-
-        private void AddAssociatedServiceDetails()
-        {
-            service = TextGenerators.TextInputAddText(CommonSelectors.Name, 50);
-            TextGenerators.TextInputAddText(CommonSelectors.Description, 1000);
-            TextGenerators.TextInputAddText(CommonSelectors.OrderGuidance, 1000);
-
-            CommonActions.ClickSave();
-
-            CommonActions.PageLoadedCorrectGetIndex(
-                typeof(AssociatedServicesController),
-                nameof(AssociatedServicesController.EditAssociatedService))
-                .Should()
-                .BeTrue();
-        }
-
-        private void AddListPrices(string priceType, string service)
-        {
-            var serviceId = GetAssociatedServiceID();
-            CommonActions.ClickLinkElement(AssociatedServicesObjects.EditPriceLink(serviceId));
-            CommonActions.PageTitle()
-                .Should()
-                .BeEquivalentTo($"{service}List price".FormatForComparison());
-
-            CommonActions.ClickLinkElement(ManageListPricesObjects.AddPriceLink);
-
-            CommonActions.PageTitle()
-                .Should()
-                .BeEquivalentTo($"{service}List price type".FormatForComparison());
-
-            if (priceType == ListPriceTypes.Flat_price.ToString())
-            {
-                ListPricesForServices.AddFlatPrice(priceType, service);
-                AssociatedServiceDashboard();
-            }
-            else
-            {
-                ListPricesForServices.AddTieredPrice(priceType, service);
-                AssociatedServiceDashboard();
-            }
-        }
-
-        private void AssociatedServiceDashboard()
-        {
-            CommonActions.ClickLastRadio();
-            CommonActions.ClickSave();
-            CommonActions.PageLoadedCorrectGetIndex(
-                typeof(AssociatedServicesController),
-                nameof(AssociatedServicesController.AssociatedServices))
-                .Should().BeTrue();
-
             CommonActions.ClickAllCheckboxes();
             CommonActions.ClickSave();
             CommonActions.PageLoadedCorrectGetIndex(
                 typeof(CatalogueSolutionsController),
                 nameof(CatalogueSolutionsController.ManageCatalogueSolution))
                 .Should().BeTrue();
-        }
-
-        private string GetAssociatedServiceID()
-        {
-            using var dbContext = Factory.DbContext;
-
-            var serviceId = dbContext.AssociatedServices.FirstOrDefault(x => x.CatalogueItem.Name == service);
-            return (serviceId != null) ? serviceId.CatalogueItemId.ToString() : string.Empty;
         }
     }
 }
