bd98933e85ae81efc04ae17cc53a1477d6fc6551
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2025-10-08 12:44:47 +0100

    Update Terraform AzureRM provider version (#1578)
    * Specify diagnostics metrics
    
    * Update terraform provider versions
    
    * Newer SQL properties

--- terraform/webapp/modules/bc_app_gateway_ingress/appgw_diagnostics.tf
+++ terraform/webapp/modules/bc_app_gateway_ingress/appgw_diagnostics.tf
@@ -2,6 +2,7 @@ module "diagnostics" {
   source                     = "../bc_diagnostics_setting"
   name                       = local.diagnostics_name
   enable_logs                = local.gateway_logs
+  enable_metrics             = local.gateway_metrics
   log_analytics_workspace_id = var.log_analytics_workspace_id
   target_resource_id         = azurerm_application_gateway.app_gateway[0].id
   count                      = var.use_app_gateway ? 1 : 0
--- terraform/webapp/modules/bc_sql_server/main.tf
+++ terraform/webapp/modules/bc_sql_server/main.tf
@@ -5,44 +5,45 @@ locals {
 }
 
 resource "azurerm_mssql_server" "sql_server" {
-  name                          = var.sqlsvr_name
-  resource_group_name           = var.resource_group
-  location                      = var.region
-  version                       = var.sql_version
-  administrator_login           = var.sql_admin_username
-  administrator_login_password  = var.sql_admin_password
+  name                                     = var.sqlsvr_name
+  resource_group_name                      = var.resource_group
+  location                                 = var.region
+  version                                  = var.sql_version
+  administrator_login                      = var.sql_admin_username
+  administrator_login_password             = var.sql_admin_password
+  express_vulnerability_assessment_enabled = true
 
   identity {
-    type                        = "SystemAssigned"
+    type = "SystemAssigned"
   }
-  
+
   azuread_administrator {
-    login_username              = var.sqladmins
-    object_id                   = var.sqladmins
+    login_username = var.sqladmins
+    object_id      = var.sqladmins
   }
 
   tags = {
-    environment                 = var.environment,
-    architecture                = "new"
+    environment  = var.environment,
+    architecture = "new"
   }
 }
 
 resource "azurerm_mssql_firewall_rule" "sql_azure_services" {
-  name                = "azure_services"
-  server_id           = azurerm_mssql_server.sql_server.id
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+  name             = "azure_services"
+  server_id        = azurerm_mssql_server.sql_server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
 
 resource "azurerm_mssql_firewall_rule" "sql_bjss_vpn" {
-  for_each            = local.firewall_rules
-  name                = each.key
-  server_id           = azurerm_mssql_server.sql_server.id
-  start_ip_address    = each.value
-  end_ip_address      = each.value
+  for_each         = local.firewall_rules
+  name             = each.key
+  server_id        = azurerm_mssql_server.sql_server.id
+  start_ip_address = each.value
+  end_ip_address   = each.value
 }
 
 resource "azurerm_mssql_server_extended_auditing_policy" "auditing" {
-  server_id              = azurerm_mssql_server.sql_server.id 
+  server_id              = azurerm_mssql_server.sql_server.id
   log_monitoring_enabled = true
 }
