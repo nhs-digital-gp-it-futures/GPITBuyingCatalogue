68aa2ba20af946f9fab4a4ef51fbb5e29e411433
hg-engineer <223458555+hg-engineer@users.noreply.github.com>
2025-11-17 14:21:08 +0000

    adding test file and unit test for selectSublocationOverviewModel, to test the title and advice properties contain the correct information for a split or merger order

---
+++ tests/NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests/Models/Shared/SelectSublocationOverviewModelTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
+using NHSD.GPIT.BuyingCatalogue.WebApp.Models.Shared.ServiceRecipientModels;
+using Xunit;
+
+namespace NHSD.GPIT.BuyingCatalogue.WebApp.UnitTests.Models.Shared;
+
+    public static class SelectSublocationOverviewModelTests
+    {
+        [Theory]
+        [InlineData(OrderTypeEnum.AssociatedServiceMerger, SelectSublocationsOverviewModel.MergerAdvice)]
+        [InlineData(OrderTypeEnum.AssociatedServiceSplit, SelectSublocationsOverviewModel.SplitAdvice)]
+        public static void Ctor_Sets_TitleAndAdvice_For_MergerOrSplit(
+        OrderTypeEnum orderType,
+        string expectedAdvice)
+        {
+            var order = new Order
+            {
+                OrderingParty = new EntityFramework.Organisations.Models.Organisation
+                {
+                    Name = "ICB Test",
+                },
+                OrderType = orderType,
+            };
+
+            var model = new SelectSublocationsOverviewModel(
+                isConfirm: true,
+                order,
+                new List<SublocationModel>(),
+                addOrChangeSublocationsLink: "/add",
+                backLink: "/back");
+
+            model.Title.Should().Be(SelectSublocationsOverviewModel.MergerOrSplitTitle);
+            model.Advice.Should().Be(expectedAdvice);
+            model.ProcessType.Should().Be("order");
+            model.Caption.Should().Be(order.CallOffId.ToString());
+        }
+}
