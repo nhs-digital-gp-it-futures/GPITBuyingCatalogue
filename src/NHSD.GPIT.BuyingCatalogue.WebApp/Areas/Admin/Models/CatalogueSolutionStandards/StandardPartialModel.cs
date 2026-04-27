627313372d8303eccc344a993a895d17dbdd3718
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2026-04-27 13:59:46 +0100

    Remove unused import

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Models/CatalogueSolutionStandards/StandardPartialModel.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Models/CatalogueSolutionStandards/StandardPartialModel.cs
@@ -2,7 +2,6 @@
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Catalogue.Models;
 using NHSD.GPIT.BuyingCatalogue.EntityFramework.Ordering.Models;
 using NHSD.GPIT.BuyingCatalogue.ServiceContracts.Solutions.Models;
-using NHSD.GPIT.BuyingCatalogue.UI.Components.Views.Shared.TagHelpers.Tags;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Models.CatalogueSolutionStandards;
 
