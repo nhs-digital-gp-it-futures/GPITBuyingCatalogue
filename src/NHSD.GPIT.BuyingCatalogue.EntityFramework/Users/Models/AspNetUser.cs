cc05d9106de3504508a5fee5a619d0649df4bc3f
bjss-colin <90630744+bjss-colin@users.noreply.github.com>
2026-05-06 13:11:22 +0100

    Remove unused using

--- src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Users/Models/AspNetUser.cs
+++ src/NHSD.GPIT.BuyingCatalogue.EntityFramework/Users/Models/AspNetUser.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.AspNetCore.Identity;
