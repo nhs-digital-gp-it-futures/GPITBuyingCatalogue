f2eaf65baeffcfbe423f0e9fb2576eae1b2bf193
ImranBashir-cgi <imran.bashir@cgi.com>
2026-05-26 09:40:18 +0100

    Add order pages engine, test data and order test

---
+++ tests/NHSD.GPIT.BuyingCatalogue.PlaywrightTests/Tests/Ordering/OrderTests.cs
@@ -0,0 +1,29 @@
+﻿using NHSD.GPIT.BuyingCatalogue.PlaywrightTests.Infrastructure;
+using NHSD.GPIT.BuyingCatalogue.PlaywrightTests.TestData.Builders;
+using Xunit.Abstractions;
+
+namespace NHSD.GPIT.BuyingCatalogue.PlaywrightTests.Tests.Ordering;
+
+public class OrderTests : BaseTest
+{
+    public OrderTests(TestServerFixture fixture, ITestOutputHelper output)
+        : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    [Trait("Category", Categories.OrderJourney)]
+    public async Task CatalogueSolutionOrder()
+    {
+        var data = new OrderTestDataBuilder()
+            .WithBaseUrl(Fixture.BaseUrl)
+            .Build();
+
+        await orderPages.LoginAsync(data);
+        await orderPages.CreateNewOrderAsync(data);
+        await orderPages.StepOnePrepareOrderAsync(data);
+        await orderPages.StepTwoAddSolutionsAndServicesAsync(data);
+        await orderPages.StepThreeCompleteContractAsync();
+        await orderPages.StepFourReviewAndCompleteOrderAsync();
+    }
+}
