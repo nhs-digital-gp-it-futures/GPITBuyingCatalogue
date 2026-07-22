9780f777fd81530360ed6439e25c8333b5dfa48c
Thomas <adawari.thomas@cgi.com>
2026-07-22 10:59:41 +0100

    fix admin display

--- src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/HomeController.cs
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Areas/Admin/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace NHSD.GPIT.BuyingCatalogue.WebApp.Areas.Admin.Controllers
 {
-    [Authorize(Roles = "AdminOnly,Onboarding,View")]
+    [Authorize(Roles = "Authority,Onboarding,View")]
     [Area("Admin")]
     [Route("admin")]
     public sealed class HomeController : Controller
