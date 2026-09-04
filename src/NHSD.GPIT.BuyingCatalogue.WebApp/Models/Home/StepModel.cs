cf567915e53cb644434a6ed7fb7325e9bf90e290
philhalder <phil.halder@cgi.com>
2026-09-04 13:29:55 +0100

    24867 GP Landing page

---
+++ src/NHSD.GPIT.BuyingCatalogue.WebApp/Models/Home/StepModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NHSD.GPIT.BuyingCatalogue.WebApp.Models.Home
+{
+    /// <summary>
+    /// Steps for the GPIT Buying Catalogue home page for the GP Option. Ticket 24867.
+    /// </summary>
+    public sealed class StepModel
+    {
+        public int Number { get; set; }
+
+        public string Title { get; set; }
+
+        public string HelpText { get; set; }
+    }
+}
